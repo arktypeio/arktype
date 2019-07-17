3e33adc6dbd7f51ffc261f61d111f228f6d9a1fa
David Blass <ssalbdivad@gmail.com>
2019-07-17 15:53:00 -0400

    Add redo-components, remove build output from git.

--- pkgs/shapeql/dist/filters/filter.d.ts
+++
@@ -1,8 +0,0 @@
-import { ValueFrom, DeepPartial } from "redo-utils";
-export declare type FilterFunction = Parameters<ValueFrom<Array<any>, "filter">>[0];
-export declare type FilterOptions<IsDeep extends boolean = false> = {
-    objectFilter?: FilterFunction;
-    arrayFilter?: FilterFunction;
-    deep?: IsDeep;
-};
-export declare const filter: <ObjectType, ReturnType_1 extends IsDeep extends true ? DeepPartial<ObjectType> : Partial<ObjectType>, IsDeep extends boolean = false>(o: ObjectType, options: FilterOptions<IsDeep>) => ReturnType_1;
--- pkgs/shapeql/dist/filters/withArrays.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const withArrays: (o: object) => object;
