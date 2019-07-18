51aca97bd0e79103ac55b93b3ccfeb8936ce5701
sarthak <seemanjeev@gmail.com>
2019-07-18 14:55:55 -0400

    Add TestResolver.

--- pkgs/redo-server/src/resolvers/index.ts
+++ pkgs/redo-server/src/resolvers/index.ts
@@ -1,3 +1,4 @@
 import { SessionResolver } from "./SessionResolver"
 import { UserResolver } from "./UserResolver"
-export const resolvers = [SessionResolver, UserResolver]
+import { TestResolver } from "./TestResolver"
+export const resolvers = [SessionResolver, UserResolver, TestResolver]
