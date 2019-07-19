75d91c05b09d4e592fa4ebb550f3550ea0137815
David Blass <david@redo.qa>
2019-07-19 13:34:26 -0400

    Fix storybook in redo-components.

--- pkgs/redo-components/.storybook/config.js
+++ pkgs/redo-components/.storybook/config.js
@@ -1,9 +1,8 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react"
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context("../src", true, /.stories.tsx$/)
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+    req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
---
+++ pkgs/redo-components/.storybook/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = ({ config }) => {
+    console.log(config)
+    config.module.rules.push({
+        test: /\.tsx?$/,
+        use: [
+            {
+                loader: require.resolve("babel-loader")
+            },
+            require.resolve("react-docgen-typescript-loader")
+        ]
+    })
+
+    config.resolve.extensions.push(".ts", ".tsx")
+
+    return config
+}
