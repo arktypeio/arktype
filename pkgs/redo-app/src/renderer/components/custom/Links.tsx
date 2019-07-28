324800c2333884b7703b78d29d026545a3127ddd
sarthak <seemanjeev@gmail.com>
2019-07-28 11:22:01 -0400

    Merge from master.

--- pkgs/redo-app/src/renderer/components/custom/Links.tsx
+++ pkgs/redo-app/src/renderer/components/custom/Links.tsx
@@ -10,3 +10,17 @@ export const SecondarySignInButton = (props: SecondaryButtonLinkProps) => (
 export const SecondarySignUpButton = (props: SecondaryButtonLinkProps) => (
     <SecondaryButton linkTo={Page.SignUp} {...props} />
 )
+
+// export const SecondarySignUpButton = (props: SecondaryButtonLinkProps) => {
+//     const [changePage] = useMutation({})
+//     const linkTo = Page.SignUp
+//     const onClick2 = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+//         if (onClick2) {
+//             onClick2(e)
+//         }
+//         if (linkTo) {
+//             changePage({ page: linkTo })
+//         }
+//     }
+//     return <SecondaryButton onClick={onClick2} {...props} />
+// }
