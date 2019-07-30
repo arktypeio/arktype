0a628a2e36dfb8b44e7b82fcf1ad8b33e0059650
David Blass <david@redo.qa>
2019-07-29 21:55:48 -0400

    Add wsl support for website devserver script.

--- pkgs/redo-website/webpack.devServer.config.ts
+++ pkgs/redo-website/webpack.devServer.config.ts
@@ -1,10 +1,20 @@
 import merge from "webpack-merge"
 import { devServerConfig } from "redo-bundle/webpack.base"
 import { config } from "./webpack.common"
+import isWsl from "is-wsl"
+import open from "open"
+import { exec } from "child_process"
 
 const webDevServerConfig = merge.smart(devServerConfig, config, {
     devServer: {
-        open: true
+        host: "0.0.0.0",
+        after() {
+            if (isWsl) {
+                exec("echo 'start http://localhost:8080' | cmd.exe")
+            } else {
+                open("http://localhost:8080")
+            }
+        }
     }
 } as any)
 
