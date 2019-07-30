d83f7f155aeefd387b2d4f29ae258a9939cb08f9
David Blass <david@redo.qa>
2019-07-30 04:07:02 -0400

    Fix cross-platform hmr.

--- pkgs/redo-website/webpack.devServer.config.ts
+++ pkgs/redo-website/webpack.devServer.config.ts
@@ -1,20 +1,12 @@
 import merge from "webpack-merge"
 import { devServerConfig } from "redo-bundle/webpack.base"
 import { config } from "./webpack.common"
-import isWsl from "is-wsl"
-import open from "open"
-import { exec } from "child_process"
 
 const webDevServerConfig = merge.smart(devServerConfig, config, {
     devServer: {
         host: "0.0.0.0",
-        after() {
-            if (isWsl) {
-                exec("echo 'start http://localhost:8080' | cmd.exe")
-            } else {
-                open("http://localhost:8080")
-            }
-        }
+        useLocalIp: true,
+        open: true
     }
 } as any)
 
