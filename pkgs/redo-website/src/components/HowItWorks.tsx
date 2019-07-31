371b746e937d2720b09769e8aa0e2ee5532e7af8
David Blass <david@redo.qa>
2019-07-31 19:57:15 -0400

    Improve website layout.

--- pkgs/redo-website/src/components/HowItWorks.tsx
+++ pkgs/redo-website/src/components/HowItWorks.tsx
@@ -1,25 +1,41 @@
 import React, { FC } from "react"
-import { Row, Text, Card } from "redo-components"
-import { Icon } from "@material-ui/core"
-import TrendingUp from "@material-ui/icons/TrendingUp"
-import WhatsHot from "@material-ui/icons/Whatshot"
-import Code from "@material-ui/icons/Code"
+import { Row, Text, Card, Column } from "redo-components"
+import { makeStyles, useTheme } from "@material-ui/styles"
+import { Theme, Grid } from "@material-ui/core"
+
+const stylize = makeStyles((theme: Theme) => ({
+    descriptionText: {
+        lineHeight: 3.6
+    }
+}))
+
+type StepProps = {
+    label: string
+    children: string
+}
+
+const Step: FC<StepProps> = ({ label, children }) => {
+    const { descriptionText } = stylize()
+    return (
+        <Card width="100%">
+            <Row align="baseline">
+                <Text variant="h3" color="secondary">
+                    {label}
+                </Text>
+                <Text className={descriptionText} variant="h5" color="primary">
+                    {children}
+                </Text>
+            </Row>
+        </Card>
+    )
+}
 
 export const HowItWorks: FC = () => {
     return (
-        <Row justify="space-between">
-            <Card>
-                <Code fontSize="large" />
-                <Text variant={"h4"}>We're</Text>
-            </Card>
-            <Card>
-                <TrendingUp fontSize="large" />
-                <Text variant={"h4"}>Fucking</Text>
-            </Card>
-            <Card>
-                <WhatsHot fontSize="large" />
-                <Text variant={"h4"}>Awesome</Text>
-            </Card>
-        </Row>
+        <>
+            <Step label="1.">Open the Redo desktop app</Step>
+            <Step label="2.">Interact with your website</Step>
+            <Step label="3.">Save your new automated test</Step>
+        </>
     )
 }
