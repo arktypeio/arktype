b71c9baf175d8bdcd06e47a7adc564bdcc5260af
sarthak <seemanjeev@gmail.com>
2019-08-12 13:49:45 -0400

    Delete modalView and edit stories.

--- pkgs/redo-components/src/buttons/Button.tsx
+++ pkgs/redo-components/src/buttons/Button.tsx
@@ -28,7 +28,6 @@ export const Button: FC<ButtonProps> = ({
     style,
     ...rest
 }) => {
-    console.log(useTheme())
     const { style: kindStyle, ...kindRest } = useKind(kind)
     return (
         <MuiButton
