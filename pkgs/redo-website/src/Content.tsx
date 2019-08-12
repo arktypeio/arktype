a0a570e950c29d617ff8b12d9be89f0a69d20e58
bavannah <30559691+bavannah@users.noreply.github.com>
2019-08-12 17:33:33 -0400

    Fix cross-browser header and add scroll-under, add layout abstractions.

--- pkgs/redo-website/src/Content.tsx
+++ pkgs/redo-website/src/Content.tsx
@@ -2,13 +2,21 @@ import "typeface-ubuntu"
 import React, { FC } from "react"
 import { AppContents, Column } from "redo-components"
 import { AppHeader, HowItWorks, SignUp, SubHeader } from "./components"
+import { layout } from "./constants"
 
 export const Content: FC = () => {
     return (
         <AppContents>
             <Column align="center" style={{ overflow: "hidden" }}>
                 <AppHeader mobile={true} />
-                <Column spacing={4} style={{ maxWidth: 500, padding: 16 }}>
+                <Column
+                    spacing={4}
+                    style={{
+                        position: "absolute",
+                        top: layout.header.height,
+                        ...layout.content
+                    }}
+                >
                     <SubHeader />
                     <HowItWorks />
                     <SignUp />
