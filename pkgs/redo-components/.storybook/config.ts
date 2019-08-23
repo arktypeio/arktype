e7782fe6bb53455736ec0ea0d2ebe444663e5786
David Blass <david@redo.qa>
2019-08-22 23:54:28 -0400

    Iterate on storyshots tests.

--- pkgs/redo-components/.storybook/config.ts
+++ pkgs/redo-components/.storybook/config.ts
@@ -1,21 +1,27 @@
 import { configure } from "@storybook/react"
 import { withWrapper } from "./wrapper"
 
-const loadStories = require.context
-    ? () => {
-          const req = require.context("../src", true, /\.stories.tsx$/)
-          return () => req.keys().forEach((file: string) => req(file))
-      }
-    : () => {
-          const { walkPaths } = require("redo-utils/dist/fsUtils") as {
-              walkPaths: (dir: string) => string[]
+const loadStories =
+    "context" in require
+        ? () => {
+              const req = (require as any).context(
+                  "../src",
+                  true,
+                  /\.stories.tsx$/
+              )
+              return () => req.keys().forEach((file: string) => req(file))
           }
-          const { resolve } = require("path")
-          walkPaths(resolve(__dirname, "..", "src")).forEach(path => {
-              if (path.match(/\.stories.tsx$/)) {
-                  require(path)
+        : () => {
+              const { walkPaths } = require("redo-utils/dist/fsUtils") as {
+                  walkPaths: (dir: string) => string[]
               }
-          })
-      }
+              const { resolve } = require("path")
+              walkPaths(resolve(__dirname, "..", "src")).forEach(path => {
+                  if (path.match(/\.stories.tsx$/)) {
+                      require(path)
+                  }
+              })
+          }
+
 configure(loadStories, module)
 withWrapper()
