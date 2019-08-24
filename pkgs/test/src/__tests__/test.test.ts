209aa319a5da4ee7ec27e9dd13caf4bb36cc23c6
David Blass <ssalbdivad@gmail.com>
2019-08-24 01:01:36 -0400

    Working test example.

--- pkgs/test/src/__tests__/test.test.ts
+++ pkgs/test/src/__tests__/test.test.ts
@@ -2,11 +2,15 @@ import { test as t } from "../test"
 
 describe("test", () => {
     test("works", async () => {
-        await t({
-            type: "click",
-            selector: ".MuiButton-label",
-            value: "",
-            tags: []
-        })
+        await t(
+            ["click", { selector: { css: ".MuiButton-label" } }],
+            [
+                "set",
+                {
+                    selector: { css: "[name='email']" },
+                    value: "awesome@burba.com"
+                }
+            ]
+        )
     }, 60000)
 })
--- pkgs/test/src/test.ts
+++ pkgs/test/src/test.ts
@@ -1,34 +1,85 @@
-import p, { Page } from "puppeteer"
-import { BrowserEventInput } from "redo-model"
-import { ValueFrom } from "redo-utils"
+import p, { Page, Browser } from "puppeteer"
+import { ValueFrom, ValueOf } from "redo-utils"
 
-export const test = async (...steps: BrowserEventInput[]) => {
+export const test = async (...steps: Step[]) => {
     const browser = await p.launch({ headless: false, slowMo: 250 })
     const page = await browser.newPage()
     await page.goto("https://redo.qa")
     await page.screenshot({ path: "before.png" })
     for (const step of steps) {
-        await redo(step, page)
+        await redo({ step, context: { browser, page } })
     }
     await page.screenshot({ path: "after.png" })
     await browser.close()
     return true
 }
 
-export const redo = async (step: BrowserEventInput, page: Page) => {
-    await stepTypes[step.type](step, page)
+export type Context = {
+    browser: Browser
+    page: Page
 }
 
-export const click = async ({ selector }: BrowserEventInput, page: Page) => {
-    await page.click(selector)
+export type Selector = {
+    css: string
+}
+
+export type RedoArgs = {
+    step: Step
+    context: Context
+}
+
+export const redo = async ({ step: [type, args], context }: RedoArgs) => {
+    await stepTypes[type](args as any, context)
+}
+
+export type ClickArgs = {
+    selector: Selector
+}
+
+export const click = async (
+    { selector: { css } }: ClickArgs,
+    { page }: Context
+) => {
+    await page.click(css)
+}
+
+export type GoArgs = {
+    url: string
+}
+
+export const go = async ({ url }: GoArgs, { page }: Context) =>
+    await page.goto(url)
+
+export type SetArgs = {
+    selector: Selector
+    value: string
+}
+
+export const set = async (
+    { selector: { css }, value }: SetArgs,
+    { page }: Context
+) => {
+    await page.type(css, value)
+}
+
+export type ScreenshotArgs = {}
+
+export const screenshot = async (args: ScreenshotArgs, { page }: Context) => {
+    await page.screenshot()
 }
 
 // TODO: Update BrowserEventInput type in redo-model
-export const stepTypes: {
-    [K in ValueFrom<BrowserEventInput, "type">]: (
-        step: BrowserEventInput,
-        page: Page
-    ) => Promise<void>
-} = {
-    click
+export const stepTypes = {
+    click,
+    go,
+    set,
+    screenshot
 }
+
+export type StepTypes = typeof stepTypes
+
+export type StepKey = keyof StepTypes
+
+export type Step = ValueOf<
+    { [K in keyof StepTypes]: [K, Parameters<StepTypes[K]>[0]] }
+>
