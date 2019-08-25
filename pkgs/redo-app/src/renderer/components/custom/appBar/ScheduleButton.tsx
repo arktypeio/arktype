2f4c599b360e74461d95eaa227fbe9cc022e7b9c
bavannah <30559691+bavannah@users.noreply.github.com>
2019-08-24 20:30:36 -0400

    Create Icon Index

--- pkgs/redo-app/src/renderer/components/custom/appBar/ScheduleButton.tsx
+++ pkgs/redo-app/src/renderer/components/custom/appBar/ScheduleButton.tsx
@@ -1,9 +1,8 @@
 import React, { FC } from "react"
-import { Schedule } from "@material-ui/icons"
-import { IconButton } from "redo-components"
+import { IconButton, Icons } from "redo-components"
 
 export type ScheduleButtonProps = {}
 
 export const ScheduleButton: FC<ScheduleButtonProps> = ({}) => {
-    return <IconButton Icon={Schedule} style={{ color: "white" }} />
+    return <IconButton Icon={Icons.schedule} style={{ color: "white" }} />
 }
