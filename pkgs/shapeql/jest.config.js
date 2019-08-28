9449106ff457b5cd9226f8451f422d6dce876860
David Blass <ssalbdivad@gmail.com>
2019-08-27 22:06:39 -0400

    Cleanup.

--- pkgs/shapeql/jest.config.js
+++ pkgs/shapeql/jest.config.js
@@ -2,7 +2,5 @@ const { join } = require("path")
 
 module.exports = {
     ...require("@re-do/recommended/jest"),
-    setupFiles: [join(__dirname, "jest.setup.js")],
-    reporters: ["default", "jest-stare"],
-    testResultsProcessor: "./node_modules/jest-stare"
+    setupFiles: [join(__dirname, "jest.setup.js")]
 }
