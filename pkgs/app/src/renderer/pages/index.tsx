bcd90c5cdf500f6b4715308a00e65fa710a5f7d3
David Blass <ssalbdivad@users.noreply.github.com>
2019-10-09 21:17:55 -0400

    Add test results page.

--- pkgs/app/src/renderer/pages/index.tsx
+++ pkgs/app/src/renderer/pages/index.tsx
@@ -2,5 +2,6 @@ export * from "./Home"
 export * from "./Learner"
 export * from "./landing"
 export * from "./Detail"
+export * from "./Results"
 // Should always be the last export
 export * from "./PageRouter"
