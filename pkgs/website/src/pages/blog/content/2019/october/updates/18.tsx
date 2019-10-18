b50ace2b8dd6393459dc16129a80cf6488529506
David Blass <ssalbdivad@users.noreply.github.com>
2019-10-18 14:23:28 -0400

    Fix spacing between posts and progress, reorganize content dir.

--- pkgs/website/src/pages/blog/content/2019/october/updates/18.tsx
+++
@@ -1,9 +0,0 @@
-import { UpdateData } from "../../../../common"
-
-export const oct182019: UpdateData = {
-    date: new Date(2019, 9, 18),
-    goals: {
-        "integrate landing page analytics with MailChimp": false,
-        "finish new type model on test-execution branch": false
-    }
-}
