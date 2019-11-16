09944456c51861033d435089145d1716b240f5d8
David Blass <ssalbdivad@gmail.com>
2019-11-16 13:37:39 -0500

    11-16-2019 daily progress.

--- pkgs/website/src/pages/blog/content/2019/november/updates.tsx
+++ pkgs/website/src/pages/blog/content/2019/november/updates.tsx
@@ -82,5 +82,11 @@ export const november2019Updates: UpdateData[] = [
         goals: {
             "finish input type generation change": false
         }
+    },
+    {
+        date: new Date(2019, 10, 16),
+        goals: {
+            "finish input type generation change": false
+        }
     }
 ]
