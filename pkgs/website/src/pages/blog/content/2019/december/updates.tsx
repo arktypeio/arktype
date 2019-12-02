7012b0377a7bf7d391faecaaea1f838e17ca9d79
David Blass <ssalbdivad@gmail.com>
2019-12-02 13:23:45 -0500

    12-2-2019 daily progress.

--- pkgs/website/src/pages/blog/content/2019/december/updates.tsx
+++ pkgs/website/src/pages/blog/content/2019/december/updates.tsx
@@ -7,5 +7,12 @@ export const december2019Updates: UpdateData[] = [
             "update context args PR": false,
             "integrate app with new model": false
         }
+    },
+    {
+        date: new Date(2019, 11, 2),
+        goals: {
+            "update context args PR": false,
+            "integrate app with new model": false
+        }
     }
 ]
