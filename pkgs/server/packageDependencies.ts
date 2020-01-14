aa930b97019123f87517c0b19d1f9886681af5b4
David Blass <ssalbdivad@gmail.com>
2020-01-13 21:04:02 -0500

    Centralize 'command' util, clean up server pack script.

--- pkgs/server/packageDependencies.ts
+++ pkgs/server/packageDependencies.ts
@@ -1,21 +1,19 @@
 import { removeSync, moveSync } from "fs-extra"
-import { commandSync } from "execa"
+import { command } from "@re-do/utils/dist/command"
 
-console.log("Packaging generated dependencies...")
 //rm -rf .tmp-sls-pkg
 removeSync(".tmp-sls-pkg")
 //unzip .serverless/redo-server.zip -d .tmp-sls-pkg
-commandSync("unzip .serverless/redo-server.zip -d .tmp-sls-pkg")
+command("unzip .serverless/redo-server.zip -d .tmp-sls-pkg")
 //rsync -rk node_modules/@types .tmp-sls-pkg/node_modules
-commandSync("rsync -rk node_modules/@types .tmp-sls-pkg/node_modules")
+command("rsync -rk node_modules/@types .tmp-sls-pkg/node_modules")
 //rsync -rk node_modules/@prisma .tmp-sls-pkg/node_modules
-commandSync("rsync -rk node_modules/@prisma .tmp-sls-pkg/node_modules")
+command("rsync -rk node_modules/@prisma .tmp-sls-pkg/node_modules")
 //zip -r redo-server.zip * {cwd:.tmp-sls-pkg}
-commandSync("zip -r redo-server.zip *", { shell: true, cwd: ".tmp-sls-pkg" })
+command("zip -r redo-server.zip *", { cwd: ".tmp-sls-pkg" })
 // mv .tmp-sls-pkg/redo-server.zip .serverless
 moveSync(".tmp-sls-pkg/redo-server.zip", ".serverless/redo-server.zip", {
     overwrite: true
 })
 // rm -rf .tmp-sls-pkg
 removeSync(".tmp-sls-pkg")
-console.log("Successfully packaged.")
