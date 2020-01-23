b10465a87a888105d68b755c813936bb80ec72da
David Blass <ssalbdivad@gmail.com>
2020-01-23 18:25:54 -0500

    Update versions and publish.

--- pkgs/server/packageDependencies.ts
+++ pkgs/server/packageDependencies.ts
@@ -1,10 +1,15 @@
-import { removeSync, moveSync } from "fs-extra"
+import { join } from "path"
+import { removeSync, moveSync, copySync } from "fs-extra"
 import { command } from "@re-do/utils/dist/command"
 
 removeSync(".tmp-sls-pkg")
 command("unzip .serverless/redo-server.zip -d .tmp-sls-pkg")
 command("rsync -rk node_modules/@types .tmp-sls-pkg/node_modules")
 command("rsync -rk node_modules/@prisma .tmp-sls-pkg/node_modules")
+copySync(
+    join(__dirname, "schema.gql"),
+    join(__dirname, ".tmp-sls-pkg", "schema.gql")
+)
 command("zip -r redo-server.zip *", { cwd: ".tmp-sls-pkg" })
 moveSync(".tmp-sls-pkg/redo-server.zip", ".serverless/redo-server.zip", {
     overwrite: true
