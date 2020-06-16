c215efc5d6526b3e01d035542f06d474ad0e8e50
David Blass <ssalbdivad@gmail.com>
2020-06-16 05:21:15 -0400

    Temporarily remove Twitch info.

--- pkgs/website/src/components/TwitchInfo.tsx
+++ pkgs/website/src/components/TwitchInfo.tsx
@@ -4,7 +4,7 @@ import { layout } from "../constants"
 
 export const TwitchInfo = () => (
     <Column align="center" width={layout.signUpWidth}>
-        <a href="https://bit.ly/on-twitch" target="_blank">
+        {/* <a href="https://bit.ly/on-twitch" target="_blank">
             <Text variant="h4" color="primary">
                 <Icons.twitch color="primary" style={{ fontSize: 58 }} />
                 Watch live!
@@ -15,6 +15,6 @@ export const TwitchInfo = () => (
             We're building Redo live on Twitch! Drop in to watch, say hi, or ask
             us anything! A lot of us could stand to feel a little less alone
             right now 💓
-        </Text>
+        </Text> */}
     </Column>
 )
