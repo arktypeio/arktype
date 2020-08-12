5e2cfacdb7df5d0a66064a3e82c438e558ed21a6
David Blass <ssalbdivad@users.noreply.github.com>
2020-08-12 13:51:36 -0400

    Ensure demo video is always displayed under Steps (#221)

--- pkgs/website/src/components/howItWorks/HowItWorks.tsx
+++ pkgs/website/src/components/howItWorks/HowItWorks.tsx
@@ -6,20 +6,14 @@ import { Steps } from "./Steps"
 export const HowItWorks = () => {
     const maxWidth = window.innerWidth >= 528 ? 480 : 0.9 * window.innerWidth
     return (
-        <Column align="center" style={{ paddingTop: 48 }}>
+        <Column align="center" style={{ paddingTop: 48, maxWidth }}>
             <Text variant="h2" style={{ fontWeight: 700 }}>
                 How it works
             </Text>
-            <Row wrap="wrap" justify="center" style={{ maxWidth }}>
-                <div style={{ maxWidth }}>
-                    <Steps>{steps}</Steps>
-                </div>
-                <video
-                    style={{ maxWidth }}
-                    src="assets/RedoDemo.mp4"
-                    controls
-                />
-            </Row>
+            <div style={{ maxWidth }}>
+                <Steps>{steps}</Steps>
+            </div>
+            <video style={{ maxWidth }} src="assets/RedoDemo.mp4" controls />
         </Column>
     )
 }
