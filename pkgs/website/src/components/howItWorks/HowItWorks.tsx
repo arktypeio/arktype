b575dcc2b656c3e9e9e0fae175d0014b9411d0df
David Blass <ssalbdivad@gmail.com>
2020-08-12 11:53:46 -0400

    Fix mobile demo video layout styling.

--- pkgs/website/src/components/howItWorks/HowItWorks.tsx
+++ pkgs/website/src/components/howItWorks/HowItWorks.tsx
@@ -4,17 +4,18 @@ import { steps } from "content"
 import { Steps } from "./Steps"
 
 export const HowItWorks = () => {
+    const maxWidth = window.innerWidth >= 528 ? 480 : 0.9 * window.innerWidth
     return (
         <Column align="center" style={{ paddingTop: 48 }}>
             <Text variant="h2" style={{ fontWeight: 700 }}>
                 How it works
             </Text>
-            <Row wrap="wrap" justify="center" style={{ maxWidth: "90vw" }}>
-                <div style={{ width: 480 }}>
+            <Row wrap="wrap" justify="center" style={{ maxWidth }}>
+                <div style={{ maxWidth }}>
                     <Steps>{steps}</Steps>
                 </div>
                 <video
-                    style={{ width: 480 }}
+                    style={{ maxWidth }}
                     src="assets/RedoDemo.mp4"
                     controls
                 />
