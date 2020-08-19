7dfc4260fea8755d89e15a612bf185eca85df133
David Blass <ssalbdivad@users.noreply.github.com>
2020-08-18 20:24:05 -0400

    Improve web sign up error handling, integrate Google Analytics. (#223)

--- pkgs/website/src/components/signUp/SignUpForm.tsx
+++ pkgs/website/src/components/signUp/SignUpForm.tsx
@@ -1,43 +1,46 @@
 import React from "react"
 import { Form, FormText, FormSubmit, Button } from "@re-do/components"
 import { track } from "./analytics"
+import validator from "validator"
 
 export const SignUpForm = () => (
     <Form<{ email: string }, boolean>
-        validate={(_) => ({ email: [] })}
+        validate={({ email }) => {
+            const errors: string[] = []
+            if (!validator.isEmail(email)) {
+                errors.push("That doesn't look like a valid email.")
+            }
+            if (!(window as any).ga.loaded) {
+                const uBlockErrorMessage = `It looks like you have uBlock or another adblocker\
+                    enabled. That's totally cool, but unfortunately, it's\
+                    blocking us from saving your email. Try temporarily\
+                    disabling it, reloading the page, then submitting again.`
+                errors.push(uBlockErrorMessage)
+            }
+            return { email: errors }
+        }}
         submit={async (options: any) => {
-            track.prelaunchRegister({
-                email: options?.variables?.email!
+            const email = options.variables.email
+            track.subscribe({
+                email
             })
-            return { data: true }
+            return { data: { email } }
         }}
     >
         {({ data }) => (
             <>
                 <FormText name="email" />
                 <FormSubmit>
-                    {data ? (
-                        <Button
-                            style={{
-                                fontSize: "large",
-                                fontWeight: 700,
-                                minWidth: 200
-                            }}
-                            disabled
-                        >
-                            You're in the loop 💌
-                        </Button>
-                    ) : (
-                        <Button
-                            style={{
-                                fontSize: "large",
-                                fontWeight: 700,
-                                minWidth: 200
-                            }}
-                        >
-                            Keep me posted!
-                        </Button>
-                    )}
+                    <Button
+                        style={{
+                            fontSize: "large",
+                            fontWeight: 700,
+                            minWidth: 200
+                        }}
+                        disabled={!!data}
+                    >
+                        {data ? "You're in the loop 💌" : "Keep me posted!"}
+                    </Button>
                 </FormSubmit>
             </>
         )}
--- pkgs/website/src/components/signUp/analytics.ts
+++ pkgs/website/src/components/signUp/analytics.ts
@@ -1,5 +1,12 @@
 import Analytics from "analytics-node"
 import { v4 } from "uuid"
+import reactGA from "react-ga"
+
+reactGA.initialize("UA-173540201-1", {
+    debug: true,
+    alwaysSendToDefaultTracker: true
+})
+reactGA.pageview(window.location.pathname + window.location.search)
 
 const client = new Analytics(
     process.env.NODE_ENV === "production"
@@ -7,17 +14,22 @@ const client = new Analytics(
         : "lcfnD5Bi8kwcsL0S6ctbBUS43J18vLs6"
 )
 
-type TrackOptions = {
+type SubscribeOptions = {
     email: string
 }
 
-const prelaunchRegister = ({ email }: TrackOptions) => {
+const subscribe = ({ email }: SubscribeOptions) => {
+    if (!localStorage.userId) {
+        localStorage.userId = v4()
+    }
     client.identify({
-        anonymousId: getAnonymousUserId(),
+        anonymousId: localStorage.userId,
         traits: {
             email
         }
     })
+    reactGA.set({ userId: localStorage.userId, email })
+    reactGA.event({ category: "User", action: "Signed Up" })
 }
 
 export const getAnonymousUserId = () => {
@@ -28,5 +40,5 @@ export const getAnonymousUserId = () => {
 }
 
 export const track = {
-    prelaunchRegister
+    subscribe
 }
