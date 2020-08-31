e320a4339f635c54ef4ac07ec67ee6deb375870b
David Blass <david@redo.qa>
2020-08-31 12:12:11 -0400

    Migrate server code out of monorepo.

--- pkgs/server/src/utils.ts
+++
@@ -1,9 +0,0 @@
-export const ifExists = async <T>(find: () => Promise<T>) => {
-    try {
-        return await find()
-    } catch {
-        return null
-    }
-}
-
-export const APP_SECRET = process.env.APP_SECRET as string
