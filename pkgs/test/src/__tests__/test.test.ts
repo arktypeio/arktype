4cdd2a2a7439de7553ca62163745558c1f5e710e
David Blass <ssalbdivad@gmail.com>
2020-09-14 14:29:38 -0400

    Fix test selectors.

--- pkgs/test/src/__tests__/test.test.ts
+++ pkgs/test/src/__tests__/test.test.ts
@@ -2,15 +2,19 @@ import { test as t } from "../test"
 
 describe("test", () => {
     test("works", async () => {
-        await t([
+        await t(
             [
-                "set",
-                {
-                    selector: "[name='email']",
-                    value: "david@redo.qa"
-                }
+                ["click", { selector: "'Get Started'" }],
+                [
+                    "set",
+                    {
+                        selector: "[name='email']",
+                        value: "david@redo.qa"
+                    }
+                ],
+                ["click", { selector: "'Keep me posted!'" }]
             ],
-            ["click", { selector: ".MuiButton-label" }]
-        ])
+            { headless: false, slowMo: 50 }
+        )
     }, 60000)
 })
