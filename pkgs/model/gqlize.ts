28de4a53d63f53389908e9600cf4662574376ee7
David Blass <ssalbdivad@gmail.com>
2021-02-02 13:25:58 -0500

    Fix model build.

---
+++ pkgs/model/gqlize.ts
@@ -0,0 +1,5 @@
+import { gqlize } from "gqlize"
+import { writeFileSync } from "fs-extra"
+import { join } from "path"
+
+writeFileSync(join(__dirname, "queries.gql"), gqlize({schema: join(__dirname, "schema.gql")}))
