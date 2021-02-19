bb8564339b10fff68a103372cc1b517919980825
David Blass <ssalbdivad@gmail.com>
2021-02-18 22:52:31 -0500

    Update model to find and store elements from tests.

--- pkgs/model/src/__tests__/model.test.ts
+++ pkgs/model/src/__tests__/model.test.ts
@@ -1,11 +1,15 @@
 import { join } from "path"
-import { loadStore } from "../model"
+import { loadStore, RedoStore } from "../model"
 import { removeSync } from "fs-extra"
+jest.mock("uuid", () => ({ v4: () => "FAKE-UUID" }))
 
 describe("store", () => {
-    const path = join(__dirname, "redo.json")
-    removeSync(path)
-    const store = loadStore({ path })
+    let store: RedoStore
+    beforeEach(() => {
+        const path = join(__dirname, "redo.json")
+        removeSync(path)
+        store = loadStore({ path })
+    })
     test("can retrieve existing tests", () => {
         expect(store.getTests()).toStrictEqual([])
     })
@@ -15,5 +19,38 @@ describe("store", () => {
         expect(store.getTests()).toStrictEqual([newTest])
         store.createTest(newTest)
         expect(store.getTests()).toStrictEqual([newTest, newTest])
+    })
+    const testData = {
+        name: "Test With Steps",
+        steps: [
+            { kind: "click", selector: "#button" },
+            {
+                kind: "set",
+                selector: "#textbox",
+                value: "New Value"
+            }
+        ],
+        tags: []
+    }
+    const storedTestData = {
+        name: "Test With Steps",
+        steps: [
+            { kind: "click", id: "FAKE-UUID" },
+            {
+                kind: "set",
+                id: "FAKE-UUID",
+                value: "New Value"
+            }
+        ],
+        tags: []
+    }
+    test("translates selectors to elements", () => {
+        store.createTest(testData as any)
+        expect(store.getTests()).toStrictEqual([storedTestData])
+    })
+    test("translates stored tests to executable tests", () => {
+        expect(store.testToSteps(storedTestData as any)).toStrictEqual(
+            testData.steps
+        )
     })
 })
--- pkgs/model/src/model.ts
+++ pkgs/model/src/model.ts
@@ -6,6 +6,15 @@ import {
     existsSync,
     writeJSONSync
 } from "fs-extra"
+import { v4 } from "uuid"
+
+export type StoredStep = Omit<Step, "selector"> & { element: string }
+
+export type StoredTest = {
+    name: string
+    steps: StoredStep[]
+    tags: string[]
+}
 
 export type Test = {
     name: string
@@ -14,7 +23,7 @@ export type Test = {
 }
 
 export type Element = {
-    id: number
+    id: string
     selector: string
 }
 
@@ -22,25 +31,62 @@ export type LoadStoreArgs = {
     path: string
 }
 
-export type RedoStore = {
-    tests: Test[]
+export type RedoData = {
+    tests: StoredTest[]
     elements: Element[]
 }
+
+export type RedoStore = ReturnType<typeof loadStore>
 
 export const loadStore = ({ path }: LoadStoreArgs) => {
     if (!existsSync(path)) {
         writeJSONSync(path, { tests: [], elements: [] }, { spaces: 4 })
     }
     const store = createStore({
-        initial: readJSONSync(path) as RedoStore,
+        initial: readJSONSync(path) as RedoData,
         handler: (data) => writeJsonSync(path, data, { spaces: 4 })
     })
+    const getElements = () => store.query({ elements: true }).elements
+    const createElement = (data: Element) =>
+        store.mutate({ elements: (_) => _.concat(data) })
     return {
-        createTest: (data: Test) =>
-            store.mutate({ tests: (_) => _.concat(data) }),
+        createTest: (data: Test) => {
+            data.steps = data.steps.map((step) => {
+                if ("selector" in step) {
+                    const { selector, ...data } = step
+                    const existingElement = getElements().find(
+                        (element) => element.selector === selector
+                    )
+                    if (existingElement) {
+                        return { ...data, id: existingElement.id }
+                    }
+                    const id = v4()
+                    createElement({ selector, id })
+                    return { ...data, id } as any
+                }
+            })
+            store.mutate({ tests: (_) => _.concat(data as any) })
+        },
         getTests: () => store.query({ tests: true }).tests,
-        createElement: (data: Element) =>
-            store.mutate({ elements: (_) => _.concat(data) }),
-        getElements: () => store.query({ elements: true }).elements
+        createElement: getElements,
+        testToSteps: (test: StoredTest): Step[] =>
+            test.steps.map((step) => {
+                if (step.element) {
+                    const { element, ...data } = step
+                    const storedElement = getElements().find(
+                        (storedElement) => storedElement.id === element
+                    )
+                    if (!storedElement) {
+                        throw new Error(
+                            `Element with id ${element} does not exist.`
+                        )
+                    }
+                    return {
+                        ...data,
+                        id: storedElement.id
+                    }
+                }
+                return step as any
+            })
     }
 }
