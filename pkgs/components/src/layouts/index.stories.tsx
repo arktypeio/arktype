a2c9c0fc5d7e303bb09e5ab4d461a1d39fe6d3b0
David Blass <david@redo.qa>
2021-04-27 05:46:28 -0400

    Begin fixing layout stories.

--- pkgs/components/src/layouts/index.stories.tsx
+++ pkgs/components/src/layouts/index.stories.tsx
@@ -1,9 +1,8 @@
 import React from "react"
-import { storiesOf } from "@storybook/react"
-import { withKnobs, select, boolean, number } from "@storybook/addon-knobs"
+import { select, boolean, number } from "@storybook/addon-knobs"
 import { defaultTheme, ThemeProvider } from "../styles"
 import { Card } from "../cards"
-import { Row as R, Column as C } from "."
+import { Row, Column } from "."
 
 export default {
     title: "Layouts"
@@ -40,6 +39,34 @@ const getKnobProps = () => ({
     )
 })
 
+const argTypes = {
+    align: {
+        control: {
+            type: "radio",
+            options: ["stretch", "center", "flex-start", "flex-end", "baseline"]
+        }
+    },
+    fontSize: { control: "number" },
+    color: { control: "color" },
+    grow: boolean("grow", false),
+    height: number("height", undefined as any),
+    width: number("width", undefined as any),
+    reverse: boolean("reverse", false),
+    full: boolean("full", false),
+    justify: select(
+        "justify",
+        {
+            "flex-start": "flex-start",
+            center: "center",
+            "flex-end": "flex-end",
+            "space-between": "space-between",
+            "space-around": "space-around",
+            "space-evenly": "space-evenly"
+        },
+        "flex-start"
+    )
+}
+
 const cards = (
     <>
         <Card>One</Card>
@@ -48,7 +75,7 @@ const cards = (
     </>
 )
 
-const C = ({ children }: any) => (
+const Context = ({ children }: any) => (
     <ThemeProvider
         theme={{
             ...defaultTheme,
@@ -65,91 +92,96 @@ const C = ({ children }: any) => (
     </ThemeProvider>
 )
 
-storiesOf("Layout", module)
-    .addDecorator(withKnobs)
-    .add("Row", () => (
-        <C>
-            <Row {...getKnobProps()}>{cards}</Row>
-        </C>
-    ))
-    .add("Column", () => (
-        <C>
+export const SingleRow = (props: any) => (
+    <Context>
+        <Row {...getKnobProps()}>{cards}</Row>
+    </Context>
+)
+
+export const SingleColumn = (props: any) => (
+    <Context>
+        <Column {...getKnobProps()}>{cards}</Column>
+    </Context>
+)
+
+export const RowOfColumns = (props: any) => (
+    <Context>
+        <Row {...getKnobProps()}>
             <Column {...getKnobProps()}>{cards}</Column>
-        </C>
-    ))
-    .add("Row of columns", () => (
-        <C>
-            <Row {...getKnobProps()}>
-                <Column {...getKnobProps()}>{cards}</Column>
-                <Column {...getKnobProps()}>{cards}</Column>
-                <Column {...getKnobProps()}>{cards}</Column>
-            </Row>
-        </C>
-    ))
-    .add("Row of rows", () => (
-        <C>
-            <Row>
-                <Row {...getKnobProps()}>{cards}</Row>
-                <Row {...getKnobProps()}>{cards}</Row>
-                <Row {...getKnobProps()}>{cards}</Row>
-            </Row>
-        </C>
-    ))
-    .add("Column of rows", () => (
-        <C>
-            <Column {...getKnobProps()}>
-                <Row {...getKnobProps()}>{cards}</Row>
-                <Row {...getKnobProps()}>{cards}</Row>
-                <Row {...getKnobProps()}>{cards}</Row>
+            <Column {...getKnobProps()}>{cards}</Column>
+            <Column {...getKnobProps()}>{cards}</Column>
+        </Row>
+    </Context>
+)
+
+export const RowOfRows = (props: any) => (
+    <Context>
+        <Row>
+            <Row {...getKnobProps()}>{cards}</Row>
+            <Row {...getKnobProps()}>{cards}</Row>
+            <Row {...getKnobProps()}>{cards}</Row>
+        </Row>
+    </Context>
+)
+
+export const ColumnofRows = (props: any) => (
+    <Context>
+        <Column {...getKnobProps()}>
+            <Row {...getKnobProps()}>{cards}</Row>
+            <Row {...getKnobProps()}>{cards}</Row>
+            <Row {...getKnobProps()}>{cards}</Row>
+        </Column>
+    </Context>
+)
+
+export const ColumnOfColumns = (props: any) => (
+    <Context>
+        <Column {...getKnobProps()}>
+            <Column {...getKnobProps()}>{cards}</Column>
+            <Column {...getKnobProps()}>{cards}</Column>
+            <Column {...getKnobProps()}>{cards}</Column>
+        </Column>
+    </Context>
+)
+
+export const StandardLayout = (props: any) => (
+    <Context>
+        <Row full>
+            <Column width={100} full>
+                Sidebar
             </Column>
-        </C>
-    ))
-    .add("Column of columns", () => (
-        <C>
-            <Column {...getKnobProps()}>
-                <Column {...getKnobProps()}>{cards}</Column>
-                <Column {...getKnobProps()}>{cards}</Column>
-                <Column {...getKnobProps()}>{cards}</Column>
+            <Column grow full>
+                <Row height={100}>Header</Row>
+                <Row grow>
+                    <Column full align="center" justify="space-between">
+                        <p>Left content</p>
+                        {cards}
+                    </Column>
+                    <Column full align="center">
+                        <p>Right content</p>
+                        <Row grow justify="center" align="center">
+                            <Card>More content</Card>
+                        </Row>
+                    </Column>
+                </Row>
+                <Row height={50}>Footer</Row>
             </Column>
-        </C>
-    ))
-    .add("Page", () => (
-        <C>
-            <Row full>
-                <Column width={100} full>
-                    Sidebar
-                </Column>
-                <Column grow full>
-                    <Row height={100}>Header</Row>
-                    <Row grow>
-                        <Column full align="center" justify="space-between">
-                            <p>Left content</p>
-                            {cards}
-                        </Column>
-                        <Column full align="center">
-                            <p>Right content</p>
-                            <Row grow justify="center" align="center">
-                                <Card>More content</Card>
-                            </Row>
-                        </Column>
-                    </Row>
-                    <Row height={50}>Footer</Row>
-                </Column>
+        </Row>
+    </Context>
+)
+
+export const ResponsiveLayout = (props: any) => (
+    <Context>
+        <Row wrap="wrap">
+            <Row md={4} justify="space-around">
+                <Card>Try</Card>
             </Row>
-        </C>
-    ))
-    .add("Responsive", () => (
-        <C>
-            <Row wrap="wrap">
-                <Row md={4} justify="space-around">
-                    <Card>Try</Card>
-                </Row>
-                <Row md={4} justify="space-around">
-                    <Card>Resizing</Card>
-                </Row>
-                <Row md={4} justify="space-around">
-                    <Card>Your window</Card>
-                </Row>
+            <Row md={4} justify="space-around">
+                <Card>Resizing</Card>
             </Row>
-        </C>
-    ))
+            <Row md={4} justify="space-around">
+                <Card>Your window</Card>
+            </Row>
+        </Row>
+    </Context>
+)
