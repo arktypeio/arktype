cab0a4bd6e54604133d68149331b23597221b97c
David Blass <david@redo.qa>
2021-05-08 02:45:35 -0400

    Start paring down vite config.

--- pkgs/app2/jsrx.ts
+++ pkgs/app2/jsrx.ts
@@ -1,20 +1,89 @@
 import { jsrx, $, shell } from "jsrx"
+import { createServer, build } from "vite"
+import { ChildProcess, isDev, shellAsync } from "@re-do/node-utils"
 
-jsrx({
-    dev: {
-        "buildEnvTypes": $(`ts-node -O '{"module":"commonjs"}' scripts/buildEnvTypes.ts`),
-        "build": $(`ts-node -O '{"module":"commonjs"}' scripts/build.ts`),
-        "precompile": $(`cross-env MODE=production npm run build`),
-        "compile": $(`electron-builder build --config electron-builder.config.js --dir --config.asar=false`),
-        "pretest": $(`cross-env MODE=test npm run build`),
-        "test": $(`node tests/app.spec.js`),
-        "watch": $(`ts-node -O '{"module":"commonjs"}' scripts/watch.ts`),
-        "lint": $(`eslint . --ext js,ts,vue`),
-        "pretypecheck": $(`npm run buildEnvTypes`),
-        "typecheck-main": $(`tsc --noEmit -p packages/main/tsconfig.json`),
-        "typecheck-renderer": $(`tsc --noEmit -p packages/renderer/tsconfig.json`),
-        "typecheck": $(`npm run typecheck-main && npm run typecheck-preload && npm run typecheck-renderer`)
+const electronPath = require("electron")
+const viteConfigs = ["src/main/vite.config.ts", "src/renderer/vite.config.ts"]
+const mode = isDev() ? "development" : "production"
+
+const buildAll = async () => {
+    for (const configFile of viteConfigs) {
+        await build({
+            configFile,
+            mode
+        })
+    }
+}
+
+type GetWatcherArgs = {
+    name: string
+    configFile: string
+    writeBundle: () => void
+}
+
+const getWatcher = ({ name, configFile, writeBundle }: GetWatcherArgs) => {
+    return build({
+        configFile,
+        plugins: [{ name, writeBundle }],
+        mode
+    })
+}
+
+const setupMainPackageWatcher = () => {
+    let mainProcess: ChildProcess
+
+    return getWatcher({
+        name: "reload-app-on-main-package-change",
+        configFile: "src/main/vite.config.ts",
+        writeBundle: async () => {
+            if (mainProcess && !mainProcess.killed) {
+                mainProcess.kill()
+            }
+            mainProcess = shellAsync(`${electronPath} "."`)
+        }
+    })
+}
+
+const watch = async () => {
+    const viteDevServer = await createServer({
+        server: {
+            port: Number(process.env.DEV_SERVER_PORT)
+        },
+        configFile: "src/renderer/vite.config.ts"
+    })
+    await viteDevServer.listen()
+    await setupMainPackageWatcher()
+}
+
+jsrx(
+    {
+        dev: {
+            watch,
+            lint: $(`prettier --write`),
+            typecheck: $(`tsc --noEmit`)
+        },
+        prod: {
+            compile: $(
+                `electron-builder build --config electron-builder.config.js --dir --config.asar=false`
+            )
+        },
+        shared: {
+            build: () => buildAll()
+        }
     },
-    prod: {},
-    shared: {}
-})
+    {
+        excludeOthers: true
+    }
+)
+
+// const setupPreloadPackageWatcher = (viteDevServer: ViteDevServer) => {
+//     return getWatcher({
+//         name: "reload-page-on-preload-package-change",
+//         configFile: "packages/preload/vite.config.js",
+//         writeBundle() {
+//             viteDevServer.ws.send({
+//                 type: "full-reload"
+//             })
+//         }
+//     })
+// }
