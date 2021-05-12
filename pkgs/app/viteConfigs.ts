1c54045e75820b234594f8c644cb25db1d5a046e
David Blass <ssalbdivad@gmail.com>
2021-05-12 16:24:02 -0400

    Migrate @re-do/bundle to vite.

--- pkgs/app/viteConfigs.ts
+++ pkgs/app/viteConfigs.ts
@@ -1,118 +1,58 @@
 import { join } from "path"
-import { builtinModules } from "module"
-import { merge } from "@re-do/utils"
-import { isDev } from "@re-do/node-utils"
-import { UserConfig } from "vite"
-import reactRefreshPlugin from "@vitejs/plugin-react-refresh"
-import commonJsExternalsPlugin from "vite-plugin-commonjs-externals"
+import { getNodeConfig, getWebConfig } from "@re-do/bundle"
 
-const sourceRoot = join(__dirname, "src")
+const pkgRoot = join(__dirname, "src")
 const outRoot = join(__dirname, "dist")
 
-const externals = [
-    "electron",
-    "electron-updater",
-    "electron-redux",
-    "playwright-core",
-    "fs-extra",
-    ...builtinModules
-]
-
-const materialUiResolves = [
-    {
-        find: /^@material-ui\/icons\/(.*)/,
-        replacement: "@material-ui/icons/esm/$1"
-    },
-    {
-        find: /^@material-ui\/core\/(.+)/,
-        replacement: "@material-ui/core/es/$1"
-    },
-    {
-        find: /^@material-ui\/core$/,
-        replacement: "@material-ui/core/es"
-    }
-]
-
 const localResolves = [
-    { find: "main", replacement: join(sourceRoot, "main") },
+    { find: "main", replacement: join(pkgRoot, "main") },
     {
         find: "renderer",
-        replacement: join(sourceRoot, "renderer")
+        replacement: join(pkgRoot, "renderer")
     },
-    { find: "state", replacement: join(sourceRoot, "state") },
+    { find: "state", replacement: join(pkgRoot, "state") },
     {
         find: "observer",
-        replacement: join(sourceRoot, "observer")
+        replacement: join(pkgRoot, "observer")
     }
 ]
-
-const getConfig = (options?: Partial<UserConfig>) =>
-    merge<UserConfig>(
-        {
-            mode: isDev() ? "development" : "production",
-            resolve: {
-                alias: [...materialUiResolves, ...localResolves]
-            },
-            build: {
-                sourcemap: "inline",
-                minify: isDev() ? false : "terser",
-                terserOptions: {
-                    ecma: 2020,
-                    compress: {
-                        passes: 2
-                    },
-                    safari10: false
-                },
-                rollupOptions: {
-                    external: externals,
-                    output: {
-                        entryFileNames: "[name].js"
-                    }
-                },
-                emptyOutDir: true
-            },
-            plugins: [commonJsExternalsPlugin({ externals })]
-        },
-        options
-    )
 
 export type GetConfigArgs = {
     watch?: boolean
 }
 
 export const getMainConfig = ({ watch }: GetConfigArgs = {}) =>
-    getConfig({
-        root: join(sourceRoot, "main"),
-        build: {
-            target: "node14",
-            outDir: join(outRoot, "main"),
-            lib: {
-                entry: join(sourceRoot, "main", "index.ts"),
-                formats: ["cjs"]
-            },
-            watch: watch ? {} : undefined
+    getNodeConfig({
+        srcDir: join(pkgRoot, "main"),
+        outDir: join(outRoot, "main"),
+        watch,
+        options: {
+            resolve: {
+                alias: localResolves
+            }
         }
     })
 
 export const getRendererConfig = ({ watch }: GetConfigArgs = {}) =>
-    getConfig({
-        root: join(sourceRoot, "renderer"),
-        build: {
-            target: "chrome89",
-            polyfillDynamicImport: false,
-            outDir: join(outRoot, "renderer"),
-            watch: watch ? {} : undefined
-        },
-        plugins: [reactRefreshPlugin()]
+    getWebConfig({
+        srcDir: join(pkgRoot, "renderer"),
+        outDir: join(outRoot, "renderer"),
+        watch,
+        options: {
+            resolve: {
+                alias: localResolves
+            }
+        }
     })
 
 export const getObserverConfig = ({ watch }: GetConfigArgs = {}) =>
-    getConfig({
-        root: join(sourceRoot, "observer"),
-        build: {
-            target: "chrome89",
-            polyfillDynamicImport: false,
-            outDir: join(outRoot, "observer"),
-            watch: watch ? {} : undefined
+    getWebConfig({
+        srcDir: join(pkgRoot, "observer"),
+        outDir: join(outRoot, "observer"),
+        watch,
+        options: {
+            resolve: {
+                alias: localResolves
+            }
         }
     })
--- pkgs/bundle/src/index.ts
+++ pkgs/bundle/src/index.ts
@@ -1,244 +1,106 @@
-import { resolve } from "path"
-import { spawn } from "child_process"
-import HtmlWebpackPlugin from "html-webpack-plugin"
-import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin"
-import isWsl from "is-wsl"
-import { merge } from "webpack-merge"
-import {
-    Configuration,
-    IgnorePlugin,
-    HotModuleReplacementPlugin,
-    NoEmitOnErrorsPlugin,
-    ProvidePlugin
-} from "webpack"
-import { listify } from "@re-do/utils"
-import { getMode, isDev } from "@re-do/node-utils"
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin"
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer"
+import { join } from "path"
+import { builtinModules } from "module"
+import { merge } from "@re-do/utils"
+import { isDev } from "@re-do/node-utils"
+import { UserConfig, Terser } from "vite"
+import reactRefreshPlugin from "@vitejs/plugin-react-refresh"
+import commonJsExternalsPlugin from "vite-plugin-commonjs-externals"
 
-export type ConfigArgs = {
-    entries: string[]
-    tsconfig: string
-    analyzeBundle: boolean
-}
+const externals = [
+    "electron",
+    "electron-updater",
+    "electron-redux",
+    "playwright-core",
+    "fs-extra",
+    ...builtinModules
+]
 
-const getPlugins = (tsconfig: string, analyze?: boolean) => {
-    const plugins = [
-        new ForkTsCheckerWebpackPlugin({
-            typescript: { configFile: tsconfig }
-        })
-    ]
-    if (analyze) {
-        plugins.push(new BundleAnalyzerPlugin() as any)
+const materialUiResolves = [
+    {
+        find: /^@material-ui\/icons\/(.*)/,
+        replacement: "@material-ui/icons/esm/$1"
+    },
+    {
+        find: /^@material-ui\/core\/(.+)/,
+        replacement: "@material-ui/core/es/$1"
+    },
+    {
+        find: /^@material-ui\/core$/,
+        replacement: "@material-ui/core/es"
     }
-    return plugins
+]
+
+const terserOptions: Terser.MinifyOptions = {
+    ecma: 2020,
+    compress: {
+        passes: 2
+    },
+    safari10: false
 }
 
-const getCommonConfig = ({
-    entries,
-    tsconfig,
-    analyzeBundle
-}: ConfigArgs): Configuration => ({
-    mode: getMode(),
-    entry: entries,
-    devtool: isDev() ? "inline-source-map" : "source-map",
-    context: resolve(__dirname, ".."),
-    performance: {
-        hints: isDev() && "warning"
-    },
-    node: {
-        __dirname: false,
-        __filename: false
-    },
+const getBaseConfig = (): UserConfig => ({
+    mode: isDev() ? "development" : "production",
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx", ".mjs", ".json"],
-        plugins: [new TsconfigPathsPlugin() as any]
+        alias: [...materialUiResolves]
     },
-    module: {
-        rules: [
-            {
-                test: /\.(j|t)sx?$/,
-                loader: "babel-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.m?js$/,
-                resolve: {
-                    fullySpecified: false
-                }
-            },
-            {
-                test: /\.(graphql|gql)$/,
-                exclude: /node_modules/,
-                loader: "graphql-tag/loader"
+    build: {
+        sourcemap: "inline",
+        minify: isDev() ? false : "terser",
+        terserOptions,
+        rollupOptions: {
+            external: externals,
+            output: {
+                entryFileNames: "[name].js"
             }
-        ]
+        },
+        emptyOutDir: true
     },
-    plugins: getPlugins(tsconfig, analyzeBundle)
+    plugins: [commonJsExternalsPlugin({ externals })]
 })
 
-const getFrontendConfig = (args: ConfigArgs): Configuration =>
-    merge(getCommonConfig(args), {
-        module: {
-            rules: [
-                {
-                    test: /\.(jpg|png|ico|icns|woff|woff2)$/,
-                    loader: "url-loader"
-                },
-                {
-                    test: /\.svg$/,
-                    use: [
-                        {
-                            loader: "babel-loader"
-                        },
-                        {
-                            loader: "react-svg-loader",
-                            options: {
-                                jsx: true
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.css$/,
-                    use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-                },
-                {
-                    test: /node_modules[\/\\](iconv-lite)[\/\\].+/,
-                    resolve: {
-                        aliasFields: ["main"]
-                    }
-                }
-            ]
-        },
-        output: {
-            publicPath: "/"
-        },
-        resolve: {
-            alias: {
-                "react-hot-loader": resolve(
-                    __dirname,
-                    "..",
-                    "node_modules",
-                    "react-hot-loader"
-                )
-            }
-        },
-        plugins: [
-            new IgnorePlugin({
-                checkResource: (name) =>
-                    name.includes("iconv-loader") || name.endsWith(".exe")
-            }),
-            new HtmlWebpackPlugin({
-                template: resolve(__dirname, "template.html")
-            })
-        ]
-    })
-
-const getWebConfig = (args: ConfigArgs): Configuration =>
-    merge(getFrontendConfig(args), {
-        plugins: [
-            new ProvidePlugin({
-                process: "process/browser",
-                Buffer: ["buffer", "Buffer"]
-            })
-        ]
-    })
-
-const getMainConfig = (args: ConfigArgs): Configuration =>
-    merge(getCommonConfig(args), {
-        target: "electron-main",
-        output: {
-            filename: "main.js"
-        }
-    })
-
-const getRendererConfig = (args: ConfigArgs): Configuration =>
-    merge(getFrontendConfig(args), {
-        target: "electron-renderer",
-        output: {
-            filename: "renderer.js"
-        },
-        resolve: {
-            /*
-            Override default value ["browser"] since we have enabled node integration
-            And have access to more than we would in a basic web environment.
-            In particular, this allows us to import puppeteer, which specifies a
-            "browser" field in its package.json that breaks our ability to import it.
-            */
-            aliasFields: []
-        },
-        devServer: {
-            after() {
-                spawn("npm", ["run", "electron"], {
-                    shell: true,
-                    env: process.env,
-                    stdio: "inherit"
-                })
-                    .on("close", (code) => process.exit(code ?? undefined))
-                    .on("error", (spawnError) => console.error(spawnError))
-            }
-        }
-    } as Configuration)
-
-const getDevServerConfig = (customConfig?: object): Configuration =>
-    ({
-        resolve: {
-            alias: {
-                "react-dom": resolve(
-                    __dirname,
-                    "..",
-                    "node_modules",
-                    "@hot-loader",
-                    "react-dom"
-                )
-            }
-        },
-        entry: [
-            "react-hot-loader/patch",
-            "webpack-dev-server/client?http://localhost:8080",
-            "webpack/hot/only-dev-server"
-        ],
-        plugins: [new HotModuleReplacementPlugin(), new NoEmitOnErrorsPlugin()],
-        devServer: {
-            historyApiFallback: true,
-            hot: true,
-            writeToDisk: true,
-            host: isWsl ? "0.0.0.0" : undefined,
-            useLocalIp: isWsl,
-            ...customConfig
-        }
-    } as Configuration)
-
-const baseOptions = {
-    common: getCommonConfig,
-    web: getWebConfig,
-    renderer: getRendererConfig,
-    main: getMainConfig
+export type GetConfigArgs = {
+    srcDir: string
+    outDir: string
+    watch?: boolean
+    options?: UserConfig
 }
 
-export type BaseName = keyof typeof baseOptions
-
-export type BaseConfigOptions = {
-    base: BaseName
-    entry: string | string[]
-    tsconfig: string
-    devServer?: object
-    analyzeBundle?: boolean
+export const getNodeConfig = ({
+    srcDir,
+    outDir,
+    watch,
+    options = {}
+}: GetConfigArgs) => {
+    const baseNodeConfig = merge<UserConfig>(getBaseConfig(), {
+        root: srcDir,
+        build: {
+            target: "node14",
+            outDir,
+            lib: {
+                entry: join(srcDir, "index.ts"),
+                formats: ["cjs"]
+            },
+            watch: watch ? {} : undefined
+        }
+    })
+    return merge<UserConfig>(baseNodeConfig, options)
 }
 
-export const makeConfig = (
-    {
-        base,
-        entry,
-        tsconfig,
-        devServer,
-        analyzeBundle = false
-    }: BaseConfigOptions,
-    merged: Partial<Configuration>[] = []
-) =>
-    merge(
-        baseOptions[base]({ entries: listify(entry), tsconfig, analyzeBundle }),
-        devServer ? getDevServerConfig(devServer) : {},
-        ...merged
-    )
+export const getWebConfig = ({
+    srcDir,
+    outDir,
+    watch,
+    options = {}
+}: GetConfigArgs) => {
+    const baseWebConfig = merge<UserConfig>(getBaseConfig(), {
+        root: srcDir,
+        build: {
+            target: "chrome89",
+            polyfillDynamicImport: false,
+            outDir,
+            watch: watch ? {} : undefined
+        },
+        plugins: [reactRefreshPlugin()]
+    })
+    return merge<UserConfig>(baseWebConfig, options)
+}
