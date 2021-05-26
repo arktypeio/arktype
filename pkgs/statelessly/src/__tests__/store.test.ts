4743cfcb25143971737a26a93093e27692806394
David Blass <ssalbdivad@gmail.com>
2021-05-26 16:59:33 -0400

    Update all statelessly tests to action model.

--- pkgs/statelessly/src/__tests__/store.test.ts
+++ pkgs/statelessly/src/__tests__/store.test.ts
@@ -29,7 +29,23 @@ const initialRoot: Root = Object.freeze({
     d: [initialA, initialA]
 })
 
-// let store = createStore({ initial: initialRoot, actions: {} })
+const getStore = () =>
+    createStore(initialRoot, {
+        enableB: { b: true },
+        nameC: (name: string) => ({
+            c: name
+        }),
+        addOneToEndOfList: {
+            a: { b: { a: (_) => [..._, 1] } }
+        },
+        addNumberToEndOfList: (value: number) => ({
+            a: { b: { a: (_) => [..._, value] } }
+        }),
+        appendObjectToArray: { d: (value) => value.concat(initialA) },
+        emptyDArray: { d: [] }
+    })
+
+let store = getStore()
 
 // const cHandler = jest.fn()
 // const bing = jest.fn()
@@ -52,99 +68,107 @@ const initialRoot: Root = Object.freeze({
 //     actions: {}
 // })
 
-// describe("queries", () => {
-//     beforeAll(() => {
-//         store = createStore({ initial: initialRoot })
-//     })
-//     test("handle shallow", () => {
-//         expect(store.query({ b: true })).toStrictEqual({ b: false })
-//     })
-//     test("handle deep", () => {
-//         expect(store.query({ a: true })).toStrictEqual({ a: initialA })
-//     })
-//     test("handle object arrays", () => {
-//         expect(store.query({ d: true })).toStrictEqual({
-//             d: [initialA, initialA]
-//         })
-//     })
-//     test("handle filtered object within array", () => {
-//         expect(store.query({ d: { a: true } })).toStrictEqual({
-//             d: [{ a: 0 }, { a: 0 }]
-//         })
-//     })
-//     test("don't include extraneous keys", () => {
-//         expect(store.query({ a: { a: true } })).toStrictEqual({
-//             a: { a: initialA.a }
-//         })
-//     })
-// })
+describe("queries", () => {
+    beforeAll(() => {
+        store = getStore()
+    })
+    test("handle shallow", () => {
+        expect(store.query({ b: true })).toStrictEqual({ b: false })
+    })
+    test("handle deep", () => {
+        expect(store.query({ a: true })).toStrictEqual({ a: initialA })
+    })
+    test("handle object arrays", () => {
+        expect(store.query({ d: true })).toStrictEqual({
+            d: [initialA, initialA]
+        })
+    })
+    test("handle filtered object within array", () => {
+        expect(store.query({ d: { a: true } })).toStrictEqual({
+            d: [{ a: 0 }, { a: 0 }]
+        })
+    })
+    test("don't include extraneous keys", () => {
+        expect(store.query({ a: { a: true } })).toStrictEqual({
+            a: { a: initialA.a }
+        })
+    })
+})
 
-// describe("gets", () => {
-//     beforeAll(() => {
-//         store = createStore({ initial: initialRoot })
-//     })
-//     test("shallow", () => {
-//         expect(store.get("c")).toBe("")
-//     })
-//     test("nested", () => {
-//         expect(store.get("a/b/a")).toStrictEqual([0])
-//     })
-//     test("from array", () => {
-//         expect(store.get("d/0/a")).toBe(0)
-//     })
-// })
+describe("gets", () => {
+    beforeAll(() => {
+        store = getStore()
+    })
+    test("shallow", () => {
+        expect(store.get("c")).toBe("")
+    })
+    test("nested", () => {
+        expect(store.get("a/b/a")).toStrictEqual([0])
+    })
+    test("from array", () => {
+        expect(store.get("d/0/a")).toBe(0)
+    })
+})
 
-// describe("updates", () => {
-//     beforeEach(() => {
-//         store = createStore({ initial: initialRoot })
-//     })
-//     test("handles shallow", () => {
-//         store.update({ c: (value) => value + "suffix" })
-//         expect(store.getState()).toStrictEqual({
-//             ...initialRoot,
-//             c: initialRoot.c + "suffix"
-//         })
-//     })
-//     test("handles deep", () => {
-//         store.update({
-//             a: { b: { a: (value) => value.concat([1]) } }
-//         })
-//         expect(store.getState()).toStrictEqual({
-//             ...initialRoot,
-//             a: {
-//                 ...initialRoot.a,
-//                 b: {
-//                     ...initialRoot.a.b,
-//                     a: initialRoot.a.b.a.concat([1])
-//                 }
-//             }
-//         })
-//     })
-//     test("handles object arrays", () => {
-//         store.update({ d: (value) => value.concat(initialA) })
-//         expect(store.getState()).toStrictEqual({
-//             ...initialRoot,
-//             d: [initialA, initialA, initialA]
-//         })
-//     })
-//     test("sets array value", () => {
-//         store.update({ d: [] })
-//         expect(store.getState()).toStrictEqual({
-//             ...initialRoot,
-//             d: []
-//         })
-//     })
-
-//     test("doesn't update extraneous keys", () => {
-//         store.update({
-//             a: { b: { a: (value) => value.concat([1]) } }
-//         })
-//         expect(store.getState()).toStrictEqual({
-//             ...initialRoot,
-//             a: { ...initialA, b: { ...initialA.b, a: [0, 1] } }
-//         })
-//     })
-// })
+describe("actions", () => {
+    beforeEach(() => {
+        store = getStore()
+    })
+    test("shallow set value", () => {
+        store.enableB()
+        expect(store.getState()).toStrictEqual({
+            ...initialRoot,
+            b: true
+        })
+    })
+    test("shallow set function", () => {
+        store.nameC("redo")
+        expect(store.getState()).toStrictEqual({
+            ...initialRoot,
+            c: "redo"
+        })
+    })
+    test("deep set value", () => {
+        store.addOneToEndOfList()
+        expect(store.getState()).toStrictEqual({
+            ...initialRoot,
+            a: {
+                ...initialRoot.a,
+                b: {
+                    ...initialRoot.a.b,
+                    a: initialRoot.a.b.a.concat([1])
+                }
+            }
+        })
+    })
+    test("deep set function", () => {
+        store.addNumberToEndOfList(5)
+        expect(store.getState()).toStrictEqual({
+            ...initialRoot,
+            a: {
+                ...initialRoot.a,
+                b: {
+                    ...initialRoot.a.b,
+                    a: initialRoot.a.b.a.concat([5])
+                }
+            }
+        })
+    })
+    test("handles object arrays", () => {
+        store.appendObjectToArray()
+        expect(store.getState()).toStrictEqual({
+            ...initialRoot,
+            d: [initialA, initialA, initialA]
+        })
+    })
+    test("sets array value", () => {
+        store.emptyDArray()
+        expect(store.getState()).toStrictEqual({
+            ...initialRoot,
+            d: []
+        })
+    })
+})
 
 // describe("side effects", () => {
 //     beforeEach(() => {
@@ -176,18 +200,3 @@ const initialRoot: Root = Object.freeze({
 //         expect(functionalHandler).toHaveBeenCalled()
 //     })
 // })
-
-let store = createStore(initialRoot, { toggleB: { b: (_) => !_ } })
-
-describe("actions", () => {
-    beforeEach(() => {
-        store = createStore(initialRoot, { toggleB: { b: (_) => !_ } })
-    })
-    test("simple update", () => {
-        store.toggleB()
-        expect(store.getState()).toStrictEqual({
-            ...initialRoot,
-            b: !initialRoot.b
-        })
-    })
-})
