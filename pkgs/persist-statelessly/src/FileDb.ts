b4703f7775353da501e7a54b46425fd6458f34d2
David Blass <ssalbdivad@gmail.com>
2021-06-17 13:21:23 -0400

    Start working on db storage model.

--- pkgs/persist-statelessly/src/FileDb.ts
+++ pkgs/persist-statelessly/src/FileDb.ts
@@ -1,7 +1,7 @@
-import { transform, Unlisted } from "@re-do/utils"
+import { deepMap, transform, Unlisted } from "@re-do/utils"
 import { FileStore, FileStoreOptions } from "./FileStore"
 
-export type Model = Record<string, object[]>
+export type Model = Record<string, Record<string, any>[]>
 
 export type FileDb<T extends Model> = {
     [K in keyof T]: Interactions<Unlisted<T[K]>>
@@ -20,13 +20,31 @@ export const createFileDb = <T extends Model>({
     return transform(fallback, ([k, v]) => [
         k,
         {
-            persist: (o: object) =>
-                store.update({
-                    [k]: (_: object[]) => _.concat(o)
-                } as any),
+            persist: (o: any) => persist(k, o, store),
             retrieve: () => store.get(k as any)
         }
     ]) as any
+}
+
+const persist = <V extends Record<string, any>>(
+    typeName: string,
+    value: V,
+    store: FileStore<any, {}>
+) => {
+    const id = 0
+    const storedData = transform(value, ([k, v]) => {
+        let storedValue = v
+        if (v && typeof v === "object") {
+            storedValue = Array.isArray(v)
+                ? v.map((_) => persist(k, _, store))
+                : persist(k, v, store)
+        }
+        return [k, storedValue]
+    })
+    store.update({
+        [typeName]: (_: any[]) => _.concat({ id, ...storedData })
+    } as any)
+    return id
 }
 
 type Persisted<O extends object> = {
--- pkgs/persist-statelessly/src/__tests__/FileDb.test.ts
+++ pkgs/persist-statelessly/src/__tests__/FileDb.test.ts
@@ -1,28 +1,68 @@
+import { rmSync } from "fs"
 import { join } from "path"
 import { createFileDb, FileDb } from ".."
 
-type Root = {
-    a: {
-        enabled: boolean
-    }[]
+const path = join(__dirname, "store.json")
+
+type User = {
+    name: string
+    friends: User[]
+    groups: Group[]
 }
 
-const fallback: Root = {
-    a: []
+type Group = {
+    name: string
+    description: string
+    users: User[]
 }
+
+const fallback = {
+    users: [] as User[],
+    groups: [] as Group[]
+}
+
+const shallowUserData: User = {
+    name: "Mister Shallow",
+    friends: [],
+    groups: []
+}
+
+const groupData: Group = {
+    name: "Adventurer's Club",
+    description: "The most daring individuals in the world",
+    users: []
+}
+
+const deepUserData: User = {
+    name: "Mister Deep",
+    friends: [shallowUserData],
+    groups: []
+}
+
+type Root = typeof fallback
 
 let db: FileDb<Root>
 
 describe("persist", () => {
     beforeEach(() => {
+        rmSync(path)
         db = createFileDb({
             fallback,
-            path: join(__dirname, "store.json"),
+            path,
             bidirectional: false
         })
     })
-    test("works", () => {
-        db.a.persist({ enabled: true })
-        expect(db.a.retrieve()).toStrictEqual([{ enabled: true }])
+    test("shallow", () => {
+        db.users.persist(shallowUserData)
+        expect(db.users.retrieve()).toStrictEqual([
+            { ...shallowUserData, id: 0 }
+        ])
+    })
+    test("deep", () => {
+        db.users.persist(deepUserData)
+        expect(db.users.retrieve()).toStrictEqual([
+            { ...shallowUserData, id: 0 },
+            { ...deepUserData, friends: [0], id: 0 }
+        ])
     })
 })
