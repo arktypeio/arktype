9963a0f034617622c8ad65f9f456a3c2d7ec94cb
David Blass <ssalbdivad@users.noreply.github.com>
2021-07-12 23:37:24 -0400

    Fix fast-glob build issue, tests. (#265)

--- pkgs/test/src/__tests__/install.test.ts
+++ pkgs/test/src/__tests__/install.test.ts
@@ -8,28 +8,46 @@ import { version, install, getExecutablePath } from "../install"
 
 const REDO_DIR = join(__dirname, ".redo")
 const VERSION_DIR = join(REDO_DIR, version)
+const EXECUTABLE_PATH = getExecutablePath(VERSION_DIR)
+
 let redoMainProcess: ChildProcess | undefined
 
+const getTestRendererProcesses = async () => {
+    const allProcesses = await psList()
+    return allProcesses.filter(
+        ({ cmd }) =>
+            cmd &&
+            cmd.search(EXECUTABLE_PATH) !== -1 &&
+            cmd.search("renderer") !== -1
+    )
+}
+
+const killOldTestProcesses = async () => {
+    const oldProcesses = await getTestRendererProcesses()
+    oldProcesses.forEach((p) => process.kill(p.pid))
+}
+
+const deleteTestDir = () => rmSync(REDO_DIR, { recursive: true, force: true })
+
 describe("installation", () => {
-    afterEach(() => {
+    beforeEach(async () => {
+        await killOldTestProcesses()
+        deleteTestDir()
+    })
+    afterEach(async () => {
         if (redoMainProcess && !redoMainProcess.killed) {
             treeKill(redoMainProcess.pid)
         }
-        rmSync(REDO_DIR, { recursive: true, force: true })
+        await killOldTestProcesses()
+        deleteTestDir()
     })
     test("works", async () => {
         await install(VERSION_DIR)
-        redoMainProcess = shellAsync(getExecutablePath(VERSION_DIR))
+        redoMainProcess = shellAsync(EXECUTABLE_PATH, { stdio: "ignore" })
         let redoRendererProcesses: ProcessDescriptor[] = []
         await waitUntil(
             async () => {
-                const allProcesses = await psList()
-                redoRendererProcesses = allProcesses.filter(
-                    ({ cmd }) =>
-                        cmd &&
-                        cmd.search("redo") !== -1 &&
-                        cmd.search("--type=renderer") !== -1
-                )
+                redoRendererProcesses = await getTestRendererProcesses()
                 return !!redoRendererProcesses.length
             },
             { timeout: 60000 }
