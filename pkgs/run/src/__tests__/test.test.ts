5f6a790677e31965d3618a28f3c0bb23da9e6719
David Blass <ssalbdivad@gmail.com>
2021-07-12 22:58:10 -0400

    Fix fast-glob build issue, tests.

--- pkgs/run/src/__tests__/test.test.ts
+++ pkgs/run/src/__tests__/test.test.ts
@@ -1,31 +1,25 @@
 import { test as redoTest, Step, launch, browserHandlers } from ".."
 
 describe("test", () => {
-    const signUpSteps: Step[] = [
+    const readDocumentationSteps: Step[] = [
         { kind: "go", url: "https://redo.qa" },
         { kind: "click", element: { selector: "'Get Started'" } },
         {
-            kind: "set",
+            kind: "assertText",
             element: {
-                selector: "[name='email']"
+                selector: "pre:nth-child(3) > .hljs"
             },
-            value: "david@redo.qa"
-        },
-        {
-            kind: "click",
-            element: {
-                selector: "'Keep me posted!'"
-            }
+            value: "npm install @re-do/test"
         }
     ]
     test("works with default options", async () => {
-        await redoTest(signUpSteps)
+        await redoTest(readDocumentationSteps)
     }, 20000)
     test("works on firefox", async () => {
-        await redoTest(signUpSteps, { browser: "firefox" })
+        await redoTest(readDocumentationSteps, { browser: "firefox" })
     }, 20000)
     test("works on safari", async () => {
-        await redoTest(signUpSteps, { browser: "safari" })
+        await redoTest(readDocumentationSteps, { browser: "safari" })
     }, 20000)
     test("chromium can be positioned and sized", async () => {
         const { browser } = await launch("chrome", {
