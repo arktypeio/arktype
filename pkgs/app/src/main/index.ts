90bc2b9b4bb7855318271975d8ab74af5ca38b64
ShawnMorreau <Shawn.morreau@hotmail.com>
2021-07-14 15:39:49 -0400

    remove import

--- pkgs/app/src/main/index.ts
+++ pkgs/app/src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron"
+import { app } from "electron"
 import { installDevTools } from "./installDevTools"
 import { createMainWindow, createBuilderWindow } from "./windows"
 
