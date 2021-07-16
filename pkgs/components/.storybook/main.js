07240b92e77b323139f6c3436ae76e9d6ae700db
David Blass <david@redo.qa>
2021-07-15 22:04:46 -0400

    Fix storybok for components.

--- pkgs/components/.storybook/main.js
+++ pkgs/components/.storybook/main.js
@@ -6,7 +6,18 @@ module.exports = {
         const { dirname } = require("path")
         // https://github.com/eirslett/storybook-builder-vite/issues/55
         config.root = dirname(require.resolve("storybook-builder-vite"))
-        config.resolve.alias = [{find: /^(\.{1,2}\/.*)\.js$/, replacement: "$1"}]
+        config.resolve.alias = [{find: /^(\.{1,2}\/.*)\.js$/, replacement: "$1"},  {
+        find: /^@material-ui\/icons\/(.*)/,
+        replacement: "@material-ui/icons/esm/$1"
+    },
+    {
+        find: /^@material-ui\/core\/(.+)/,
+        replacement: "@material-ui/core/es/$1"
+    },
+    {
+        find: /^@material-ui\/core$/,
+        replacement: "@material-ui/core/es"
+    }]
         config.server.fsServe = undefined
         return config
     }
