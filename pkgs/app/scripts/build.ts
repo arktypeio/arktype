a532ccf94957a4168749c219ff6a338cc4470e6d
David Blass <ssalbdivad@gmail.com>
2021-07-29 22:05:45 -0400

    Update app build configs.

---
+++ pkgs/app/scripts/build.ts
@@ -0,0 +1,116 @@
+import treeKill from "tree-kill"
+import { join } from "path"
+import { writeFileSync } from "fs"
+import { build } from "vite"
+import { getNodeConfig, getWebConfig } from "@re-do/configs"
+import { ChildProcess, shellAsync } from "@re-do/node-utils"
+import { startElectronCmd, killExisting, srcDir, distDir } from "./common"
+
+let mainProcess: ChildProcess | undefined
+
+export const restartMain = (startIfNotRunning: boolean) => {
+    if (mainProcess && !mainProcess.killed) {
+        treeKill(mainProcess.pid, () => {
+            killExisting()
+            mainProcess = shellAsync(startElectronCmd)
+        })
+    } else if (startIfNotRunning) {
+        mainProcess = shellAsync(startElectronCmd)
+    }
+}
+
+const localResolves = [
+    { find: "main", replacement: join(srcDir, "main") },
+    {
+        find: "renderer",
+        replacement: join(srcDir, "renderer")
+    },
+    {
+        find: "observer",
+        replacement: join(srcDir, "observer")
+    },
+    { find: "common", replacement: join(srcDir, "common") },
+    {
+        find: "assets",
+        replacement: join(srcDir, "assets")
+    }
+]
+
+export type GetConfigArgs = {
+    watch?: boolean
+}
+
+const watchMainPlugin = {
+    name: "watchMain",
+    writeBundle: () => restartMain(true)
+}
+
+const watchObserverPlugin = {
+    name: "observer-watcher",
+    writeBundle: () => restartMain(false)
+}
+
+const addPreloadScriptPlugin = {
+    name: "add-renderer-preload-script",
+    writeBundle: () =>
+        writeFileSync(
+            join(distDir, "main", "preload.js"),
+            `require("electron-redux/preload")`
+        )
+}
+
+export const getMainConfig = ({ watch }: GetConfigArgs = {}) =>
+    getNodeConfig({
+        srcDir: join(srcDir, "main"),
+        outDir: join(distDir, "main"),
+        watch,
+        options: {
+            resolve: {
+                alias: localResolves
+            },
+            plugins: watch
+                ? [addPreloadScriptPlugin, watchMainPlugin]
+                : [addPreloadScriptPlugin]
+        }
+    })
+
+export const getRendererConfig = ({ watch }: GetConfigArgs = {}) =>
+    getWebConfig({
+        srcDir: join(srcDir, "renderer"),
+        outDir: join(distDir, "renderer"),
+        watch,
+        options: {
+            base: "./",
+            resolve: {
+                alias: localResolves
+            }
+        }
+    })
+
+export const getObserverConfig = ({ watch }: GetConfigArgs = {}) =>
+    getNodeConfig({
+        srcDir: join(srcDir, "observer"),
+        outDir: join(distDir, "observer"),
+        watch,
+        formats: ["es"],
+        options: {
+            build: {
+                target: "esnext"
+            },
+            resolve: {
+                alias: localResolves
+            },
+            plugins: watch ? [watchObserverPlugin] : []
+        }
+    })
+
+export const buildMain = async () => build(getMainConfig())
+
+export const buildRenderer = async () => build(getRendererConfig())
+
+export const buildObserver = async () => build(getObserverConfig())
+
+export const buildAll = async () => {
+    await Promise.all([buildRenderer(), buildObserver()])
+    return await buildMain()
+}
