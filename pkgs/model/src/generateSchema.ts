9512ba327d4b343b17c1fe53a2a3227f71d142e2
David Blass <ssalbdivad@gmail.com>
2021-08-02 16:25:27 -0400

    Change generateSchema dirName logic for windows build.

--- pkgs/model/src/generateSchema.ts
+++ pkgs/model/src/generateSchema.ts
@@ -1,17 +1,8 @@
 import gqlize from "gqlize"
 import { writeFileSync } from "fs"
-import { join, dirname } from "path"
-
-let projectDir: string
-
-try {
-    projectDir = join(__dirname, "..")
-} catch {
-    // @ts-ignore
-    projectDir = join(dirname(new URL(import.meta.url).pathname), "..")
-}
+import { dirName } from "@re-do/node-utils"
 
 writeFileSync(
-    join(projectDir, "queries.gql"),
-    gqlize.gqlize({ schema: join(projectDir, "schema.gql") })
+    dirName("..", "queries.gql"),
+    gqlize.gqlize({ schema: dirName("..", "schema.gql") })
 )
