72b48faa388538d305ba950ca28db8510d87fd6e
David Blass <david@redo.qa>
2021-12-28 20:40:14 -0500

    Begin moving READMEs over to docs website.

--- pkgs/docs/src/pages/index.tsx
+++ pkgs/docs/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
                         className="button button--secondary button--lg"
                         to="/docs/intro"
                     >
-                        Docusaurus Tutorial - 5min ⏱️
+                        Tutorial - 5min ⏱️
                     </Link>
                 </div>
             </div>
