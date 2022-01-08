dca1f213be0c52f485b67222e59a0aefebce851d
David Blass <david@redo.qa>
2022-01-07 19:41:15 -0500

    Add more test.

---
+++ pkgs/model/src/definitions/str/expression/__tests__/or.assert.ts
@@ -0,0 +1,9 @@
+import { assert } from "@re-/assert"
+import { define } from "./internal.js"
+
+export const testList = () => {
+    describe("type", () => {
+        test("is okay", () => {})
+        describe("errors", () => {})
+    })
+}
