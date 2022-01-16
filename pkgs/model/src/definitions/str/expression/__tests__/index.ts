f799c9c9fb83f8c8c5b33ec2c4fd65857a9b9977
David Blass <david@redo.qa>
2022-01-16 18:59:57 -0500

    Continue building tests.

--- pkgs/model/src/definitions/str/expression/__tests__/index.ts
+++ pkgs/model/src/definitions/str/expression/__tests__/index.ts
@@ -1,7 +1,9 @@
 import { testArrowFunction } from "./arrowFunction.assert.js"
 import { testList } from "./list.assert.js"
+import { testUnion } from "./union.assert.js"
 
 describe("expression", () => {
     describe("arrow function", testArrowFunction)
     describe("list", testList)
+    describe("union", testUnion)
 })
