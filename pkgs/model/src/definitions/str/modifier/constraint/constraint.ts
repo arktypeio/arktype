9d189a3ce30ee217a3970b5413f0133f0e8b3045
David Blass <david@redo.qa>
2022-01-24 20:18:40 -0500

    Begin building constraints.

--- pkgs/model/src/definitions/str/modifier/constraint/constraint.ts
+++ pkgs/model/src/definitions/str/modifier/constraint/constraint.ts
@@ -3,8 +3,10 @@ import { Fragment } from "../../fragment.js"
 import {
     duplicateModifierError,
     typeDefProxy,
-    createParser
+    createParser,
+    ExtractableDefinition
 } from "../internal.js"
+import { isInteger } from "@re-/tools"
 
 // Any
 // Not (!)
@@ -41,13 +43,36 @@ export namespace Constraint {
                 }
                 return {
                     typeDef: Fragment.parse(parts[0], ctx),
-                    constraints: parts[1]
+                    constraint: parts[1]
                 }
             }
         },
+        // define("number:5<n<10,int")
+        // ["5<n<10", "int"]
+        // 4.3
+        //
         {
             matches: (def) => def.includes(":"),
             allows: ({ def, components, ctx }, valueType, opts) => {
+                const parts = components.constraint.split(",")
+                const numericKeywords: Record<string, (n: number) => string> = {
+                    int: (n) => (isInteger(n) ? "" : `${n} must be an integer.`)
+                }
+                const assertNumber = (value: ExtractableDefinition) => {
+                    if (typeof value !== "number") {
+                        throw new Error(
+                            `Constraint ${components.constraint} value must be a number.`
+                        )
+                    }
+                    return value
+                }
+                parts.reduce((message, part) => {
+                    if (part in numericKeywords) {
+                        const value = assertNumber(valueType)
+                        return message + numericKeywords[part](value)
+                    }
+                    return message
+                }, "")
                 return {}
             },
             generate: ({ components }, opts) =>
