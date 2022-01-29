02084e2ae800a4dda9e6959f32fb5375b4f9903b
David Blass <david@redo.qa>
2022-01-28 19:56:26 -0500

    Continue migrating tests.

--- pkgs/model/src/definitions/str/builtin/__tests__/numberLiteral.assert.ts
+++ pkgs/model/src/definitions/str/builtin/__tests__/numberLiteral.assert.ts
@@ -6,13 +6,29 @@ export const testNumberLiteral = () => {
         // As of TS 4.5, I don't think it's possible to parse a number literal from a string type
         // Runtime functionality like "getDefault" and "validate" will still use the more specific
         // value, but the TS type is inferred as "number"
-        test("integer", () => {
+        test("whole", () => {
             assert(define("4").type).typed as number
         })
         test("decimal", () => {
             assert(define("1.234").type).typed as number
         })
-        describe("errors", () => {})
+        test("negative", () => {
+            assert(define("-5.7").type).typed as number
+        })
+        describe("errors", () => {
+            test("multiple decimals", () => {
+                // @ts-expect-error
+                assert(() => define("127.0.0.1")).throwsAndHasTypeError(
+                    "Unable to determine the type of '127.0.0.1'."
+                )
+            })
+            test("with alpha", () => {
+                // @ts-expect-error
+                assert(() => define("13three7")).throwsAndHasTypeError(
+                    "Unable to determine the type of '13three7'."
+                )
+            })
+        })
     })
     describe("validation", () => {})
     describe("generation", () => {})
