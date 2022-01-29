032d2c3e19bdfbbde1b214ec2f3ed2f218c26a93
David Blass <david@redo.qa>
2022-01-28 21:15:36 -0500

    Add tests for number and bigint literal.

--- pkgs/model/src/definitions/str/builtin/__tests__/numberLiteral.assert.ts
+++ pkgs/model/src/definitions/str/builtin/__tests__/numberLiteral.assert.ts
@@ -2,10 +2,10 @@ import { assert } from "@re-/assert"
 import { define } from "@re-/model"
 
 export const testNumberLiteral = () => {
+    // As of TS 4.5, I don't think it's possible to parse a number literal from a string type
+    // Runtime functionality like "getDefault" and "validate" will still use the more specific
+    // value, but the TS type is inferred as "number"
     describe("type", () => {
-        // As of TS 4.5, I don't think it's possible to parse a number literal from a string type
-        // Runtime functionality like "getDefault" and "validate" will still use the more specific
-        // value, but the TS type is inferred as "number"
         test("whole", () => {
             assert(define("4").type).typed as number
         })
@@ -30,6 +30,42 @@ export const testNumberLiteral = () => {
             })
         })
     })
-    describe("validation", () => {})
-    describe("generation", () => {})
+    describe("validation", () => {
+        test("whole", () => {
+            const { validate } = define("8")
+            assert(validate(8).errors).is(undefined)
+            assert(validate(8.0).errors).is(undefined)
+            assert(validate(8.000001).errors).is(
+                "8.000001 is not assignable to 8."
+            )
+            assert(validate("8").errors).is("'8' is not assignable to 8.")
+        })
+        test("decimal", () => {
+            const { validate } = define("1.618")
+            assert(validate(1.618).errors).is(undefined)
+            assert(validate(2).errors).is("2 is not assignable to 1.618.")
+            assert(validate("1.618").errors).is(
+                "'1.618' is not assignable to 1.618."
+            )
+        })
+        test("negative", () => {
+            const { validate } = define("-13.37")
+            assert(validate(-13.37).errors).is(undefined)
+            assert(validate(-14).errors).is("-14 is not assignable to -13.37.")
+            assert(validate("-13.37").errors).is(
+                "'-13.37' is not assignable to -13.37."
+            )
+        })
+    })
+    describe("generation", () => {
+        test("whole", () => {
+            assert(define("31").generate()).is(31)
+        })
+        test("decimal", () => {
+            assert(define("31.31").generate()).is(31.31)
+        })
+        test("negative", () => {
+            assert(define("-31.31").generate()).is(-31.31)
+        })
+    })
 }
