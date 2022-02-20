d580585903634f0a9c98cfe301745e2d5fb607e1
David Blass <david@redo.qa>
2022-02-20 17:04:19 -0500

    Add some initial string validation examples.

--- pkgs/model/src/definitions/str/builtin/keyword/string.ts
+++ pkgs/model/src/definitions/str/builtin/keyword/string.ts
@@ -1,16 +1,40 @@
 import { defineKeywords, listKeywords } from "./internal.js"
 import validator from "validator"
+import { StringLiteral } from "../stringLiteral.js"
+import { Func } from "@re-/tools"
+
+const validateLiteral = (
+    valueType: unknown,
+    validate: Func<[string], boolean>
+) =>
+    StringLiteral.matches(valueType) &&
+    validate(StringLiteral.valueFrom(valueType))
 
 export const stringHandlers = defineKeywords({
     string: {
-        generate: () => "" as string,
-        allows: (valueType) =>
-            typeof valueType === "string" && !!valueType.match("'.*'")
+        generate: () => "",
+        allows: (valueType) => StringLiteral.matches(valueType)
     },
     email: {
         generate: () => "david@redo.dev",
+        allows: (valueType) => validateLiteral(valueType, validator.isEmail)
+    },
+    alpha: {
+        generate: () => "",
+        allows: (valueType) => validateLiteral(valueType, validator.isAlpha)
+    },
+    alphanumeric: {
+        generate: () => "",
         allows: (valueType) =>
-            typeof valueType === "string" && validator.isEmail(valueType)
+            validateLiteral(valueType, validator.isAlphanumeric)
+    },
+    lowercase: {
+        generate: () => "",
+        allows: (valueType) => validateLiteral(valueType, validator.isLowercase)
+    },
+    uppercase: {
+        generate: () => "",
+        allows: (valueType) => validateLiteral(valueType, validator.isLowercase)
     }
 })
 
