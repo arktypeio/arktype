fd0dc235e701bba3c23268dfecbb536be81b35dd
David Blass <david@redo.dev>
2022-03-07 09:08:59 -0800

    Add changelogs.

--- pkgs/docs/src/pages/index.tsx
+++ pkgs/docs/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import clsx from "clsx"
 import Layout from "@theme/Layout"
-import Link from "@docusaurus/Link"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import { useColorMode } from "@docusaurus/theme-common"
 import styles from "./index.module.css"
