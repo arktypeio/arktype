03dfa234f3c1c38f9357565ac960d5435fb356d5
David Blass <david@redo.dev>
2022-06-19 09:52:02 -0400

    Finish up basic docgen prototype.

--- scripts/docgen/write.ts
+++ scripts/docgen/write.ts
@@ -1,6 +1,6 @@
 import { join } from "node:path"
-import { ensureDir, writeFile } from "@re-/node"
-import { EntryPointData, RepoMetadata } from "./extract.js"
+import { ensureDir, shell, writeFile } from "@re-/node"
+import { EntryPointData, ExportData, RepoMetadata } from "./extract.js"
 
 export const writeDocs = (repoMetadata: RepoMetadata, outDir: string) => {
     for (const pkg of repoMetadata) {
@@ -14,14 +14,47 @@ export const writeDocs = (repoMetadata: RepoMetadata, outDir: string) => {
             writeEntryPoint(entryPoint, entryPointOutDir)
         }
     }
+    shell(`prettier --write ${outDir}`)
 }
 
-export const writeEntryPoint = (
+const writeEntryPoint = (
     entryPoint: EntryPointData,
     entryPointOutDir: string
 ) => {
     for (const exported of entryPoint.exports) {
-        const mdFilePath = join(entryPointOutDir, `${exported.name}.json`)
-        writeFile(mdFilePath, JSON.stringify(exported))
+        const mdFilePath = join(entryPointOutDir, `${exported.name}.md`)
+        writeFile(mdFilePath, generateMarkdownForExport(exported))
+    }
+}
+
+const generateMarkdownForExport = (exported: ExportData) => {
+    const md = new MarkdownSection(exported.name)
+    md.section("tags").text(JSON.stringify(exported.tsDocs, null, 4))
+    md.section("text").text(exported.text)
+    return md.toString()
+}
+
+class MarkdownSection {
+    private contents: (string | MarkdownSection)[]
+    constructor(header: string, private depth = 1) {
+        this.contents = [`${"#".repeat(depth)} ${header}\n`]
+    }
+
+    section(header: string) {
+        const section = new MarkdownSection(header, this.depth + 1)
+        this.contents.push(section)
+        return section
+    }
+
+    text(content: string) {
+        this.contents.push(`${content}\n`)
+        return this
+    }
+
+    toString(): string {
+        return this.contents.reduce(
+            (result: string, section) => result + section.toString(),
+            ""
+        )
     }
 }
