12dd7155c9e0b8a92c31fa163b4f0b59607a0e00
David Blass <david@redo.dev>
2022-06-27 16:22:00 -0400

    chore: migrate docs dir to gh-pages branch

--- docs/assets/js/495c6acc.6c5cc071.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkredo_dev=self.webpackChunkredo_dev||[]).push([[252],{2966:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"test"}')}}]);
--- docs/assets/js/810dd841.fe031fd4.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkredo_dev=self.webpackChunkredo_dev||[]).push([[793],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
