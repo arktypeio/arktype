12321654e0e50bee3a93ea2da990aec0ac3befa0
David Blass <david@redo.dev>
2022-07-06 15:14:05 -0400

    Add some notes on text-based snippet extraction design.

--- scripts/docgen/snippets/extractSnippets.ts
+++ scripts/docgen/snippets/extractSnippets.ts
@@ -29,6 +29,9 @@ export const addDefaultsToTransformOptions = (
     ...options
 })
 
+// NOTE: There should be no references to ts-morph in this file other than passing it through functions so that
+// when we get to parseSnipComment, we can pass it to the ts-specific functionality
+
 export const extractPackageSnippets = ({
     project,
     sources,
@@ -65,6 +68,7 @@ export type FileSnippets = {
 
 export type LabeledSnippets = Record<string, Snippet>
 
+// Update: Take text of sourceFile as first param instead of sourceFile node
 const extractSnippetsFromFile = (
     sourceFile: SourceFile,
     ctx: ExtractFileSnippetContext
@@ -75,6 +79,7 @@ const extractSnippetsFromFile = (
     return {
         all: {
             // It's important we get the file text after extracting snippets so snip comments are not included
+            // Update: Call extractTextBetweenLines on the file starting with the first ending with the last to exclude snip comments
             text: sourceFile.getFullText().trim()
         },
         byLabel
@@ -99,6 +104,8 @@ const applyTransforms = (
     }
 }
 
+// We probably don't need this anymore now that we're returning LabeledSnippets
+// from extractSnippetRanges, although might be useful to reference in the meantime
 const extractTextFromSnippetRanges = (
     sourceFile: SourceFile,
     snippetRanges: SnipRange[]
@@ -123,16 +130,31 @@ const extractTextFromSnippetRanges = (
     return labeledSnippets
 }
 
+// Update: Should iterate over lines of the file contents instead of comments
+// It is fine to assume that if a line contains @snip it is a snip comment, don't need to check for comment syntax
+// Instead of returning SnipRange[], return LabeledSnippets, which maps snippet.id to the text contents
 const extractSnippetRangesFromFile = (sourceFile: SourceFile): SnipRange[] => {
     const snipRanges: SnipRange[] = []
-    const openBlocks: ParsedSnip[] = []
+    const openBlocks: SnipStart[] = []
     const comments = sourceFile.getDescendantsOfKind(
         SyntaxKind.SingleLineCommentTrivia
     )
+    // Iterate over each line of the file
     for (const comment of comments) {
         if (comment.getText().includes("@snip")) {
             const parsedSnip = parseSnipComment(comment)
+            // Move the logic to determine text or position here
+            if (parsedSnip.kind === "@snipStatement") {
+                // Here, we check if we're in a ts file or not by whether it matches /^.*\.(c|m)?tsx?$/
+                // If it is a TS file, call getNextTsStatement()
+                // Otherwise, call getNextTextStatement()
+            }
+            // For @snipStart and @snipEnd, just use the positions (we don't need to do anything with ts-morph)
             if (parsedSnip.kind === "@snipStart") {
+                // Add a position here to the parsedSnip
+                // We still need a way to make sure that if we're parsing a TS file,
+                // That the actual text we end up adding as a labeled snippet comes from ts-morph
+                // So that it will reflect the transformations.
                 openBlocks.push(parsedSnip)
             } else if (parsedSnip.kind === "@snipEnd") {
                 const matchingSnipStartIndex = openBlocks.findIndex(
@@ -172,6 +194,18 @@ const extractSnippetRangesFromFile = (sourceFile: SourceFile): SnipRange[] => {
     return snipRanges
 }
 
+// This should be called when extracting text from the entire file as well
+const extractTextBetweenLines = (
+    lines: string[],
+    firstLine: number,
+    lastLine: number
+) => {
+    // For each line from first to last, check if the line text is included using the
+    // "outputWillInclude" function  (or equivalent text-based version)
+    // If it is, append it to the text, otherwise do nothing
+}
+
+// Note: For plain text provider, statement will refer to next line
 type SnipKind = `@snip${"Statement" | "Start" | "End"}`
 
 type SnipRange = {
@@ -181,13 +215,25 @@ type SnipRange = {
         | { start: Node<ts.Node>; end: Node<ts.Node> }
 }
 
+// parseSnip is now just responsible for extracting ID and kind, which  will be used to get text/position
+type UpdatedParsedSnip = {
+    id: string
+    kind: SnipKind
+}
+
+type SnipStart = UpdatedParsedSnip & {
+    lineNumber: number
+}
+
 type ParsedSnip = {
     id: string
     node: Node<ts.Node>
     kind: SnipKind
 }
 
+// Update: snipComment to be a text value
 const parseSnipComment = (snipComment: Node<ts.Node>): ParsedSnip => {
+    // This first block looks fine and can be reused
     const commentText = snipComment.getText()
     const snipText = commentText.slice(commentText.indexOf("@snip"))
     const parts = snipText.split(" ")
@@ -197,8 +243,11 @@ const parseSnipComment = (snipComment: Node<ts.Node>): ParsedSnip => {
             `Snip comment '${snipText}' requires a label like '@snipStatement:mySnipLabel'.`
         )
     }
+    // Just return the kind and id here, the logic from this block will move to the extractFromFile function
     let node
-    if (kind === "@snipStatement" || kind === "@snipStart") {
+    if (kind === "@snipStatement") {
+        node = getNextIncludedSibling(snipComment)
+    } else if (kind === "@snipStart") {
         node = getNextIncludedSibling(snipComment)
     } else if (kind === "@snipEnd") {
         node = getLastIncludedSibling(snipComment)
@@ -212,6 +261,22 @@ const parseSnipComment = (snipComment: Node<ts.Node>): ParsedSnip => {
         node,
         kind
     }
+}
+
+const getNextTsStatement = (
+    project: Project,
+    filePath: string,
+    position: number
+): string => {
+    // Add filePath to project
+    // get node at position
+    // Then call getNextIncludedSibling on that node
+    // Return text of that node
+    return ""
+}
+
+const getNextIncludedTextStatement = (lines: string[]): string => {
+    return ""
 }
 
 const getNextIncludedSibling = (snipComment: Node<ts.Node>) => {
@@ -234,6 +299,7 @@ const getLastIncludedSibling = (snipComment: Node<ts.Node>) => {
     return lastIncluded
 }
 
+// This needs a text-based implementation alongside this TS based implementation
 const getFirstIncluded = (nodes: Node<ts.Node>[]) =>
     nodes.find(outputWillInclude)
 
