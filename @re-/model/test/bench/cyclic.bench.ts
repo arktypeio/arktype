fe9601ddfcf0f3410194f397bead99b39c6138b9
David Blass <david@redo.dev>
2022-07-11 22:11:53 -0400

    Update cyclic benches.

--- @re-/model/test/bench/cyclic.bench.ts
+++ @re-/model/test/bench/cyclic.bench.ts
@@ -13,54 +13,54 @@ const eagerConfig = { parse: { eager: true } }
 bench("cyclic(10)", () => {
     const cyclic10Space = space(cyclic10)
 })
-    .median(`1.99us`)
+    .median(`1.19us`)
     .type()
-    .median(`151.66ms`)
+    .median(`40.13ms`)
 
 bench("cyclic eager(10)", () => {
     const cyclic10Space = space(cyclic10, eagerConfig)
-}).median(`31.26us`)
+}).median(`35.23us`)
 
 bench("cyclic(50)", () => {
     const cyclic50Space = space(cyclic50)
 })
-    .median(`12.58us`)
+    .median(`26.83us`)
     .type()
-    .median(`155.25ms`)
+    .median(`75.10ms`)
 
 bench("cyclic eager(50)", () => {
     const cyclic50Space = space(cyclic50, eagerConfig)
-}).median(`172.69us`)
+}).median(`204.17us`)
 
 bench("cyclic(100)", () => {
     const cyclic100Space = space(cyclic100)
 })
-    .median(`32.41us`)
+    .median(`54.83us`)
     .type()
-    .median(`178.53ms`)
+    .median(`117.48ms`)
 
 bench("cyclic eager(100)", () => {
     const cyclic100Space = space(cyclic100, eagerConfig)
-}).median(`361.61us`)
+}).median(`428.02us`)
 
 bench("cyclic(250)", () => {
     const cyclic250Space = space(cyclic250)
 })
-    .median(`135.87us`)
+    .median(`140.75us`)
     .type()
-    .median(`256.90ms`)
+    .median(`259.14ms`)
 
 bench("cyclic eager(250)", () => {
     const cyclic250Space = space(cyclic250, eagerConfig)
-}).median(`1.05ms`)
+}).median(`1.36ms`)
 
 bench("cyclic(500)", () => {
     const cyclic500Space = space(cyclic500)
 })
-    .median(`354.97us`)
+    .median(`308.43us`)
     .type()
-    .median(`549.15ms`)
+    .median(`572.96ms`)
 
 bench("cyclic eager(500)", () => {
     const cyclic500Space = space(cyclic500, eagerConfig)
-}).median(`2.72ms`)
+}).median(`2.71ms`)
