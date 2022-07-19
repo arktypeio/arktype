80ede73c3bc8f817522dc2513556800c54b4c989
David Blass <david@redo.dev>
2022-07-19 16:26:26 -0400

    Fix scripts node path on Windows.

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,4 +1,4 @@
 import { fromHere, shell } from "@re-/node"
 
 const mochaBin = fromHere("..", "node_modules", "mocha", "bin", "mocha.js")
-shell(mochaBin, { cwd: fromHere("docgen") })
+shell(`node ${mochaBin}`, { cwd: fromHere("docgen") })
