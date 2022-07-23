9b60e680a1bd3cd16bccf79e52bf6ce5150005a7
David Blass <david@redo.dev>
2022-07-23 00:42:34 -0400

    Further optimize alias inference.

--- @re-/type/src/nodes/str/alias.ts
+++ @re-/type/src/nodes/str/alias.ts
@@ -1,4 +1,4 @@
-import { And, Get, WithPropValue } from "@re-/tools"
+import { WithPropValue } from "@re-/tools"
 import { Resolution } from "../resolution.js"
 import { Root } from "../root.js"
 import { Base } from "./base.js"
@@ -7,35 +7,44 @@ export namespace Alias {
     export type TypeOf<
         Def extends keyof Ctx["dict"],
         Ctx extends Base.Parsing.InferenceContext
-    > = And<
-        "onResolve" extends keyof Ctx["meta"] ? true : false,
-        Def extends "$resolution" ? false : true
-    > extends true
-        ? Root.TypeOf<
-              Ctx["meta"]["onResolve"],
-              {
-                  dict: WithPropValue<
-                      Ctx["dict"],
-                      "$resolution",
-                      Ctx["dict"][Def]
-                  >
-                  meta: Ctx["meta"]
-                  seen: Ctx["seen"] & { [K in Def]: true }
-              }
-          >
-        : And<
-              "onCycle" extends keyof Ctx["meta"] ? true : false,
-              Def extends keyof Ctx["seen"] ? true : false
-          > extends true
-        ? Root.TypeOf<
-              Ctx["meta"]["onCycle"],
-              {
-                  dict: WithPropValue<Ctx["dict"], "$cyclic", Ctx["dict"][Def]>
-                  meta: Ctx["meta"]
-                  seen: {}
-              }
-          >
-        : Root.TypeOf<Ctx["dict"][Def], Ctx & { seen: { [K in Def]: true } }>
+    > = "onResolve" extends keyof Ctx["meta"]
+        ? Def extends "$resolution"
+            ? BaseTypeOf<Def, Ctx>
+            : OnResolveTypeOf<Def, Ctx>
+        : "onCycle" extends keyof Ctx["meta"]
+        ? Def extends keyof Ctx["seen"]
+            ? OnCycleTypeOf<Def, Ctx>
+            : BaseTypeOf<Def, Ctx>
+        : BaseTypeOf<Def, Ctx>
+
+    type BaseTypeOf<
+        Def extends keyof Ctx["dict"],
+        Ctx extends Base.Parsing.InferenceContext
+    > = Root.TypeOf<Ctx["dict"][Def], Ctx & { seen: { [K in Def]: true } }>
+
+    type OnResolveTypeOf<
+        Def extends keyof Ctx["dict"],
+        Ctx extends Base.Parsing.InferenceContext
+    > = Root.TypeOf<
+        Ctx["meta"]["onResolve"],
+        {
+            dict: WithPropValue<Ctx["dict"], "$resolution", Ctx["dict"][Def]>
+            meta: Ctx["meta"]
+            seen: Ctx["seen"] & { [K in Def]: true }
+        }
+    >
+
+    type OnCycleTypeOf<
+        Def extends keyof Ctx["dict"],
+        Ctx extends Base.Parsing.InferenceContext
+    > = Root.TypeOf<
+        Ctx["meta"]["onCycle"],
+        {
+            dict: WithPropValue<Ctx["dict"], "$cyclic", Ctx["dict"][Def]>
+            meta: Ctx["meta"]
+            seen: {}
+        }
+    >
 
     export const matches = (def: string, ctx: Base.Parsing.Context) =>
         !!ctx.space && def in ctx.space.dictionary
