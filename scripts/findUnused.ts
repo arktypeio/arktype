704efef041fdb69607ae475e6df4efd5d19d5054
David Blass <david@redo.dev>
2022-07-28 13:49:28 -0400

    Some initial notes for findUnused.

--- scripts/findUnused.ts
+++ scripts/findUnused.ts
@@ -5,7 +5,21 @@ const project = new Project({
     tsConfigFilePath: "tsconfig.references.json"
 })
 
-const unused: Record<string, string[]> = {}
+const unusedByFile: Record<string, string[]> = {}
+
+/**
+ * Goal: Find exports that are not referenced in any non-test file,
+ * and are not an entry point for a public package (i.e. @re-/type, @re-/assert)
+ *
+ * Notes:
+ * - Even if the export is re-exported by an index.ts file, unless it is explicitly
+ * references somewhere by name, it should not be considered used.
+ *
+ * - Can either check whether the export is referenced anywhere explicitly by name,
+ * even if it is reexported (e.g. export { type } from "@re-/type") (this would
+ * be a heuristic for whether it's part of a public API) or have a special
+ * case for entry point files for public packages.
+ */
 
 for (const sourceFile of project.getSourceFiles()) {
     const file = relative(".", sourceFile.getFilePath())
@@ -30,13 +44,13 @@ for (const sourceFile of project.getSourceFiles()) {
         }
     }
     if (unusedInFile.length) {
-        unused[file] = unusedInFile
+        unusedByFile[file] = unusedInFile
     }
 }
 
-if (Object.keys(unused).length) {
+if (Object.keys(unusedByFile).length) {
     console.error("The following unused exports must be removed:")
-    for (const [file, unusedNames] of Object.entries(unused)) {
+    for (const [file, unusedNames] of Object.entries(unusedByFile)) {
         console.group(`\n${file}:`)
         for (const unusedName of unusedNames) {
             console.log(`❌${unusedName}`)
