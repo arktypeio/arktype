26329f19dadbc39e7ea7c8168886aecd9ccbf0c8
Shawn Morreau <shawn@redo.dev>
2022-08-03 19:27:05 -0400

    find refs

--- scripts/findUnused.ts
+++ scripts/findUnused.ts
@@ -1,40 +1,30 @@
 import { relative } from "node:path"
 import { fromHere } from "@re-/node"
-import {
-    BindingNamedNode,
-    ClassDeclaration,
-    Project,
-    SyntaxKind
-} from "ts-morph"
+import { Project, SyntaxKind } from "ts-morph"
 
 const project = new Project({
-    tsConfigFilePath: fromHere("..", "tsconfig.shawn.json")
+    tsConfigFilePath: fromHere("..", "tsconfig.references.json")
 })
-
 const unusedByFile: Record<string, string[]> = {}
-
-/**
- * Goal: Find exports that are not referenced in any non-test file,
- * and are not an entry point for a public package (i.e. @re-/type, @re-/assert)
- *
- * Notes:
- * - Even if the export is re-exported by an index.ts file, unless it is explicitly
- * references somewhere by name, it should not be considered used.
- *
- * - Can either check whether the export is referenced anywhere explicitly by name,
- * even if it is reexported (e.g. export { type } from "@re-/type") (this would
- * be a heuristic for whether it's part of a public API) or have a special
- * case for entry point files for public packages.
- */
-
+const snippetFileRegex = /snippets\/\w+/
+const exportAllRegex = /export \*/
+const packagesThatExposePublicApi = {}
 for (const sourceFile of project.getSourceFiles()) {
     const file = relative(".", sourceFile.getFilePath())
+    if (snippetFileRegex.test(file)) {
+        continue
+    }
     const unusedInFile = []
     for (const statement of sourceFile.getVariableStatements()) {
         if (statement.hasModifier(SyntaxKind.ExportKeyword)) {
+            if (exportAllRegex.test(statement.getText())) {
+                console.log(statement.getText())
+            }
             const declarations = statement.getDeclarations()
             for (const declaration of declarations) {
-                const references = declaration.findReferences()
+                const references = declaration
+                    .findReferences()
+                    .flatMap((ref) => ref.getReferences())
                 if (references.length === 1) {
                     unusedInFile.push(declaration.getName())
                 }
@@ -45,30 +35,8 @@ for (const sourceFile of project.getSourceFiles()) {
         }
     }
 }
-//     const references = exportedSymbol
-//         .getDeclarations()
-//         .flatMap((declaration) => {
-//             if (declaration.getKindName() === "ExportSpecifier") {
-//                 return []
-//             }
-//             if (declaration.getSourceFile() !== sourceFile) {
-//                 return []
-//             }
-//             return (declaration as any as BindingNamedNode)
-//                 .findReferences()
-//                 .flatMap((ref) => ref.getReferences())
-//         })
-//     if (references.length === 1) {
-//         unusedInFile.push(exportName)
-//     }
-// }
-// if (unusedInFile.length) {
-//     unusedByFile[file] = unusedInFile
-// }
-//}
-
 if (Object.keys(unusedByFile).length) {
-    console.error("Code doesn't need to be exported or is completely unused")
+    console.error("Code doesn't need to be exported or is unused")
     for (const [file, unusedNames] of Object.entries(unusedByFile)) {
         console.group(`\n${file}:`)
         for (const unusedName of unusedNames) {
