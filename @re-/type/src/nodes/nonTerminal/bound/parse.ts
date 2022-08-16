24f1ad3263fb8e858eabdc1c0fb3b03a84a5223e
David Blass <david@redo.dev>
2022-08-15 22:01:48 -0400

    More bounds cleanup.

--- @re-/type/src/nodes/nonTerminal/bound/parse.ts
+++ @re-/type/src/nodes/nonTerminal/bound/parse.ts
@@ -32,6 +32,12 @@ export namespace Bound {
 
     export const startChars = boundStartChars
 
+    export type Token = keyof typeof tokens
+
+    export type Char = keyof typeof startChars
+
+    export type DoubleBoundToken = keyof typeof doubleBoundTokens
+
     export const doubleBoundTokens = tokenSet({
         "<": 1,
         "<=": 1
@@ -41,31 +47,13 @@ export namespace Bound {
         token: string
     ): token is DoubleBoundToken => token in doubleBoundTokens
 
-    export type Token = keyof typeof tokens
-
-    export type Char = keyof typeof startChars
-
-    export type DoubleBoundToken = keyof typeof doubleBoundTokens
-
-    export type Raw = {
-        left?: Left
-        right?: RawRight
-    }
-
     export type Left = [NumberLiteralDefinition, DoubleBoundToken]
 
-    export type RawRight = [Token, NumberLiteralDefinition]
-
-    export type RawDouble = Required<Raw>
-
-    export type RawSingle = { right: RawRight }
-
-    export const isDoubleBoundCandidate = (bounds: Raw): bounds is RawDouble =>
-        "left" in bounds && "right" in bounds
+    export type Right = [Token, NumberLiteralDefinition]
 
     /** A BoundableNode must be either:
      *    1. A number-typed keyword terminal (e.g. "integer" in "integer>5")
-     *    2. A string-typed keyword terminal (e.g. "alphanum" in "100>alphanum")
+     *    2. A string-typed keyword terminal (e.g. "alphanum" in "100<alphanum")
      *    3. Any list node (e.g. "(string|number)[]" in "(string|number)[]>0")
      */
     export type Boundable =
@@ -84,7 +72,7 @@ export namespace Bound {
     type InvalidDoubleBoundMessage<T extends Token> =
         `Double-bound expressions must specify their bounds using < or <= (got ${T}).`
 
-    export const invalidDoubleBoundMessage = <T extends Token>(
+    const invalidDoubleBoundMessage = <T extends Token>(
         T: T
     ): InvalidDoubleBoundMessage<T> =>
         `Double-bound expressions must specify their bounds using < or <= (got ${T}).`
@@ -92,30 +80,24 @@ export namespace Bound {
     type NonNumericBoundMessage<Value extends string> =
         `Bounding values must be specified using a number literal (got '${Value}').`
 
-    export const nonNumericBoundingMessage = <Value extends string>(
+    const nonNumericBoundingMessage = <Value extends string>(
         Value: Value
     ): NonNumericBoundMessage<Value> =>
         `Bounding values must be specified using a number literal (got '${Value}').`
 
-    export type Validate<Bounds extends Raw, Expression> = {} extends Bounds
-        ? Bounds
-        : CheckEach<ValidateEach<Bounds>, Expression>
+    type NonPrefixLeftBoundMessage<
+        Value extends NumberLiteralDefinition,
+        T extends Token
+    > = `Left bound '${Value}${T}...' must occur at the beginning of the definition.`
 
-    type CheckEach<
-        ValidatedBounds extends BoundValidationResult,
-        Expression
-    > = ValidatedBounds["left"] extends ErrorToken<string>
-        ? ValidatedBounds["left"]
-        : ValidatedBounds["right"] extends ErrorToken<string>
-        ? ValidatedBounds["right"]
-        : Expression extends Boundable
-        ? ValidatedBounds
-        : ErrorToken<`Bounded expression '${Tree.ToString<Expression>}' must be a number-or-string-typed keyword or a list-typed expression.`>
-
-    type BoundValidationResult = {
-        left?: [NumberLiteralDefinition, DoubleBoundToken] | ErrorToken<string>
-        right?: [Token, NumberLiteralDefinition] | ErrorToken<string>
-    }
+    const nonPrefixLeftBoundMessage = <
+        Value extends NumberLiteralDefinition,
+        T extends Token
+    >(
+        Value: Value,
+        T: T
+    ): NonPrefixLeftBoundMessage<Value, T> =>
+        `Left bound '${Value}${T}...' must occur at the beginning of the definition.`
 
     export type Finalize<Root, Bounds extends Partial> = {} extends Bounds
         ? Root
@@ -131,34 +113,6 @@ export namespace Bound {
                 ? false
                 : true
             : false
-
-    type ValidateEach<Bounds extends Raw> = Bounds extends RawDouble
-        ? ValidateDoubleBound<Bounds>
-        : Bounds extends RawSingle
-        ? ValidateSingleBound<Bounds>
-        : {
-              left: ErrorToken<UnpairedLeftBoundMessage>
-          }
-
-    type ValidateDoubleBound<Bounds extends RawDouble> = {
-        left: ValidateDoubleBoundSide<Bounds["left"][0], Bounds["left"][1]>
-        right: ValidateDoubleBoundSide<Bounds["right"][1], Bounds["right"][0]>
-    }
-
-    type ValidateDoubleBoundSide<
-        Value,
-        T extends Token
-    > = Value extends NumberLiteralDefinition
-        ? T extends DoubleBoundToken
-            ? [Value, T]
-            : ErrorToken<InvalidDoubleBoundMessage<T>>
-        : ErrorToken<NonNumericBoundMessage<Value & string>>
-
-    type ValidateSingleBound<Bounds extends RawSingle> = {
-        right: Bounds["right"][1] extends NumberLiteralDefinition
-            ? Bounds
-            : ErrorToken<NonNumericBoundMessage<Bounds["right"][1] & string>>
-    }
 
     export type Reduce<
         L extends Left.Base,
@@ -179,7 +133,7 @@ export namespace Bound {
                   bounds: { left: [L["root"], T] }
               }>
             : Left.Error<InvalidDoubleBoundMessage<T>>
-        : Left.Error<`Left bound '${L["root"]}${T}...' must occur at the beginning of the definition.`>
+        : Left.Error<NonPrefixLeftBoundMessage<L["root"], T>>
 
     type ReduceRight<
         L extends Left.Base,
@@ -247,20 +201,19 @@ export namespace Bound {
         }
     }
 
-    export const unpairedLeftBoundMessage = `Left bounds are only valid when paired with right bounds.`
+    const unpairedLeftBoundMessage = `Left bounds are only valid when paired with right bounds.`
     type UnpairedLeftBoundMessage = typeof unpairedLeftBoundMessage
 
     type UnboundableMessage<Root extends string> =
         `Bounded expression '${Root}' must be a number-or-string-typed keyword or a list-typed expression.`
-
-    export const unboundableMessage = <Root extends string>(
+    const unboundableMessage = <Root extends string>(
         Root: Root
     ): UnboundableMessage<Root> =>
         `Bounded expression '${Root}' must be a number-or-string-typed keyword or a list-typed expression.`
 
     const createBound = (
         s: State.WithRoot,
-        right: RawRight,
+        right: Right,
         ctx: Base.Parsing.Context
     ) => {
         s.bounds.right = right
@@ -282,7 +235,7 @@ export namespace Bound {
         }
     }
 
-    const validateSingleBound = (bound: RawRight): SingleBoundDefinition => {
+    const validateSingleBound = (bound: Right): SingleBoundDefinition => {
         return [bound[0], toNumber(bound[1])]
     }
 
