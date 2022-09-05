f4a517d5610f46ac89cc0db252847a6b815fe058
David Blass <david@redo.dev>
2022-09-05 14:29:21 -0400

    Update deps.

--- @re-/type/src/__tests__/space/space.bench.ts
+++ @re-/type/src/__tests__/space/space.bench.ts
@@ -8,29 +8,30 @@ import {
     cyclic500
 } from "./generated/cyclic.js"
 
+const recursive = space({ dejaVu: { dejaVu: "dejaVu?" } })
+const dejaVu: typeof recursive.$root.infer.dejaVu = {}
+let i = 0
+let current = dejaVu
+while (i < 50) {
+    current.dejaVu = { dejaVu: {} }
+    current = current.dejaVu
+    i++
+}
+bench("validate recursive", () => {
+    recursive.dejaVu.check(dejaVu)
+}).median(`77.57us`)
+
 bench("cyclic(10)", () => {
     const cyclic10Space = space(cyclic10)
 })
     .median(`16.00us`)
     .type(`2053 instantiations`)
 
-bench("cyclic(50)", () => {
-    const cyclic50Space = space(cyclic50)
-})
-    .median(`88.55us`)
-    .type(`5219 instantiations`)
-
 bench("cyclic(100)", () => {
     const cyclic100Space = space(cyclic100)
 })
     .median(`186.09us`)
     .type(`9189 instantiations`)
-
-bench("cyclic(250)", () => {
-    const cyclic250Space = space(cyclic250)
-})
-    .median(`511.60us`)
-    .type(`20926 instantiations`)
 
 bench("cyclic(500)", () => {
     const cyclic500Space = space(cyclic500)
