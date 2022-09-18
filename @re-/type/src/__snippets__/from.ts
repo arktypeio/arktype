f920d7c24017babdbd591ecee798b7b9668487db
David Blass <david@redo.dev>
2022-09-18 17:49:17 -0400

    Add some ideas for io API.

---
+++ @re-/type/src/__snippets__/from.ts
@@ -0,0 +1,69 @@
+//import { type } from "../index.js"
+
+import { narrow, Narrow } from "@re-/tools"
+
+const type = {} as any
+const io = {} as any
+
+type UserInput = {
+    name: string
+    age?: number
+}
+
+type UserOut = {
+    first: string
+    last: string
+    age?: number
+}
+
+const user = type({
+    name: "string",
+    age: "number?"
+})
+
+const newUser = user({ name: "David" })
+
+const user2 = type([
+    "$io",
+    { name: "string", age: "number?" },
+    { first: "string", last: "string", age: "number?" },
+    // Keys whose type does not change (and eventually those that can be auto-converted) should be optional to return
+    ({ name }) => {
+        const [first, last] = name.split(" ")
+        return {
+            first,
+            last
+        }
+    }
+])
+
+const user3 = type(
+    io(
+        { name: "string", age: "number?" },
+        { first: "string", last: "string", age: "number?" },
+        // Keys whose type does not change (and eventually those that can be auto-converted) should be optional to return
+        ({ name }) => {
+            const [first, last] = name.split(" ")
+            return {
+                first,
+                last
+            }
+        }
+    )
+)
+
+const user4 = type({
+    age: "number?",
+    name: io(
+        "string",
+        { first: "string", last: "string" },
+        // Keys whose type does not change (and eventually those that can be auto-converted) should be optional to return
+        (full) => {
+            const [first, last] = full.split(" ")
+            return {
+                first,
+                last
+            }
+        }
+    )
+})
