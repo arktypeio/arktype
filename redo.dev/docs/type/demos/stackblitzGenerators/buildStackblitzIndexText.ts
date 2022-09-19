517edbbe1009c74e5a8c99222edab3c337e46bf0
Shawn Morreau <shawn@redo.dev>
2022-09-19 16:51:34 -0400

    fix demo so that it will not error out with valid input

--- redo.dev/docs/type/demos/stackblitzGenerators/buildStackblitzIndexText.ts
+++ redo.dev/docs/type/demos/stackblitzGenerators/buildStackblitzIndexText.ts
@@ -15,18 +15,18 @@ type DemoAdapter = [importFromDemo: string, dataFromImports: string]
 const demoAdapters: Record<EmbedId, DemoAdapter> = {
     type: [
         `import { user, fetchUser, errors } from "./type"`,
-        `{ definition: user.definition, data: fetchUser(), error: errors.summary }`
+        `{ definition: user.definition, data: fetchUser(), error: errors ? errors.summary : undefined  }`
     ],
     space: [
         `import { types, readPackageData, errors } from "./space"`,
-        "{ definition: types.$root.dictionary, data: readPackageData(), error: errors.summary }"
+        "{ definition: types.$root.dictionary, data: readPackageData(), error: errors ? errors.summary : undefined }"
     ],
     constraints: [
         `import { employee, queryEmployee, errors } from "./constraints"`,
-        "{ definition: employee.definition, data: queryEmployee(), error: errors.summary }"
+        "{ definition: employee.definition, data: queryEmployee(), error: errors ? errors.summary : undefined}"
     ],
     declaration: [
         `import { types, getGroupsForCurrentUser, errors } from "./declaration"`,
-        "{ definition: types.$root.dictionary, data: getGroupsForCurrentUser(), error: errors.summary }"
+        "{ definition: types.$root.dictionary, data: getGroupsForCurrentUser(), error: errors ? errors.summary : undefined }"
     ]
 }
