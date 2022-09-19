c53e94852188cbeb952293cf5aadb8c8f46a88f9
Shawn Morreau <shawn@redo.dev>
2022-09-19 17:00:36 -0400

    better error handling thing

--- redo.dev/docs/type/demos/stackblitzGenerators/buildStackblitzIndexText.ts
+++ redo.dev/docs/type/demos/stackblitzGenerators/buildStackblitzIndexText.ts
@@ -15,18 +15,18 @@ type DemoAdapter = [importFromDemo: string, dataFromImports: string]
 const demoAdapters: Record<EmbedId, DemoAdapter> = {
     type: [
         `import { user, fetchUser, errors } from "./type"`,
-        `{ definition: user.definition, data: fetchUser(), error: errors ? errors.summary : undefined  }`
+        `{ definition: user.definition, data: fetchUser(), error: errors?.summary }`
     ],
     space: [
         `import { types, readPackageData, errors } from "./space"`,
-        "{ definition: types.$root.dictionary, data: readPackageData(), error: errors ? errors.summary : undefined }"
+        "{ definition: types.$root.dictionary, data: readPackageData(), error: errors?.summary }"
     ],
     constraints: [
         `import { employee, queryEmployee, errors } from "./constraints"`,
-        "{ definition: employee.definition, data: queryEmployee(), error: errors ? errors.summary : undefined}"
+        "{ definition: employee.definition, data: queryEmployee(), error: errors?.summary }"
     ],
     declaration: [
         `import { types, getGroupsForCurrentUser, errors } from "./declaration"`,
-        "{ definition: types.$root.dictionary, data: getGroupsForCurrentUser(), error: errors ? errors.summary : undefined }"
+        "{ definition: types.$root.dictionary, data: getGroupsForCurrentUser(), error: errors?.summary }"
     ]
 }
