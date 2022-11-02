a00beccc94c9cf7d640303d7e563748ce847456f
David Blass <david@arktype.io>
2022-11-02 11:08:21 -0400

    Fix inference in type functions with spaces.

--- src/ast/infer.ts
+++ src/ast/infer.ts
@@ -3,34 +3,35 @@ import type { Enclosed } from "../parser/string/operand/enclosed.js"
 import type { Keyword } from "../parser/string/operand/keyword.js"
 import type { NumberLiteral } from "../parser/string/operand/numeric.js"
 import type { Scanner } from "../parser/string/state/scanner.js"
+import type { ArktypeSpace } from "../space.js"
 
-export type inferAst<node, spaceAst> = node extends TerminalAst
-    ? inferTerminal<node, spaceAst>
+export type inferAst<node, space> = node extends TerminalAst
+    ? inferTerminal<node, space>
     : node extends readonly unknown[]
     ? node[1] extends "?"
-        ? inferAst<node[0], spaceAst> | undefined
+        ? inferAst<node[0], space> | undefined
         : node[1] extends "[]"
-        ? inferAst<node[0], spaceAst>[]
+        ? inferAst<node[0], space>[]
         : node[1] extends "|"
-        ? inferAst<node[0], spaceAst> | inferAst<node[2], spaceAst>
+        ? inferAst<node[0], space> | inferAst<node[2], space>
         : node[1] extends "&"
-        ? evaluate<inferAst<node[0], spaceAst> & inferAst<node[2], spaceAst>>
+        ? evaluate<inferAst<node[0], space> & inferAst<node[2], space>>
         : node[1] extends Scanner.Comparator
         ? node[0] extends NumberLiteral
-            ? inferAst<node[2], spaceAst>
-            : inferAst<node[0], spaceAst>
+            ? inferAst<node[2], space>
+            : inferAst<node[0], space>
         : node[1] extends "%"
-        ? inferAst<node[0], spaceAst>
+        ? inferAst<node[0], space>
         : // If the value at index 1 was none of the above, it's a normal tuple definition
           evaluate<{
-              [i in keyof node]: inferAst<node[i], spaceAst>
+              [i in keyof node]: inferAst<node[i], space>
           }>
-    : inferObjectLiteral<node, spaceAst>
+    : inferObjectLiteral<node, space>
 
-type inferTerminal<token extends TerminalAst, spaceAst> = token extends Keyword
+type inferTerminal<token extends TerminalAst, space> = token extends Keyword
     ? Keyword.Inferences[token]
-    : token extends keyof spaceAst
-    ? inferAst<spaceAst[token], spaceAst>
+    : token extends keyof space
+    ? inferAlias<token, space>
     : token extends Enclosed.StringLiteral<infer Text>
     ? Text
     : token extends Enclosed.RegexLiteral
@@ -38,6 +39,14 @@ type inferTerminal<token extends TerminalAst, spaceAst> = token extends Keyword
     : token extends number | bigint
     ? token
     : unknown
+
+type inferAlias<token extends keyof space, space> = space extends ArktypeSpace<
+    infer inferred
+>
+    ? token extends keyof inferred
+        ? inferred[token]
+        : never
+    : inferAst<space[token], space>
 
 type TerminalAst = string | number | bigint
 
--- src/type.ts
+++ src/type.ts
@@ -2,7 +2,6 @@ import type { ArktypeConfig } from "./arktype.js"
 import { Arktype } from "./arktype.js"
 import type { inferAst } from "./ast/infer.js"
 import type { validate } from "./ast/validate.js"
-import type { dictionary } from "./internal.js"
 import type { ParseError } from "./parser/common.js"
 import { initializeParserContext } from "./parser/common.js"
 import { Root } from "./parser/root.js"
@@ -11,7 +10,9 @@ import { space } from "./space.js"
 import type { LazyDynamicWrap } from "./utils/lazyDynamicWrap.js"
 import { lazyDynamicWrap } from "./utils/lazyDynamicWrap.js"
 
-const emptySpace: ArktypeSpace = space({})
+const emptySpace: EmptySpace = space({})
+
+type EmptySpace = ArktypeSpace<{}>
 
 const rawTypeFn: DynamicTypeFn = (
     definition,
@@ -20,30 +21,29 @@ const rawTypeFn: DynamicTypeFn = (
     new Arktype(
         Root.parse(definition, initializeParserContext(space.$)),
         config,
-        space
+        space as ArktypeSpace
     )
 
 export const type: TypeFn = lazyDynamicWrap<InferredTypeFn, DynamicTypeFn>(
     rawTypeFn
 )
 
-export type TypeFnOptions<resolutions extends dictionary = {}> =
-    ArktypeConfig & {
-        space?: ArktypeSpace<resolutions>
-    }
+export type TypeFnOptions<space> = ArktypeConfig & {
+    space?: space
+}
 
 export type InferredTypeFn = <
     definition,
-    spaceAst extends dictionary = {},
-    ast = Root.parse<definition, { aliases: spaceAst }>
+    space = EmptySpace,
+    ast = Root.parse<definition, { aliases: space }>
 >(
-    definition: validate<definition, ast, spaceAst>,
-    options?: TypeFnOptions<spaceAst>
-) => ast extends ParseError<string> ? never : Arktype<inferAst<ast, spaceAst>>
+    definition: validate<definition, ast, space>,
+    options?: TypeFnOptions<space>
+) => ast extends ParseError<string> ? never : Arktype<inferAst<ast, space>>
 
 type DynamicTypeFn = (
     definition: unknown,
-    options?: TypeFnOptions<dictionary<unknown>>
+    options?: TypeFnOptions<ArktypeSpace>
 ) => Arktype
 
 export type TypeFn = LazyDynamicWrap<InferredTypeFn, DynamicTypeFn>
