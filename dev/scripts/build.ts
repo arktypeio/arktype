e355b43a507f3c974f712e8a04672ae09bfaa960
David Blass <david@arktype.io>
2022-11-12 14:34:17 -0500

    Fix arktype build.

--- dev/scripts/build.ts
+++ dev/scripts/build.ts
@@ -1,7 +1,14 @@
-import { rmSync } from "node:fs"
-import { join } from "node:path"
+import { cpSync, renameSync, rmSync } from "node:fs"
+import { join, resolve } from "node:path"
 import { stdout } from "node:process"
-import { readJson, requireResolve, shell, writeJson } from "../runtime/api.js"
+import {
+    readFile,
+    readJson,
+    requireResolve,
+    shell,
+    writeFile,
+    writeJson
+} from "../runtime/api.js"
 import {
     isProd,
     outRoot,
@@ -30,14 +37,39 @@ const buildTypes = () => {
     const tsConfigData = readJson(tsConfig)
     const tempTsConfig = join(repoDirs.root, "tsconfig.temp.json")
     try {
-        writeJson(tempTsConfig, { ...tsConfigData, include: ["api.ts", "src"] })
+        writeJson(tempTsConfig, { ...tsConfigData, include: ["src"] })
         shell(
-            `pnpm tsc --project ${tempTsConfig} --outDir ${typesOut} --emitDeclarationOnly`
+            `pnpm tsc --project ${tempTsConfig} --outDir ${outRoot} --emitDeclarationOnly`
         )
+        renameSync(join(outRoot, "src"), typesOut)
+        buildApiTs("types")
     } finally {
         rmSync(tempTsConfig, { force: true })
     }
     stdout.write(`✅\n`)
+}
+
+const buildApiTs = (kind: "mjs" | "cjs" | "types" | "deno") => {
+    const originalPath =
+        kind === "mjs" || kind === "cjs"
+            ? join(outRoot, kind, "api.js")
+            : "api.ts"
+    const originalContents = readFile(originalPath)
+    if (kind === "mjs" || kind === "cjs") {
+        rmSync(originalPath)
+    }
+    let transformedContents = originalContents.replaceAll(
+        "./src/",
+        `./${kind}/`
+    )
+    if (kind === "deno") {
+        transformedContents = transformedContents.replaceAll(".js", ".ts")
+    }
+    const destinationFile = join(
+        outRoot,
+        `api.${kind === "types" ? "d.ts" : kind === "deno" ? "ts" : kind}`
+    )
+    writeFile(destinationFile, transformedContents)
 }
 
 const transpile = () => {
@@ -50,19 +82,19 @@ const transpile = () => {
 
 const swc = (kind: "mjs" | "cjs") => {
     const srcOutDir = join(outRoot, kind)
-    let cmd = `node ${requireResolve(
-        "@swc/cli"
-    )} --out-dir ${srcOutDir} -C jsc.target=es2020 --quiet `
+    let cmd = `pnpm swc --out-dir ${srcOutDir} -C jsc.target=es2020 --quiet `
     if (kind === "cjs") {
         cmd += `-C module.type=commonjs `
     }
     if (!isProd()) {
         cmd += `--source-maps inline `
     }
+    cmd += srcFiles.join(" ") + " api.ts"
     shell(cmd)
     writeJson(join(srcOutDir, "package.json"), {
         type: kind === "cjs" ? "commonjs" : "module"
     })
+    buildApiTs(kind)
 }
 
 const buildDeno = () => {
@@ -74,6 +106,7 @@ const buildDeno = () => {
         skipSourceMap: true,
         transformContents: (content) => content.replaceAll(/\.js"/g, '.ts"')
     })
+    buildApiTs("deno")
 }
 
 arktypeTsc()
