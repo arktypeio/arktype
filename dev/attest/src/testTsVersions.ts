67050d3350fd2fc6a049cc6ab98d4a3a53273f48
Shawn Morreau <shawn@redo.dev>
2022-11-15 16:41:04 -0500

    .

--- dev/attest/src/testTsVersions.ts
+++ dev/attest/src/testTsVersions.ts
@@ -3,18 +3,18 @@ import { join } from "node:path"
 import { fromHere, fromPackageRoot, readPackageJson } from "../../runtime/fs.js"
 import { shell } from "../../runtime/shell.js"
 ;(() => {
-    const versions: { [k: string]: string } = {
-        "4.7": "ts-morph@15.1.0",
-        "4.6": "ts-morph@14.0.0"
-    }
+    const versions: { [k: string]: string } = {}
     const filePath = join(fromHere(".."), ".npmrc")
     rmSync(filePath, { force: true })
     writeFileSync(filePath, "save=false")
     for (const [tsVersion, tsMorphVersion] of Object.entries(versions)) {
         shell(`pnpm i ${tsMorphVersion}`, { cwd: fromHere() })
         console.log(`testing ${tsVersion}`)
-        // TODO uncomment once tests are passing & make script for CI
-        // shell(`pnpm test`)
+        try {
+            shell(`pnpm test`)
+        } catch (e) {
+            console.error(e)
+        }
     }
     const rootJson = readPackageJson(fromPackageRoot())
     const tsMorphVersion = rootJson["devDependencies"]["ts-morph"]
