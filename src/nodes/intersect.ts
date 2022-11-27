02f6dda554a53baac90ec886669564f53e83b76b
David Blass <david@arktype.io>
2022-11-27 14:23:30 -0500

    Continue intersect and prune cleanup.

--- src/nodes/intersect.ts
+++ src/nodes/intersect.ts
@@ -1,18 +1,35 @@
 import type { ScopeRoot } from "../scope.js"
 import type { mutable } from "../utils/generics.js"
 import { isKeyOf, listFrom } from "../utils/generics.js"
+import { AttributesByType, TypeWithAttributes } from "./node.js"
 import type { BranchingTypeNode, Node } from "./node.js"
+import { intersectBigints } from "./types/bigint.js"
+import { intersectBooleans } from "./types/boolean.js"
 import {
     intersectDegenerate,
     isDegenerate,
     isNever
 } from "./types/degenerate.js"
 import type { Never } from "./types/degenerate.js"
+import { intersectNumbers } from "./types/number.js"
+import { intersectObjects } from "./types/object.js"
+import { IntersectFn } from "./types/operations.js"
+import { intersectStrings } from "./types/string.js"
 
 export const intersect = (l: Node, r: Node, scope: ScopeRoot) =>
     isDegenerate(l) || isDegenerate(r)
         ? intersectDegenerate(l, r, scope)
         : intersectBranches(listFrom(l), listFrom(r), scope)
+
+const attributeIntersections = {
+    bigint: intersectBigints,
+    boolean: intersectBooleans,
+    number: intersectNumbers,
+    object: intersectObjects,
+    string: intersectStrings
+} satisfies {
+    [k in TypeWithAttributes]: IntersectFn<AttributesByType[k]>
+}
 
 const intersectBranches = (
     l: BranchingTypeNode,
@@ -27,10 +44,11 @@ const intersectBranches = (
         )
         if (rightBranches.length) {
             const type = leftBranch.type
-            if (isKeyOf(type, intersectors)) {
-                const branchResult = intersectors[type](
+            if (isKeyOf(type, attributeIntersections)) {
+                const branchResult = attributeIntersections[type](
                     leftBranch as any,
-                    rightBranches as any
+                    rightBranches as any,
+                    scope
                 )
                 if (isNever(branchResult)) {
                     pruned.push(branchResult)
--- src/nodes/prune.ts
+++ src/nodes/prune.ts
@@ -1,6 +1,22 @@
+import { AttributesByType, TypeWithAttributes } from "./node.js"
 import type { Node } from "./node.js"
+import { pruneBigint } from "./types/bigint.js"
+import { pruneBoolean } from "./types/boolean.js"
 import { isDegenerate, pruneDegenerate } from "./types/degenerate.js"
-import type { PruneFn } from "./types/operations.js"
+import { pruneNumber } from "./types/number.js"
+import { pruneObject } from "./types/object.js"
+import { PruneFn } from "./types/operations.js"
+import { pruneString } from "./types/string.js"
+
+const attributePruners = {
+    bigint: pruneBigint,
+    boolean: pruneBoolean,
+    number: pruneNumber,
+    object: pruneObject,
+    string: pruneString
+} satisfies {
+    [k in TypeWithAttributes]: PruneFn<AttributesByType[k]>
+}
 
 export const prune: PruneFn<Node> = (l, r, scope) => {
     if (isDegenerate(l) || isDegenerate(r)) {
