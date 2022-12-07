2c3caadc54f8e32cf9bd84cd9066fbbf220138f0
David Blass <david@arktype.io>
2022-12-07 14:21:28 -0500

    update Node type

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -3,20 +3,44 @@ import type {
     Dictionary,
     evaluate,
     keySet,
-    List
+    listable,
+    PartialDictionary,
+    subtype
 } from "../utils/generics.js"
 import type { ObjectTypeName, TypeName, Types } from "../utils/typeOf.js"
 import type { Bounds } from "./bounds.js"
 import type { Keyword } from "./names.js"
 import type { RegexAttribute } from "./regex.js"
 
-export type Node = BranchableNode | BranchNode
+export type Node = NameNode | TypeNode | BranchNode
 
 export type NameNode = autocompleteString<Keyword>
 
-export type BranchableNode = NameNode | ResolutionNode
+export type TypeNode = TypeNodes[TypeName]
 
-export type BranchNode = List<BranchableNode>
+export type BranchNode = {
+    readonly [typeName in TypeName]?: listable<string | TypeNodes[typeName]>
+}
+
+export type TypeNodes = {
+    [typeName in TypeName]: evaluate<
+        AttributeNodes[typeName] | LiteralNodes[typeName]
+    >
+}
+
+type AttributeNodes = evaluate<{
+    [typeName in TypeName]: {
+        readonly type: typeName
+    } & (typeName extends keyof AttributeKeysByType
+        ? Pick<BaseAttributes, AttributeKeysByType[typeName]>
+        : {})
+}>
+
+type LiteralNodes = evaluate<{
+    [typeName in TypeName]: Types[typeName] extends LiteralValue
+        ? PrimitiveLiteralNode<Types[typeName]>
+        : never
+}>
 
 export type BaseAttributes = {
     readonly type: TypeName
@@ -32,19 +56,13 @@ export type BaseAttributes = {
     readonly bounds?: Bounds
 }
 
-export type ResolutionNode =
-    | ObjectNode
-    | StringNode
-    | NumberNode
-    | BigintNode
-    | BooleanNode
-    | SymbolNode
-    | NullNode
-    | UndefinedNode
-
-export type ObjectNode = DefineAttributeNode<
-    "object",
-    "subtype" | "props" | "requiredKeys" | "propTypes" | "bounds"
+type AttributeKeysByType = subtype<
+    PartialDictionary<TypeName, keyof BaseAttributes>,
+    {
+        object: "subtype" | "props" | "requiredKeys" | "propTypes" | "bounds"
+        string: "regex" | "bounds"
+        number: "divisor" | "bounds"
+    }
 >
 
 export type LiteralValue = string | number | boolean | bigint
@@ -52,29 +70,6 @@ export type LiteralValue = string | number | boolean | bigint
 export type PrimitiveLiteralNode<value extends LiteralValue = LiteralValue> = {
     readonly value: value
 }
-
-export type StringNode = DefineAttributeNode<"string", "regex" | "bounds">
-
-export type NumberNode = DefineAttributeNode<"number", "divisor" | "bounds">
-
-export type BigintNode = DefineAttributeNode<"bigint">
-
-export type BooleanNode = DefineAttributeNode<"boolean">
-
-export type SymbolNode = DefineAttributeNode<"symbol">
-
-export type UndefinedNode = DefineAttributeNode<"undefined">
-
-export type NullNode = DefineAttributeNode<"null">
-
-type DefineAttributeNode<
-    typeName extends TypeName,
-    key extends keyof BaseAttributes = never
-> =
-    | evaluate<{ readonly type: typeName } & Pick<BaseAttributes, key>>
-    | (Types[typeName] extends LiteralValue
-          ? PrimitiveLiteralNode<Types[typeName]>
-          : never)
 
 type PropTypesAttribute = {
     readonly number?: Node
