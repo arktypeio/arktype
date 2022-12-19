bb0e2e1c21875d3acfd4f1599ce565c78b33d933
David Blass <david@arktype.io>
2022-12-19 13:02:11 -0500

    begin working on flatten

--- src/nodes/flatten.ts
+++ src/nodes/flatten.ts
@@ -1,10 +1,19 @@
+import type { Domain } from "../utils/domains.js"
+import type { entryOf } from "../utils/generics.js"
 import type { TypeNode } from "./node.js"
+import type { Rules } from "./rules/rules.js"
 
-export type FlatCondition = string | FlatCondition[]
+export type FlatCondition = ["value", unknown] | entryOf<Omit<Rules, "props">>
 
-export const flatten = (node: TypeNode): string | unknown[] => {
+export type FlatNode =
+    | string
+    | {
+          readonly [domain in Domain]?: readonly FlatCondition[]
+      }
+
+export const flatten = (node: TypeNode): FlatNode => {
     if (typeof node === "string") {
-        return node
+        return {}
     }
-    return []
+    return {}
 }
