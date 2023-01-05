fb26575bb8c642a936087b336579d52e43f3d0a2
David Blass <david@arktype.io>
2023-01-05 10:13:45 -0500

    move scope to class (broken)

--- src/scope.ts
+++ src/scope.ts
@@ -1,59 +1,41 @@
-import type { TypeNode } from "./nodes/node.ts"
-import { resolveIfIdentifier } from "./nodes/utils.ts"
+import { getFlatKeywords, keywords } from "./nodes/keywords.ts"
+import type { TraversalNode, TypeNode, TypeRoot } from "./nodes/node.ts"
+import type {
+    ExactValue,
+    Predicate,
+    ResolvedPredicate,
+    TraversalPredicate
+} from "./nodes/predicate.ts"
 import type { inferDefinition, validateDefinition } from "./parse/definition.ts"
 import { parseDefinition } from "./parse/definition.ts"
+import { fullStringParse, maybeNaiveParse } from "./parse/string/string.ts"
 import type { parseType, Type, TypeParser } from "./type.ts"
 import { isType, nodeToType } from "./type.ts"
 import { chainableNoOpProxy } from "./utils/chainableNoOpProxy.ts"
+import type { Domain } from "./utils/domains.ts"
+import { throwInternalError, throwParseError } from "./utils/errors.ts"
+import { deepFreeze } from "./utils/freeze.ts"
 import type {
+    defined,
     Dict,
     evaluate,
     isTopType,
     merge,
-    mutable,
     stringKeyOf
 } from "./utils/generics.ts"
-import type { LazyDynamicWrap } from "./utils/lazyDynamicWrap.ts"
+import { isKeyOf, keysOf } from "./utils/generics.ts"
 import { lazyDynamicWrap } from "./utils/lazyDynamicWrap.ts"
+import type { LazyDynamicWrap } from "./utils/lazyDynamicWrap.ts"
 
 // TODO: Integrate parent
 const composeScopeParser = <parent extends Scope>(parent?: parent) =>
-    lazyDynamicWrap((aliases: Dict) => {
-        const $: mutable<Scope> = {
-            infer: chainableNoOpProxy,
-            cache: {
-                nodes: {},
-                types: {}
-            },
-            aliases,
-            compile: () => compileScope($)
-        } satisfies Omit<Scope, "type" | "extend"> as any
-        $.type = composeTypeParser($)
-        $.extend = composeScopeParser($)
-        return $
-    }) as ScopeParser<Scope extends parent ? {} : parent>
-
-const compileScope = <aliases>($: Scope<aliases>) => {
-    const types = {} as aliases
-    for (const k in $.aliases) {
-        const def = $.aliases[k]
-        types[k] =
-            typeof def === "function"
-                ? isType(def)
-                    ? def
-                    : def()
-                : nodeToType(
-                      resolveIfIdentifier(parseDefinition(def, $), $),
-                      $,
-                      {}
-                  )
-    }
-    return types
-}
+    lazyDynamicWrap((aliases: Dict) => new Scope(aliases)) as ScopeParser<
+        Scope extends parent ? {} : parent
+    >
 
 export const composeTypeParser = <$ extends Scope>($: $): TypeParser<$> =>
     lazyDynamicWrap((def, traits = {}) => {
-        const node = resolveIfIdentifier(parseDefinition(def, $), $)
+        const node = $.resolveIfIdentifier(parseDefinition(def, $))
         return nodeToType(node, $, traits)
     })
 
@@ -72,16 +54,195 @@ type DynamicScopeParser<parent> = <defs extends Dict>(
 
 export type Types<name extends string = string> = { [k in name]: Type }
 
-export type Scope<types = Types> = {
+type ScopeCache<types = Types> = {
+    nodes: { [def in string]: TypeNode }
+    types: Partial<types>
+}
+
+export class Scope<types = Types> {
+    private cache: ScopeCache<types> = {
+        nodes: {},
+        types: {}
+    }
+
+    // alias for this used to avoid TS errors when passed to a function
+    $: Scope<Types>
     type: TypeParser<types>
     extend: ScopeParser<types>
-    compile: () => types
-    infer: inferScope<types>
-    cache: {
-        nodes: { [def in string]: TypeNode }
-        types: types
+
+    constructor(public aliases: { readonly [k in keyof types]: unknown }) {
+        this.$ = this as Scope<Types>
+        this.type = composeTypeParser(this.$)
+        this.extend = composeScopeParser(this.$)
     }
-    aliases: { readonly [k in keyof types]: unknown }
+
+    get infer(): inferScope<types> {
+        return chainableNoOpProxy
+    }
+
+    compile() {
+        const types = {} as types
+        for (const name in this.aliases) {
+            const def = this.aliases[name]
+            types[name] =
+                typeof def === "function"
+                    ? isType(def)
+                        ? def
+                        : def()
+                    : nodeToType(
+                          this.resolveIfIdentifier(
+                              parseDefinition(def, this.$)
+                          ),
+                          this.$,
+                          {}
+                      )
+        }
+        return types
+    }
+
+    resolveIfIdentifier(node: TypeNode): TypeRoot {
+        return typeof node === "string"
+            ? (this.resolve(node) as TypeRoot)
+            : node
+    }
+
+    resolvePredicateIfIdentifier(domain: Domain, predicate: Predicate) {
+        return typeof predicate === "string"
+            ? this.resolvePredicate(predicate, domain)
+            : predicate
+    }
+
+    isExactValue<domain extends Domain>(
+        node: TypeNode,
+        domain: domain
+    ): node is { [_ in domain]: ExactValue<domain> } {
+        const resolution = this.resolveIfIdentifier(node)
+        return (
+            this.nodeExtendsDomain(resolution, domain) &&
+            this.isExactValuePredicate(resolution[domain])
+        )
+    }
+
+    isExactValuePredicate(predicate: Predicate): predicate is ExactValue {
+        return typeof predicate === "object" && "value" in predicate
+    }
+
+    domainsOfNode(node: TypeNode): Domain[] {
+        return keysOf(this.resolveIfIdentifier(node))
+    }
+
+    nodeExtendsDomain<domain extends Domain>(
+        node: TypeNode,
+        domain: domain
+    ): node is DomainSubtypeNode<domain> {
+        const nodeDomains = this.domainsOfNode(node)
+        return nodeDomains.length === 1 && nodeDomains[0] === domain
+    }
+
+    // TODO: Move to parse
+    isResolvable = (name: string) => {
+        return isKeyOf(name, keywords) || this.aliases[name] ? true : false
+    }
+
+    resolve(name: string) {
+        return this.resolveRecurse(name, [])
+    }
+
+    resolveFlat(name: string): TraversalNode {
+        if (isKeyOf(name, keywords)) {
+            return getFlatKeywords()[name]
+        }
+        this.resolveRecurse(name, [])
+        return this.cache.types[name].flat
+    }
+
+    resolveRecurse(name: string, seen: string[]): TypeRoot {
+        if (isKeyOf(name, keywords)) {
+            return keywords[name]
+        }
+        if (isKeyOf(name, this.cache.types)) {
+            return this.cache.types[name].root as TypeRoot
+        }
+        if (!this.aliases[name]) {
+            return throwInternalError(
+                `Unexpectedly failed to resolve alias '${name}'`
+            )
+        }
+        let root = parseDefinition(this.aliases[name], this.$)
+        if (typeof root === "string") {
+            if (seen.includes(root)) {
+                return throwParseError(
+                    buildShallowCycleErrorMessage(name, seen)
+                )
+            }
+            seen.push(root)
+            root = this.resolveRecurse(root, seen)
+        }
+        this.cache.types[name] = nodeToType(root, this.$, {})
+        return root as TypeRoot
+    }
+
+    resolvePredicate<domain extends Domain>(name: string, domain: domain) {
+        return this.resolvePredicateRecurse(name, domain, [])
+    }
+
+    resolveFlatPredicate(
+        $: Scope,
+        name: string,
+        domain: Domain
+    ): TraversalPredicate {
+        const flatResolution = this.resolveFlat(name)
+        if (typeof flatResolution === "string") {
+            if (flatResolution !== domain) {
+                return throwUnexpectedPredicateDomainError(name, domain)
+            }
+            // an empty predicate is satisfied by its domain alone
+            return []
+        }
+        if (flatResolution[0][0] === "domains") {
+            const predicate = flatResolution[0][1][domain]
+            if (predicate === undefined) {
+                return throwUnexpectedPredicateDomainError(name, domain)
+            }
+            return predicate
+        }
+        return (
+            flatResolution[0][0] === "domain"
+                ? flatResolution.slice(1)
+                : flatResolution
+        ) as TraversalPredicate
+    }
+
+    resolvePredicateRecurse<domain extends Domain>(
+        name: string,
+        domain: domain,
+        seen: string[]
+    ): ResolvedPredicate<domain, Scope> {
+        const resolution = this.resolve(name)[domain]
+        if (resolution === undefined) {
+            return throwUnexpectedPredicateDomainError(name, domain)
+        }
+        if (typeof resolution !== "string") {
+            return resolution as any
+        }
+        if (seen.includes(resolution)) {
+            return throwParseError(
+                buildShallowCycleErrorMessage(resolution, seen)
+            )
+        }
+        seen.push(resolution)
+        return this.resolvePredicateRecurse(resolution, domain, seen)
+    }
+
+    memoizedParse(def: string): TypeNode {
+        if (def in this.cache) {
+            return this.cache.nodes[def]
+        }
+        const root =
+            maybeNaiveParse(def, this.$) ?? fullStringParse(def, this.$)
+        this.cache.nodes[def] = deepFreeze(root)
+        return root
+    }
 }
 
 type parseScope<aliases> = evaluate<{
@@ -120,3 +281,20 @@ export const scope: ScopeParser<{}> = composeScopeParser()
 const rootScope = composeScopeParser()({})
 
 export const type: TypeParser<{}> = composeTypeParser(rootScope)
+
+const throwUnexpectedPredicateDomainError = (
+    name: string,
+    expectedDomain: Domain
+) =>
+    throwInternalError(
+        `Expected '${name}' to have a definition including '${expectedDomain}'`
+    )
+
+export const buildShallowCycleErrorMessage = (name: string, seen: string[]) =>
+    `Alias '${name}' has a shallow resolution cycle: ${[...seen, name].join(
+        "=>"
+    )}`
+
+export type DomainSubtypeNode<domain extends Domain> = {
+    readonly [k in domain]: defined<TypeRoot[domain]>
+}
