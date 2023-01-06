731d610aa66521efb8a5b5e103e73287e40a1e30
David Blass <david@arktype.io>
2023-01-06 14:41:57 -0500

    add experiments

--- examples/experiments.ts
+++ examples/experiments.ts
@@ -1,16 +1,68 @@
 // @ts-nocheck
 
-const date = type("date", {
+import { scope, type } from "../api"
+
+const date = type("Date", {
     from: {
         string: (s) => {
             return new Date(s)
         }
+    },
+    to: {
+        string: (d: Date) => {
+            return d.toString()
+        }
     }
 })
+
+const user = type(
+    {
+        name: "string",
+        age: "number"
+    },
+    {
+        from: {
+            string: (s) => {
+                return JSON.parse(s)
+            }
+        },
+        to: (user): InternalUser => {
+            return { ...user }
+        }
+        // to: {
+        //     string: (user) => {
+        //         return JSON.stringify(user)
+        //     },
+        //     length: (user) => user.name.length,
+        //     validate: (user): InternalUser => {
+        //         return { ...user }
+        //     }
+        // }
+    }
+)
+
+const { data } = user.check({})
+// OR
+const { data } = user({}).to("validatedUser")
+
+const { data } = user({}).to()
+
+// e.g. from accepts UserInput
+const userData = user.from({})
 
 // There is some "string input", and a definition for how it is transformed
 // What is the type of Data
 // Maybe there are some morphs for outgoing types
-const { data, problems } = date("string", "5/12/2023")
+const { data, problems } = date.from("string", "5/12/2023")
 
 // Arbitrary input/output types on morphs vs key names representing defined types
+
+const $ = scope({
+    dateString: "string",
+    date: type("Date", {
+        from: {
+            string: "parseDate"
+        }
+    }),
+    parseDate: ["string", "=>", (s) => new Date(s)]
+})
