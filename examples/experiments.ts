c4f8e8de97d6b0a0637dda44ba3175518a301e9f
David Blass <david@arktype.io>
2023-01-07 13:23:01 -0500

    update experiments

--- examples/experiments.ts
+++ examples/experiments.ts
@@ -16,54 +16,12 @@ const date = type("Date", {
     }
 })
 
-const user = type(
-    {
-        name: "string",
-        age: "number"
-    },
-    {
-        from: {
-            string: (s) => {
-                return JSON.parse(s)
-            }
-        },
-        to: (user): InternalUser => {
-            return { ...user, isInternal: true }
-        }
-        // to: {
-        //     string: (user) => {
-        //         return JSON.stringify(user)
-        //     },
-        //     length: (user) => user.name.length,
-        //     validate: (user): InternalUser => {
-        //         return { ...user }
-        //     }
-        // }
-    }
-)
-
-const { data } = user.check({})
-// OR
-const { data } = user({}).to("validatedUser")
-
-const { data } = user({}).to()
-
-// e.g. from accepts UserInput
-const userData = user.from({})
-
-// e.g. from accepts UserInput
-const { data, problems } = user({})
-
-user.from("string", '{"name": "David", "age": 29}')
-
-const { data } = user({ name: "David", age: 29 })
-
-// There is some "string input", and a definition for how it is transformed
-// What is the type of Data
-// Maybe there are some morphs for outgoing types
-const { data, problems } = date.from("string", "5/12/2023")
-
 // Arbitrary input/output types on morphs vs key names representing defined types
+
+// TODO: Change to type to always attach directly to output,  still be chainable
+// out name would be arbitrary, could make out a special case of to
+// if data specified, just override existing data
+// problems should not be used as a name
 
 const user = type(
     {
@@ -74,10 +32,10 @@ const user = type(
         in: (s: string) => {
             return JSON.parse(s)
         },
-        out: (user): ValidatedUser => {
-            return { ...user, isInternal: true }
-        },
-        to: {
+        out: {
+            out: (user): ValidatedUser => {
+                return { ...user, isInternal: true }
+            },
             string: (user) => {
                 return JSON.stringify(user)
             },
@@ -87,19 +45,4 @@ const user = type(
     }
 )
 
-type User = typeof user.infer
-
-const { out, data, problems } = user({ name: "David", age: 29 })
-
-const { data, problems } = user({ name: "David", age: 29 }).to("formattedUser")
-const { data, problems } = user({ name: "David", age: 29 }).outTo(
-    "formattedUser"
-)
-// user.check()
-
-user({ name: "David", age: 29 }).to("admin").to("string")
-
-// TODO: Change to type to always attach directly to output,  still be chainable
-// out name would be arbitrary, could make out a special case of to
-// if data specified, just override existing data
-// problems should not be used as a name
+const { out, data, string, problems } = user({ name: "David", age: 29 })
