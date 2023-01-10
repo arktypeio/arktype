fef9208bfdc39aee968fce6b0407dc3f6ed617d0
David Blass <david@arktype.io>
2023-01-10 12:27:54 -0500

    some more morph operation exam ples

--- examples/experiments.ts
+++ examples/experiments.ts
@@ -68,29 +68,67 @@ type ZZ = {
 
 const z: ZZ = (n) => n
 
+// could also use syntax like ["string", (s)=>{...}, "Date"], if it doesn't break inference
+
 const $ = scope({
     // implementation is string=>unknown, inferred as unknown=>Date
-    date: ["string=>Date", () => {}],
+    date: ["string=>Date", (s) => new Date(s)],
     // implementation unknown=>unknown, inferred as unknown=>Date
     date2: ["=>Date"],
     // implementation string=>Date, inferred as unknown=>Date
     date3: ["string=>"],
     // implementation unknown=>Date, inferred as unknown=>Date;
-    date4: ["=>"]
+    // could work without any tuple definition
+    date4: ["=>"],
+    user: {
+        name: "string",
+        birthday: "date(string)"
+    },
+    person: {
+        birthday: "otherDate(string)"
+    },
+    myType: "user&person"
 })
 
-// &, | work same
+// Union of morphs:
+// If inputs are discriminatable, allow, because we can return a determinsistic result
+// input: (a=>b) | (c=>d)
+// output:
+//   if a is discriminatable from c:
+//        (a|c)=>(b|d)
+//      could also be represented as:
+//        a: b
+//        c: d
+//   else:
+//        Error: Cannot discriminate between a and c.
 
-// input: a & (b=>c)
-// output: (a&b)=>c
+// Union with morph: treat the same as a union of two morphs
+// input: a | (b=>c)
+//   equivalent: (a=>a) | (b=>c)
+// output:
+//    if a discriminatable from c:
+//       (a|b=> a|c)
+//    else:
+//        Error: Cannot discriminate between a and c.
+
+// Intersection of morphs:
 
 // input: (a=>b) & (c=>d)
-// output: Error: Operation between two morphs... try |> instead
+// output: Error: Intersection between two non-equal morphs.
 
+// |> could work like & by default but pipe morphs
 // input: (a=>b) |> (c=>d)
 // output:
 //   if b is a subtype of c:
 //     (a => d)
 //   else:
 //     error: b is not assignable to c
-// EZ MOOOCHI
+
+// Intersection with morphs:
+
+// Just intersect with input
+// input: a & (b=>c)
+// output: (a&b)=>c
+
+// a & (b=>c)
+//
