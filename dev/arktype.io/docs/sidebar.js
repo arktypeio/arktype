0762d23116115b1a17ff382942847039ccb263b0
David Blass <david@arktype.io>
2023-02-05 06:19:29 -0500

    update arktype.io deps

--- dev/arktype.io/docs/sidebar.js
+++ dev/arktype.io/docs/sidebar.js
@@ -5,31 +5,25 @@ module.exports = {
             label: "Tutorial",
             items: [
                 { type: "doc", label: "Intro", id: "intro" },
-                { type: "doc", label: "Scopes", id: "scopes" },
-                {
-                    type: "doc",
-                    label: "Constraints",
-                    id: "constraints"
-                }
-            ]
-        },
-        {
-            type: "category",
-            label: "API",
-            items: [
-                { type: "doc", label: "Type", id: "api/type" },
-                { type: "doc", label: "Scope", id: "api/scope" },
-                { type: "doc", label: "Node", id: "api/TypeNode" }
-            ]
-        },
-        {
-            type: "category",
-            label: "Examples",
-            items: [
-                { type: "doc", label: "Useage", id: "examples/type" },
-                { type: "doc", label: "Errors", id: "examples/error" },
-                { type: "doc", label: "Advanced", id: "examples/advanced" }
+                { type: "doc", label: "Scopes", id: "scopes" }
             ]
         }
+        // {
+        //     type: "category",
+        //     label: "API",
+        //     items: [
+        //         { type: "doc", label: "Type", id: "api/type" },
+        //         { type: "doc", label: "Scope", id: "api/scope" }
+        //     ]
+        // },
+        // {
+        //     type: "category",
+        //     label: "Examples",
+        //     items: [
+        //         { type: "doc", label: "Usage", id: "examples/type" },
+        //         { type: "doc", label: "Errors", id: "examples/error" },
+        //         { type: "doc", label: "Advanced", id: "examples/advanced" }
+        //     ]
+        // }
     ]
 }
