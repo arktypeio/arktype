16d575b40ed8dd57a481cba792fb80e11a848784
Shawn Morreau <shawn@arktype.io>
2023-02-12 19:55:42 -0500

    testBuild

--- dev/scripts/testBuild.ts
+++ dev/scripts/testBuild.ts
@@ -2,17 +2,9 @@ import { shell } from "../runtime/api.ts"
 import { repoDirs } from "./common.ts"
 
 export const testBuild = (outDir: string) => {
-    outDir === repoDirs.denoOut
-        ? shell(
-              `deno run --allow-all --no-check ./dev/attest/cli.ts --skipTypes --cmd mocha`,
-              {
-                  cwd: outDir
-              }
-          )
-        : shell(`node ./dev/attest/cli.js --skipTypes --cmd mocha`, {
-              cwd: outDir
-          })
+    shell(`node ./dev/attest/cli.js --skipTypes --cmd mocha`, {
+        cwd: outDir
+    })
 }
 testBuild(repoDirs.mjsOut)
 testBuild(repoDirs.cjsOut)
-testBuild(repoDirs.denoOut)
