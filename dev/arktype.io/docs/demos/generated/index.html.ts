73802c00af390dc869a3c556b291a1269abec91b
David Blass <david@arktype.io>
2023-02-13 07:46:42 -0500

    docs: fix demo i/o data display

--- dev/arktype.io/docs/demos/generated/index.html.ts
+++ dev/arktype.io/docs/demos/generated/index.html.ts
@@ -2,24 +2,16 @@ export default `<head>
     <link href="http://fonts.cdnfonts.com/css/cascadia-code" rel="stylesheet" />
 </head>
 <div id="demo">
-    <div id="input">
-        <div class="section">
-            <h3>Definition</h3>
-            <div class="card">
-                <pre><code id="definition"></code></pre>
-            </div>
-        </div>
-        <div class="section">
-            <h3>Data</h3>
-            <div class="card">
-                <pre id="data"></pre>
-            </div>
+    <div class="section">
+        <h3>Definition</h3>
+        <div class="card">
+            <pre><code id="definition"></code></pre>
         </div>
     </div>
     <div class="section">
-        <h3>Result</h3>
+        <h3>Output</h3>
         <div class="card">
-            <pre id="result"></pre>
+            <pre><code id="output"></code></pre>
         </div>
     </div>
 </div>
