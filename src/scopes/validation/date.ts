b3e743cc9a4f4c5fdd6ea36486be87dc4d32a3a2
David Blass <david@arktype.io>
2023-02-14 06:19:55 -0500

    more validation improvements

--- src/scopes/validation/date.ts
+++ src/scopes/validation/date.ts
@@ -1,5 +1,4 @@
 import { throwParseError } from "../../utils/errors"
-import { tryParseWellFormedInteger } from "../../utils/numericLiterals"
 
 export type DateDelimiter = "." | "/" | "-"
 
@@ -12,7 +11,7 @@ export type DateOptions = {
 const dateFormatMatcher =
     /(^(y{4}|y{2})[./-](m{1,2})[./-](d{1,2})$)|(^(m{1,2})[./-](d{1,2})[./-]((y{4}|y{2})$))|(^(d{1,2})[./-](m{1,2})[./-]((y{4}|y{2})$))/gi
 
-type ParsedDate = {
+type ParsedDateParts = {
     y?: string
     m?: string
     d?: string
@@ -39,13 +38,13 @@ export const tryParseDate = (
         partPairs.push([dataParts[i], formatParts[i]])
     }
 
-    const parsedDate: ParsedDate = {}
+    const parsedDate: ParsedDateParts = {}
 
     for (const [dataPart, formatPart] of partPairs) {
         if (dataPart.length !== formatPart.length) {
             return
         }
-        parsedDate[formatPart[0] as keyof ParsedDate] = dataPart
+        parsedDate[formatPart[0] as keyof ParsedDateParts] = dataPart
     }
 
     const date = new Date(`${parsedDate.m}/${parsedDate.d}/${parsedDate.y}`)
@@ -53,46 +52,4 @@ export const tryParseDate = (
     if (date.getDate() === parseInt(parsedDate.d!)) {
         return date
     }
-}
-
-// Adapted from https://github.com/validatorjs/validator.js/blob/master/src/lib/isISO8601.js
-const iso8601Matcher =
-    /^([+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-3])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24:?00)([.,]\d+(?!:))?)?(\17[0-5]\d([.,]\d+)?)?([zZ]|([+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
-
-const isLeapYear = (year: number) =>
-    (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
-
-export const isValidDate = (data: string) => {
-    if (!iso8601Matcher.test(data)) {
-        return false
-    }
-    const ordinalMatch = data.match(/^(\d{4})-?(\d{3})([ T]{1}\.*|$)/)
-    if (ordinalMatch) {
-        const year = Number(ordinalMatch[1])
-        const day = Number(ordinalMatch[2])
-        const maxDay = isLeapYear(year) ? 366 : 365
-        return day < maxDay
-    }
-    const {
-        1: year,
-        2: month,
-        3: day
-    } = (data
-        .match(/(\d{4})-?(\d{0,2})-?(\d*)/)
-        ?.map((part) => parseInt(part)) ?? []) as Record<number, number>
-
-    const date = new Date(
-        `${year}-${month ? `0${month}`.slice(-2) : "01"}-${
-            day ? `0${day}`.slice(-2) : "01"
-        }`
-    )
-
-    if (month && day) {
-        return (
-            date.getUTCFullYear() === year &&
-            date.getUTCMonth() + 1 === month &&
-            date.getUTCDate() === day
-        )
-    }
-    return true
 }
