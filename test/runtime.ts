3ceac7fa67cf0b6e046d9c62d7ddb3a9d4e46dec
David Blass <david@arktype.io>
2023-02-14 23:48:14 -0500

    work on gif

--- test/runtime.ts
+++ test/runtime.ts
@@ -2,24 +2,36 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { scope, type } from "arktype"
 
-/**
+/**  // data is "foo"
  *   1. "string"
  *   2. "string|number"
+ *   // data to ["foo"]
  *   3. "string|number[]"
  *   4. "(string|number)[]"
  *   5. "(email|number)[]"
+ *  // data to ["david@arktype.io"]
  *   6. "(email|1<=number)[]"
- *   7. "(email|1<=number<100)[]"
+ *   7. "(email|1<number<=10)[]"
+ *  // data to ["david@arktype.io", "shawn@arktype.io"]
  *   8. {
- *      emailsOrIds: "(email|1<number<10)[]",
- *
+ *      myKey: "(email|1<number<10)[]",
+ *   }
+ *   // data to {myType: ["david@arktype.io", "shawn@arktype.io"]}
+ *   9. {
+ *      myKey: "(email|1<number<10)[]",
+ *      nested: {}
+ *   }
+ *   8. {
+ *      myKey: "(email|1<number<10)[]",
+ *      nested: {
+ *         "optional?": "parsedDate"
+ *      }
  *   }
  */
 
-const myType = type("(email|1<=number<100)[]")
+const myType = type("string|number[]")
 
 type MyType = typeof myType.infer
-//     ^?
 
 const { data, problems } = myType([3.14, ""])
 
