d3cd9ce624713781bd3b89ac5cc445250f66d80f
David Blass <david@arktype.io>
2023-02-20 13:46:31 -0500

    continue working on demo

--- test/demo.ts
+++ test/demo.ts
@@ -1,19 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { scope, type } from "arktype"
-
-
-
-
-
-
-
-
+import { type } from "arktype"
 
 const contributors = type("string|number[]")
-
-export type Contributors = typeof contributors.infer
-//          ^?
 
 const { data, problems } = contributors(["david@arktype.io"])
 
 console.log(data ?? problems.summary)
+//          ^?
