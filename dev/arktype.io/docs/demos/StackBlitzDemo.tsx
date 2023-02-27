aa1da8b2f7178c0281bc551a9b4621d607728850
David Blass <david@arktype.io>
2023-02-27 00:52:15 -0500

    docs: arktype.io polish (#658)

--- dev/arktype.io/docs/demos/StackBlitzDemo.tsx
+++ dev/arktype.io/docs/demos/StackBlitzDemo.tsx
@@ -1,4 +1,4 @@
-import { Box, CircularProgress } from "@mui/material"
+import { LinearProgress, Stack } from "@mui/material"
 import React, { useEffect, useState } from "react"
 import type { DemoProps } from "./stackblitzGenerators/index"
 import {
@@ -6,62 +6,44 @@ import {
     DEMO_ELEMENT_ID
 } from "./stackblitzGenerators/index"
 
-const DEMO_LOADER_ID = "demo-loader"
-
 export const StackBlitzDemo = (demoProps: DemoProps) => {
     const [isLoading, setIsLoading] = useState(true)
     useEffect(() => {
-        ;(async () => {
-            window.document
-                .getElementById(DEMO_LOADER_ID)
-                ?.scrollIntoView({ behavior: "smooth", block: "center" })
-            const vm = await createStackblitzDemo(demoProps)
-            // hack to workaround a caching issue where tsconfig is not applied until it is modified
-            setTimeout(
-                () =>
-                    vm.applyFsDiff({
-                        create: {
-                            "tsconfig.json": JSON.stringify(
-                                {
-                                    compilerOptions: {
-                                        module: "esnext",
-                                        target: "esnext",
-                                        strict: true
-                                    }
-                                },
-                                null,
-                                4
-                            )
-                        },
-                        destroy: []
-                    }),
-                5000
-            )
-            setIsLoading(false)
-        })()
+        activateDemo(demoProps, setIsLoading)
     }, [])
     return (
-        <Box
-            style={{
-                width: "100%",
-                height: "660px",
-                border: 0,
-                marginLeft: -8,
-                marginRight: -8,
-                padding: 16,
-                overflow: "hidden",
-                borderRadius: "8px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center"
-            }}
-        >
-            <CircularProgress
-                style={{ position: "absolute", opacity: isLoading ? 1 : 0 }}
-                color="secondary"
-                id={DEMO_LOADER_ID}
-            />
-            <div style={{ opacity: isLoading ? 0 : 1 }} id={DEMO_ELEMENT_ID} />
-        </Box>
+        <Stack width="100%" height="600px">
+            {isLoading ? <LinearProgress /> : null}
+            <div id={DEMO_ELEMENT_ID} />
+        </Stack>
     )
+}
+
+const activateDemo = async (
+    props: DemoProps,
+    setIsLoading: (value: boolean) => void
+) => {
+    const vm = await createStackblitzDemo(props)
+    // hack to workaround a caching issue where tsconfig is not applied until it is modified
+    setTimeout(
+        () =>
+            vm.applyFsDiff({
+                create: {
+                    "tsconfig.json": JSON.stringify(
+                        {
+                            compilerOptions: {
+                                module: "esnext",
+                                target: "esnext",
+                                strict: true
+                            }
+                        },
+                        null,
+                        4
+                    )
+                },
+                destroy: []
+            }),
+        5000
+    )
+    setIsLoading(false)
 }
--- dev/arktype.io/src/pages/index.tsx
+++ dev/arktype.io/src/pages/index.tsx
@@ -1,82 +1,19 @@
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
-import useIsBrowser from "@docusaurus/useIsBrowser"
-import { Stack, ThemeProvider, Typography } from "@mui/material"
+import { ThemeProvider } from "@mui/material"
 import Layout from "@theme/Layout"
 import React from "react"
-import { Boat } from "../components/boat"
-import { Features } from "../components/features"
+import { Contents } from "../components/contents"
+import { Header } from "../components/header"
 import { getTheme } from "../components/index"
-import { LogoCloud } from "../components/logoCloud"
-import { ToggleableDemo } from "../components/toggleableDemo"
-
-const Contents = () => {
-    const { siteConfig } = useDocusaurusContext()
-    return (
-        <ThemeProvider theme={getTheme()}>
-            <Header title={siteConfig.title} tagline={siteConfig.tagline} />
-            <main>
-                <Stack alignItems="center" spacing={1} padding={2}>
-                    <video
-                        autoPlay
-                        loop
-                        muted
-                        style={{
-                            maxWidth: "60em",
-                            width: "100%",
-                            borderRadius: 8
-                        }}
-                        src="/img/arktype.mp4"
-                    />
-                    <Features />
-                    <ToggleableDemo embedId="type" />
-                </Stack>
-            </main>
-        </ThemeProvider>
-    )
-}
 
 export default () => {
     const { siteConfig } = useDocusaurusContext()
     return (
         <Layout title={siteConfig.title} description={siteConfig.tagline}>
-            <Contents />
+            <ThemeProvider theme={getTheme()}>
+                <Header title={siteConfig.title} tagline={siteConfig.tagline} />
+                <Contents />
+            </ThemeProvider>
         </Layout>
-    )
-}
-
-const Header = ({ title, tagline }: Record<string, string>) => {
-    return (
-        <header
-            style={{
-                height: 180,
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center"
-            }}
-        >
-            <LogoCloud names={["typescript", "vscode", "intellij", "vim"]} />
-            <Stack flexGrow={1}>
-                <Typography component="h1" variant="h2" color="secondary">
-                    {title}
-                </Typography>
-                <Typography
-                    component="h2"
-                    variant={
-                        useIsBrowser() && window.screen.width < 1000
-                            ? "h6"
-                            : "h5"
-                    }
-                    color="common.white"
-                    style={{
-                        whiteSpace: "nowrap"
-                    }}
-                >
-                    {tagline}
-                </Typography>
-            </Stack>
-            <LogoCloud names={["javascript", "chromium", "node", "deno"]} />
-            <Boat />
-        </header>
     )
 }
