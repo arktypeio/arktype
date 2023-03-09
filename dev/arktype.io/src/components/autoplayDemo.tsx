d61028fe175b59f7dc52d671c93c2a01cfe28e67
David Blass <david@arktype.io>
2023-03-09 11:10:24 -0500

    docs: fix playsInline attribute for demo

--- dev/arktype.io/src/components/autoplayDemo.tsx
+++ dev/arktype.io/src/components/autoplayDemo.tsx
@@ -11,7 +11,7 @@ export const AutoplayDemo = (props: AutoplayDemoProps) => (
         loop
         controls={true}
         // required for autoplay on Safari
-        playsinline
+        playsInline
         // there's no audio, but required for autoplay on Chrome
         muted
         // picture in picture doesn't work well since the page is designed around the demo
