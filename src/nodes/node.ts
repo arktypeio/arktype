2febf46f8ebae5d1e191e573e41ca15e46996044
David Blass <david@arktype.io>
2023-03-22 20:50:33 -0400

    fix compare algorithm

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,12 +1,7 @@
-import type { Type } from "../scopes/type.ts"
 import type { Domain, inferDomain } from "../utils/domains.ts"
-import { domainDescriptions } from "../utils/domains.ts"
-import { throwInternalError } from "../utils/errors.ts"
-import type { Dict, mutable } from "../utils/generics.ts"
-import { hasKey, keysOf } from "../utils/generics.ts"
 import type { Compilation } from "./compile.ts"
-import type { IntersectionResult, IntersectionState } from "./compose.ts"
-import { BaseNode } from "./compose.ts"
+import type { IntersectionResult } from "./compose.ts"
+import { BaseNode, IntersectionState } from "./compose.ts"
 import { mappedKeys } from "./rules/props.ts"
 import type { BranchNode, LiteralRules } from "./rules/rules.ts"
 
@@ -34,8 +29,10 @@ export class TypeNode extends BaseNode {
             ...comparison.equalIndexPairs.map(
                 (indices) => this.branches[indices[0]]
             ),
-            ...comparison.subtypeIndices.map((lIndex) => this.branches[lIndex]),
-            ...comparison.supertypeIndices.map(
+            ...comparison.lStrictSubtypeIndices.map(
+                (lIndex) => this.branches[lIndex]
+            ),
+            ...comparison.rStrictSubtypeIndices.map(
                 (rIndex) => node.branches[rIndex]
             )
         ]
@@ -45,53 +42,53 @@ export class TypeNode extends BaseNode {
         return {
             result: new TypeNode([]) as this,
             isSubtype:
-                comparison.subtypeIndices.length +
-                    comparison.equalIndexPairs.length ===
-                node.branches.length,
-            isSupertype:
-                comparison.supertypeIndices.length +
+                comparison.lStrictSubtypeIndices.length +
                     comparison.equalIndexPairs.length ===
                 this.branches.length,
+            isSupertype:
+                comparison.rStrictSubtypeIndices.length +
+                    comparison.equalIndexPairs.length ===
+                node.branches.length,
             isDisjoint: false
         }
     }
 
-    // union(node: this, s: IntersectionState) {
-    //     const state = new IntersectionState(type, "|")
-    //     const comparison = this.compare(node, state)
-    //     if (!isBranchComparison(comparison)) {
-    //         // return isEquality(comparison) || comparison === l
-    //         //     ? r
-    //         //     : comparison === r
-    //         //     ? l
-    //         //     : // if a boolean has multiple branches, neither of which is a
-    //         //     // subtype of the other, it consists of two opposite literals
-    //         //     // and can be simplified to a non-literal boolean.
-    //         //     domain === "boolean"
-    //         //     ? true
-    //         //     : ([emptyRulesIfTrue(l), emptyRulesIfTrue(r)] as [
-    //         //           Branch,
-    //         //           Branch
-    //         //       ])
-    //     }
-    //     const resultBranches = [
-    //         ...this.json.filter(
-    //             (_, lIndex) =>
-    //                 !comparison.subtypes.includes(lIndex) &&
-    //                 !comparison.equalities.some(
-    //                     (indexPair) => indexPair[0] === lIndex
-    //                 )
-    //         ),
-    //         ...node.json.filter(
-    //             (_, rIndex) =>
-    //                 !comparison.supertypes.includes(rIndex) &&
-    //                 !comparison.equalities.some(
-    //                     (indexPair) => indexPair[1] === rIndex
-    //                 )
-    //         )
-    //     ]
-    //     return resultBranches.length === 1 ? resultBranches[0] : resultBranches
-    // }
+    union(node: this) {
+        const state = new IntersectionState(type, "|")
+        const comparison = this.compare(node, state)
+        if (!isBranchComparison(comparison)) {
+            // return isEquality(comparison) || comparison === l
+            //     ? r
+            //     : comparison === r
+            //     ? l
+            //     : // if a boolean has multiple branches, neither of which is a
+            //     // subtype of the other, it consists of two opposite literals
+            //     // and can be simplified to a non-literal boolean.
+            //     domain === "boolean"
+            //     ? true
+            //     : ([emptyRulesIfTrue(l), emptyRulesIfTrue(r)] as [
+            //           Branch,
+            //           Branch
+            //       ])
+        }
+        const resultBranches = [
+            ...this.branches.filter(
+                (_, lIndex) =>
+                    !comparison.lStrictSubtypeIndices.includes(lIndex) &&
+                    !comparison.equalIndexPairs.some(
+                        (indexPair) => indexPair[0] === lIndex
+                    )
+            ),
+            ...node.branches.filter(
+                (_, rIndex) =>
+                    !comparison.rStrictSubtypeIndices.includes(rIndex) &&
+                    !comparison.equalIndexPairs.some(
+                        (indexPair) => indexPair[1] === rIndex
+                    )
+            )
+        ]
+        return resultBranches.length === 1 ? resultBranches[0] : resultBranches
+    }
 
     toArray() {
         return {
@@ -111,11 +108,20 @@ export class TypeNode extends BaseNode {
             equalIndexPairs: [],
             intersections: []
         }
-        const rSubtypeIndices: Record<number, true> = {}
+        // Each rBranch is initialized to an empty array to which distinct
+        // intersections will be appended. If the rBranch is identified as a
+        // subtype (or equal) of any lBranch, the corresponding value should be
+        // set to null so we can avoid including previous/future intersections
+        // in the final result.
+        const intersectionsByR: (BranchNode[] | null)[] = node.branches.map(
+            () => []
+        )
         for (let lIndex = 0; lIndex < this.branches.length; lIndex++) {
             const intersectionsOfL: BranchNode[] = []
             for (let rIndex = 0; rIndex < node.branches.length; rIndex++) {
-                if (rSubtypeIndices[rIndex]) {
+                if (!intersectionsByR[rIndex]) {
+                    // we've identified this rBranch as a subtype of
+                    // an lBranch and will not yield any distinct intersections.
                     continue
                 }
                 const intersection = this.branches[lIndex].intersect(
@@ -123,22 +129,36 @@ export class TypeNode extends BaseNode {
                     state
                 )
                 if (intersection.isDisjoint) {
+                    // doesn't tell us about any redundancies or add a distinct intersection
                     continue
                 }
                 if (intersection.isSubtype) {
                     if (intersection.isSupertype) {
-                        rSubtypeIndices[rIndex] = true
+                        // If branches are equal, execute logic explained in supertype case.
+                        intersectionsByR[rIndex] = null
                         comparison.equalIndexPairs.push([lIndex, rIndex])
                     } else {
                         comparison.lStrictSubtypeIndices.push(lIndex)
                     }
+                    // If l is a subtype of the current r branch, intersections
+                    // with previous and remaining branches of r won't lead to
+                    // distinct intersections, so empty lIntersections and break
+                    // from the inner loop.
                     intersectionsOfL.length = 0
                     break
                 }
                 if (intersection.isSupertype) {
-                    rSubtypeIndices[rIndex] = true
+                    // If r is a subtype of the current l branch, we set its
+                    // intersections to null, removing any previous
+                    // intersections including it and preventing any of its
+                    // remaining intersections from being computed.
+                    intersectionsByR[rIndex] = null
                     comparison.rStrictSubtypeIndices.push(rIndex)
                 } else {
+                    // If neither l nor r is a subtype of the other, add their
+                    // intersection as a candidate for the final result (could
+                    // still be removed if it is determined l or r is a subtype
+                    // of a remaining branch).
                     intersectionsOfL.push(intersection.result)
                 }
             }
@@ -146,15 +166,15 @@ export class TypeNode extends BaseNode {
         }
         return comparison
     }
-
-    // const state = new IntersectionState(type, "&")
-    // const result = nodeIntersection(l, r, state)
-    // return isDisjoint(result)
-    //     ? throwParseError(compileDisjointReasonsMessage(state.disjoints))
-    //     : isEquality(result)
-    //     ? l
-    //     : result
 }
+
+// const state = new IntersectionState(type, "&")
+// const result = nodeIntersection(l, r, state)
+// return isDisjoint(result)
+//     ? throwParseError(compileDisjointReasonsMessage(state.disjoints))
+//     : isEquality(result)
+//     ? l
+//     : result
 
 // export type ConfigNode<$ = Dict> = {
 //     config: TypeConfig
