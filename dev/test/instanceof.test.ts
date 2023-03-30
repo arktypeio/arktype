5f366a72ddbbfe19eab6dd2da7dcbd8d8b99d252
Shawn Morreau <shawn@arktype.io>
2023-03-30 14:24:27 -0400

    add 17 to testTsVersions, update to ts-morph 18

--- dev/test/instanceof.test.ts
+++ dev/test/instanceof.test.ts
@@ -56,7 +56,7 @@ describe("instanceof", () => {
                 "Expected a constructor following 'instanceof' operator (was number)."
             )
             .type.errors(
-                "Argument of type 'number' is not assignable to parameter of type 'constructor<unknown>'."
+                "Argument of type 'number' is not assignable to parameter of type 'constructor"
             )
     })
 })
