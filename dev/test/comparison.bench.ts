d5d6fb237ad6053322f583d4f19b8a9c1a75d4b0
David Blass <david@arktype.io>
2023-04-23 17:49:18 -0400

    add comparison instantiations

--- dev/test/comparison.bench.ts
+++ dev/test/comparison.bench.ts
@@ -17,144 +17,148 @@ const validInput = {
     }
 }
 
-const arkType = type({
-    number: "number",
-    negNumber: "number",
-    maxNumber: "number",
-    string: "string",
-    longString: "string",
-    boolean: "boolean",
-    deeplyNested: {
-        foo: "string",
-        num: "number",
-        bool: "boolean"
-    }
-})
-
-const zodType = z.object({
-    number: z.number(),
-    negNumber: z.number(),
-    maxNumber: z.number(),
-    string: z.string(),
-    longString: z.string(),
-    boolean: z.boolean(),
-    deeplyNested: z.object({
-        foo: z.string(),
-        num: z.number(),
-        bool: z.boolean()
+bench("ark instantiations", () => {
+    const arkType = type({
+        number: "number",
+        negNumber: "number",
+        maxNumber: "number",
+        string: "string",
+        longString: "string",
+        boolean: "boolean",
+        deeplyNested: {
+            foo: "string",
+            num: "number",
+            bool: "boolean"
+        }
     })
-})
+}).type([6016, "instantiations"])
 
-const arkCheck = (data: any) => {
-    const state = new (globalThis as any).$ark.state()
-    if (
-        !(
-            (typeof data === "object" && data !== null) ||
-            typeof data === "function"
-        )
-    ) {
-        state.addProblem("domain", "object", data, [])
-    }
-    // state.pushUnion()
-    // ;(() => {
-    //     if (!(data.boolean === true)) {
-    //         return state.addProblem("value", ["===", true], data.boolean, [])
-    //     }
-    // })() &&
-    //     (() => {
-    //         if (!(data.boolean === false)) {
-    //             return state.addProblem(
-    //                 "value",
-    //                 ["===", false],
-    //                 data.boolean,
-    //                 []
-    //             )
-    //         }
-    //     })()
-    // state.popUnion(2, data.boolean, [])
-    // if (
-    //     !(
-    //         (typeof data.deeplyNested === "object" &&
-    //             data.deeplyNested !== null) ||
-    //         typeof data.deeplyNested === "function"
-    //     )
-    // ) {
-    //     state.addProblem("domain", "object", data.deeplyNested, [])
-    // }
-    // state.pushUnion()
-    // ;(() => {
-    //     if (!(data.deeplyNested.bool === true)) {
-    //         return state.addProblem(
-    //             "value",
-    //             ["===", true],
-    //             data.deeplyNested.bool,
-    //             []
-    //         )
-    //     }
-    // })() &&
-    //     (() => {
-    //         if (!(data.deeplyNested.bool === false)) {
-    //             return state.addProblem(
-    //                 "value",
-    //                 ["===", false],
-    //                 data.deeplyNested.bool,
-    //                 []
-    //             )
-    //         }
-    //     })()
-    // state.popUnion(2, data.deeplyNested.bool, [])
-    if (!(typeof data.boolean === "boolean")) {
-        state.addProblem("domain", "boolean", data.boolean, [])
-    }
-    if (!(typeof data.deeplyNested.bool === "boolean")) {
-        state.addProblem("domain", "boolean", data.deeplyNested.bool, [])
-    }
-    if (!(typeof data.deeplyNested.foo === "string")) {
-        state.addProblem("domain", "string", data.deeplyNested.foo, [])
-    }
-    if (!(typeof data.deeplyNested.num === "number")) {
-        state.addProblem("domain", "number", data.deeplyNested.num, [])
-    }
-    if (!(typeof data.longString === "string")) {
-        state.addProblem("domain", "string", data.longString, [])
-    }
-    if (!(typeof data.maxNumber === "number")) {
-        state.addProblem("domain", "number", data.maxNumber, [])
-    }
-    if (!(typeof data.negNumber === "number")) {
-        state.addProblem("domain", "number", data.negNumber, [])
-    }
-    if (!(typeof data.number === "number")) {
-        state.addProblem("domain", "number", data.number, [])
-    }
-    if (!(typeof data.string === "string")) {
-        state.addProblem("domain", "string", data.string, [])
-    }
-    return state.finalize(data)
-}
+bench("zod instantiations", () => {
+    const zodType = z.object({
+        number: z.number(),
+        negNumber: z.number(),
+        maxNumber: z.number(),
+        string: z.string(),
+        longString: z.string(),
+        boolean: z.boolean(),
+        deeplyNested: z.object({
+            foo: z.string(),
+            num: z.number(),
+            bool: z.boolean()
+        })
+    })
+}).type([19688, "instantiations"])
 
-const dataArray = [...new Array(1000)].map((_, i) => ({
-    ...validInput,
-    number: i
-}))
+// const arkCheck = (data: any) => {
+//     const state = new (globalThis as any).$ark.state()
+//     if (
+//         !(
+//             (typeof data === "object" && data !== null) ||
+//             typeof data === "function"
+//         )
+//     ) {
+//         state.addProblem("domain", "object", data, [])
+//     }
+//     // state.pushUnion()
+//     // ;(() => {
+//     //     if (!(data.boolean === true)) {
+//     //         return state.addProblem("value", ["===", true], data.boolean, [])
+//     //     }
+//     // })() &&
+//     //     (() => {
+//     //         if (!(data.boolean === false)) {
+//     //             return state.addProblem(
+//     //                 "value",
+//     //                 ["===", false],
+//     //                 data.boolean,
+//     //                 []
+//     //             )
+//     //         }
+//     //     })()
+//     // state.popUnion(2, data.boolean, [])
+//     // if (
+//     //     !(
+//     //         (typeof data.deeplyNested === "object" &&
+//     //             data.deeplyNested !== null) ||
+//     //         typeof data.deeplyNested === "function"
+//     //     )
+//     // ) {
+//     //     state.addProblem("domain", "object", data.deeplyNested, [])
+//     // }
+//     // state.pushUnion()
+//     // ;(() => {
+//     //     if (!(data.deeplyNested.bool === true)) {
+//     //         return state.addProblem(
+//     //             "value",
+//     //             ["===", true],
+//     //             data.deeplyNested.bool,
+//     //             []
+//     //         )
+//     //     }
+//     // })() &&
+//     //     (() => {
+//     //         if (!(data.deeplyNested.bool === false)) {
+//     //             return state.addProblem(
+//     //                 "value",
+//     //                 ["===", false],
+//     //                 data.deeplyNested.bool,
+//     //                 []
+//     //             )
+//     //         }
+//     //     })()
+//     // state.popUnion(2, data.deeplyNested.bool, [])
+//     if (!(typeof data.boolean === "boolean")) {
+//         state.addProblem("domain", "boolean", data.boolean, [])
+//     }
+//     if (!(typeof data.deeplyNested.bool === "boolean")) {
+//         state.addProblem("domain", "boolean", data.deeplyNested.bool, [])
+//     }
+//     if (!(typeof data.deeplyNested.foo === "string")) {
+//         state.addProblem("domain", "string", data.deeplyNested.foo, [])
+//     }
+//     if (!(typeof data.deeplyNested.num === "number")) {
+//         state.addProblem("domain", "number", data.deeplyNested.num, [])
+//     }
+//     if (!(typeof data.longString === "string")) {
+//         state.addProblem("domain", "string", data.longString, [])
+//     }
+//     if (!(typeof data.maxNumber === "number")) {
+//         state.addProblem("domain", "number", data.maxNumber, [])
+//     }
+//     if (!(typeof data.negNumber === "number")) {
+//         state.addProblem("domain", "number", data.negNumber, [])
+//     }
+//     if (!(typeof data.number === "number")) {
+//         state.addProblem("domain", "number", data.number, [])
+//     }
+//     if (!(typeof data.string === "string")) {
+//         state.addProblem("domain", "string", data.string, [])
+//     }
+//     return state.finalize(data)
+// }
 
-bench("arktype", () => {
-    for (let i = 0; i < 1000; i++) {
-        arkType(dataArray[i])
-    }
-}).median([600, "us"])
+// const dataArray = [...new Array(1000)].map((_, i) => ({
+//     ...validInput,
+//     number: i
+// }))
 
-bench("arktype check", () => {
-    for (let i = 0; i < 1000; i++) {
-        arkCheck(dataArray[i])
-    }
-}).median([46.16, "us"])
+// bench("arktype", () => {
+//     for (let i = 0; i < 1000; i++) {
+//         arkType(dataArray[i])
+//     }
+// }).median([600, "us"])
 
-bench("arktype allows", () => {
-    for (let i = 0; i < 1000; i++) {
-        arkType.root(dataArray[i])
-    }
-}).median([6.77, "us"])
+// bench("arktype check", () => {
+//     for (let i = 0; i < 1000; i++) {
+//         arkCheck(dataArray[i])
+//     }
+// }).median([46.16, "us"])
+
+// bench("arktype allows", () => {
+//     for (let i = 0; i < 1000; i++) {
+//         arkType.root(dataArray[i])
+//     }
+// }).median([6.77, "us"])
 
 // bench("zod", () => {
 //     for (let i = 0; i < 1000; i++) {
