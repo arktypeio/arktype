df144bc2c5c66edf10ca475095ca1f1d3a68a29e
David Blass <david@arktype.io>
2023-04-30 22:12:07 -0400

    ci: add name to dev/scripts to fix changelog

--- dev/scripts/findUnusedExports.ts
+++
@@ -1,7 +0,0 @@
-import {
-    findUnusedExports,
-    logUnusedExportsToConsole
-} from "./docgen/api/findUnusedExports.js"
-
-const unusedExports = findUnusedExports()
-logUnusedExportsToConsole(unusedExports)
