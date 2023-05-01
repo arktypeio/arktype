ed880dbb8103459a7c71cf4560e1ca04f4eeb99c
David Blass <david@arktype.io>
2023-04-30 23:16:01 -0400

    ci: fix repoBuild

--- dev/scripts/build.ts
+++ dev/scripts/build.ts
@@ -1,12 +1,16 @@
-import { rmSync } from "node:fs"
+import { cpSync, rmSync } from "node:fs"
 import { join } from "node:path"
 import { readJson, shell, writeJson } from "../attest/src/main.js"
 import { repoDirs } from "./common.js"
 
-console.log(`🔨 Building arktype...`)
-rmSync(repoDirs.outRoot, { recursive: true, force: true })
+const packageRoot = process.cwd()
+const outRoot = join(packageRoot, "dist")
+const packageJson = readJson(join(packageRoot, "package.json"))
+
+console.log(`🔨 Building ${packageJson.name}...`)
+rmSync(outRoot, { recursive: true, force: true })
 const tsConfigData = readJson(join(repoDirs.configs, "tsconfig.json"))
-const tempTsConfig = join(repoDirs.root, "tsconfig.temp.json")
+const tempTsConfig = join(packageRoot, "tsconfig.temp.json")
 writeJson(tempTsConfig, {
     ...tsConfigData,
     include: ["src"],
@@ -14,13 +18,19 @@ writeJson(tempTsConfig, {
         ...tsConfigData.compilerOptions,
         noEmit: false,
         module: "commonjs",
-        outDir: "dist",
-        rootDir: "src"
+        outDir: "dist"
     }
 })
 try {
     shell(`pnpm tsc --project ${tempTsConfig}`)
+    const outSrc = join(outRoot, "src")
+    // not sure which setting to change to get it to compile here in the first place
+    cpSync(outSrc, outRoot, {
+        recursive: true,
+        force: true
+    })
+    rmSync(outSrc, { recursive: true, force: true })
 } finally {
     rmSync(tempTsConfig, { force: true })
 }
-console.log(`📦 Successfully built arktype!`)
+console.log(`📦 Successfully built ${packageJson.name}!`)
