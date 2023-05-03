cde70581df63b6bc5c0ccaa0aece9b581d96a7e9
ShawnMorreau <Shawn.morreau@hotmail.com>
2023-05-03 16:02:20 -0700

    date kinda works

--- dev/test/date.test.ts
+++ dev/test/date.test.ts
@@ -4,21 +4,27 @@ import { parametersOf } from "../../src/utils/generics.js"
 import { attest } from "../attest/main.js"
 
 const date = (args: parametersOf<typeof Date>) => new Date(args).valueOf()
+// const d = (input: Date | string | number) => {
+//     if (input instanceof Date) {
+//         return input
+//     }
+//     try {
+//         const date = new Date(input)
+//         if (Number.isNaN(date)) {
+//             throw new Error()
+//         }
+//         return date
+//     } catch {
+//         throwParseError(
+//             "input must be a valid Date, well formed date string, or number"
+//         )
+//     }
+// }
 const d = (input: Date | string | number) => {
     if (input instanceof Date) {
-        return input.valueOf()
+        return input
     }
-    try {
-        const date = new Date(input).valueOf()
-        if (Number.isNaN(date)) {
-            throw new Error()
-        }
-        return date
-    } catch {
-        throwParseError(
-            "input must be a valid Date, well formed date string, or number"
-        )
-    }
+    return new Date(input)
 }
 type a = parametersOf<typeof Date>
 //   ^?
@@ -26,8 +32,18 @@ type ab = parametersOf<Date>
 //   ^?
 describe("test", () => {
     it("tests", () => {
-        // const t = type(${d("1/1/2020")}<Date<=${d(Date.now()))
+        // const t = type(`${d("1/1/2020")}<Date<=${d(Date.now())}`)
         const t = type(`Date<2`)
-        attest(t(d(-2)).problems?.summary).snap("Must be a Date (was Number)")
+        const date = d("10/25/1996")
+        attest(t(date).problems?.summary).snap("Must be less than 2 (was 0)")
+        attest(date.toString()).snap(
+            "Fri Oct 25 1996 00:00:00 GMT-0700 (Pacific Daylight Saving Time)"
+        )
+        // const t = type("Date<2")
+        // const tt = t(new Date("1/1/1955"))
+        // attest(tt.data.toString()).snap(
+        //     "Sat Jan 01 1955 00:00:00 GMT-0800 (Pacific Standard Time)"
+        // )
+        // attest(t(d(-2)).problems?.summary).snap("a")
     })
 })
