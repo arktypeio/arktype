d353cf6518c114aa0437c3ff2c1a821dcba3de74
David Blass <david@arktype.io>
2023-05-12 12:59:00 -0400

    fix syntax email list size

--- dev/examples/syntax.ts
+++ dev/examples/syntax.ts
@@ -31,7 +31,7 @@ export const validationSyntax = type({
     embeddedRegexLiteral: "email&/@arktype\\.io/",
     divisibility: "number%10", // a multiple of 10
     bound: "alpha>10", // an alpha-only string with more than 10 characters
-    range: "1<=email[]<99", // a list of 1 to 99 emails
+    range: "1<=email[]<100", // a list of 1 to 99 emails
     narrows: ["number", "=>", (n) => n % 2 === 1], // an odd integer
     morphs: ["string", "|>", parseFloat] // validates a string input then parses it to a number
 })
