a721590c836c2aa3719823ee1e8091291e2d7478
David Blass <david@arktype.io>
2023-06-16 19:59:46 -0400

    start planning generics

--- src/scopes/generics.ts
+++ src/scopes/generics.ts
@@ -1,8 +1,5 @@
-import { node } from "../nodes/composite/type.js"
 import { Scope } from "../scope.js"
 
-export const tsGenerics = Scope.root({
-    "Record<K, V>": node({ basis: "object" })
-})
+export const tsGenerics = Scope.root({})
 
 export const tsGenericTypes = tsGenerics.export()
