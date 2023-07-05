36dddaa73cceb9ec26eac2bf6a8dfa0f6ec0ab94
David Blass <david@arktype.io>
2023-07-04 20:56:20 -0400

    add some configs

--- experiments.ts
+++ experiments.ts
@@ -1,35 +1,26 @@
 // @ts-ignore
-import { format } from "prettier"
 import { scope } from "./src/main.js"
 import { arrayIndexTypeNode } from "./src/nodes/composite/indexed.js"
 
 const $ = scope({
     user: {
         friends: "user[]",
-        name: "string[]"
+        name: "string"
+    },
+    admin: {
+        friends: "user[]",
+        name: "string"
     }
 })
 
 const types = $.export()
 
-console.log($.compile())
+types.user.root.alias //?
 
-console.log(format(types.user.condition, { parser: "typescript" }))
+types.admin.root.alias //?
 
-// /******** ArkType Metadata Operator *********/
+const result = types.user.root.getPath("friends", arrayIndexTypeNode())
 
-// // The left - hand side of this operator will be used to add metadata like
-// // custom error messages (doesn't affect type) or how keys are checked (does
-// // affect type).
+result.alias //?
 
-// // Would be available as a root expression, a tuple expression, and likely
-// // chained as something like `.meta`, `.config`, or `.configure`
-
-// // Option 1️⃣
-// type("string", "@", { keys: "strict", description: "a string" })
-
-// // Option 2️⃣
-// type("string", "~", { keys: "distilled", description: "a string" })
-
-// // Option 3️⃣
-// type("string", "#", { keys: "loose", description: "a string" })
+$.compile() //?
