a113e4fbcd931b9d81380ee61be88b8f55dbb18a
David Blass <david@arktype.io>
2023-08-03 18:02:38 -0400

    repo build improvements

--- ark/repo/ts.cjs
+++ ark/repo/ts.cjs
@@ -9,7 +9,7 @@ if (fileArgIndex === process.argv.length - 1) {
 }
 
 execSync(
-	`node --conditions arktype-dev --loader ts-node/esm --no-warnings=ExperimentalWarning ${process.argv
+	`node --conditions arktype-repo --loader ts-node/esm --no-warnings=ExperimentalWarning ${process.argv
 		.slice(fileArgIndex + 1)
 		.join(" ")}`,
 	{
