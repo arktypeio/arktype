a7107420b2b35fad629a021d10b235759971c504
David Blass <david@arktype.io>
2023-08-24 17:20:59 -0400

    begin implementing Describable

--- ark/schema/traits/description.ts
+++ ark/schema/traits/description.ts
@@ -1,36 +1,26 @@
-import type { Base, BaseDefinition } from "../node.js"
-import { RuleNode } from "./trait.js"
+import { compose } from "@arktype/util"
 
 export interface DescriptionDefinition extends BaseDefinition {
 	readonly value: string
 }
 
-export class DescriptionNode extends RuleNode<DescriptionDefinition> {
+export class DescriptionNode {
 	readonly kind = "description"
-
-	writeDefaultDescription() {
-		return this.value
-	}
 
 	protected reduceRules(other: DescriptionNode) {
 		return null
 	}
 }
 
-export const Describable = <base extends Base<object>>(base: base) => {
-	abstract class Describable extends base {
-		readonly description?: DescriptionNode
-
-		constructor(...args: any[]) {
-			super(...args)
-			this.description = args[0]?.description
-		}
-
+export const Describable =
+	(abstract: { writeDefaultDescription(): string }) =>
+	(input: { description?: string | DescriptionNode }) => ({
+		description: input.description ?? abstract.writeDefaultDescription(),
 		toString() {
-			return this.description?.toString() ?? this.writeDefaultDescription()
+			return this.description
 		}
+	})
 
-		abstract writeDefaultDescription(): string
-	}
-	return Describable
-}
+const z = compose(Describable)({
+	writeDefaultDescription: () => "foo"
+})
