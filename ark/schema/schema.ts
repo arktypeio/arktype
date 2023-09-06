2dabaf7e416551fdf295cf246b9dbb53c7538214
David Blass <david@arktype.io>
2023-09-05 20:17:33 -0400

    iterate on schema

--- ark/schema/schema.ts
+++ ark/schema/schema.ts
@@ -1,42 +1,81 @@
-import type { conform, exact, listable } from "@arktype/util"
+import type { AbstractableConstructor, exact, listable } from "@arktype/util"
 import type { NonEnumerableDomain } from "./bases/domain.js"
+import type { BoundSet, Narrow } from "./main.js"
 import type { CastTo } from "./utils.js"
 
 export type SchemaDefinition = listable<PredicateDefinition>
 
-export type ConstraintDefinition<rule> =
-	| rule
-	| {
-			rule: rule
-			description?: string
-	  }
+export type ConstraintDefinition<rule> = rule | ConstraintDefinitionObject<rule>
+
+type ConstraintDefinitionObject<rule> = {
+	rule: rule
+	description?: string
+}
 
 export type DomainDefinition<
 	domain extends NonEnumerableDomain = NonEnumerableDomain
 > = ConstraintDefinition<domain>
 
+type DomainBasis<domain extends NonEnumerableDomain = NonEnumerableDomain> = {
+	domain: DomainDefinition<domain>
+}
+
+export type PrototypeDefinition<
+	constructor extends AbstractableConstructor = AbstractableConstructor
+> = ConstraintDefinition<constructor>
+
+type PrototypeBasis<
+	constructor extends AbstractableConstructor = AbstractableConstructor
+> = {
+	prototype: PrototypeDefinition<constructor>
+}
+
+export type IdentityDefinition<value = unknown> = ConstraintDefinition<value>
+
+type IdentityBasis<identity = unknown> = {
+	identity: IdentityDefinition<identity>
+}
+
+export type BasisDefinition = DomainBasis | PrototypeBasis | IdentityBasis
+
+export interface NarrowablePredicate {
+	narrows?: NarrowDefinition
+}
+
+export interface NumberPredicate extends NarrowablePredicate {
+	divisor?: DivisorDefinition
+	bounds?: BoundSet
+}
+
+export type RefinementsFor<def> = def extends IdentityBasis
+	? {}
+	: def extends DomainBasis<"number">
+	? {}
+	: {}
+
 export type DivisorDefinition = ConstraintDefinition<number>
 
-export type RegexDefinition = ConstraintDefinition<RegExp>
+export type BoundsDefinition = ConstraintDefinition<BoundSet>
+
+export type NarrowDefinition = ConstraintDefinition<listable<Narrow>>
+
+export type RegexDefinition = ConstraintDefinition<listable<RegExp>>
 
 export type PredicateDefinition =
 	| Record<PropertyKey, never>
-	| { domain: DomainDefinition }
+	| IdentityBasis
 	| { domain: DomainDefinition<"number">; divisor?: DivisorDefinition }
 	| { domain: DomainDefinition<"string">; regex?: RegexDefinition }
 
 export type TypeSchema<t = unknown> = CastTo<t>
 
 export const schema = <const branches extends readonly PredicateDefinition[]>(
-	...branches: conform<
-		branches,
-		{
-			[i in keyof branches]: exact<
-				branches[i],
-				PredicateDefinition & { domain: branches[i & keyof branches] }
-			>
-		}
-	>
+	...branches: {
+		[i in keyof branches]: exact<
+			branches[i],
+			PredicateDefinition & { domain: branches[i & keyof branches] }
+		>
+	}
 ) => branches
 
 // export type inferBranches<branches extends readonly PredicateDefinition[]> = {
