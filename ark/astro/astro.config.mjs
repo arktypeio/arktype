0aee1884f24e5a5e242c06ba4b77b6587c302549
David Blass <david@arktype.io>
2023-09-14 17:51:00 -0400

    some docs config

--- ark/astro/astro.config.mjs
+++ ark/astro/astro.config.mjs
@@ -1,27 +1,27 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config"
+import starlight from "@astrojs/starlight"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: "My Docs",
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/withastro/starlight"
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "Guides",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
+						{ label: "Example Guide", link: "/guides/example/" }
+					]
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+					label: "Reference",
+					autogenerate: { directory: "reference" }
+				}
+			]
+		})
+	]
+})
