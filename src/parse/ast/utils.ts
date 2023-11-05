f6a0bdf96347983c4b9771d003667dd3e6871bf9
hsiwe <46127681+Hsiwe@users.noreply.github.com>
2023-11-05 15:38:32 +0300

    Improve astToString, add tests for astToString

--- src/parse/ast/utils.ts
+++ src/parse/ast/utils.ts
@@ -1,12 +1,25 @@
-import type { Literalable } from "../../utils/generics.js"
+import type { List, Literalable } from "../../utils/generics.js"
+import type { Scanner } from "../string/shift/scanner.js"
+import type { InfixExpression, PostfixExpression } from "./ast.js"
 
-export type astToString<ast> = `'${astToStringRecurse<ast, "">}'`
-
-type astToStringRecurse<ast, result extends string> = ast extends [
-    infer head,
-    ...infer tail
-]
-    ? astToStringRecurse<tail, `${result}${astToStringRecurse<head, "">}`>
+export type astToString<ast> = `'${astToStringRecurse<ast>}'`;
+    
+type astToStringRecurse<ast> = 
+    ast extends PostfixExpression<infer operator, infer operand>
+        ? operator extends "[]"
+            ? `${groupAst<operand>}[]`
+            : never
+    : ast extends InfixExpression<infer operator, infer l, infer r>
+        ? operator extends "&" | "|" | "%" | Scanner.Comparator
+            ? `${groupAst<l>}${operator}${groupAst<r>}`
+            : never
     : ast extends Literalable
-    ? `${result}${ast extends bigint ? `${ast}n` : ast}`
-    : "..."
+        ? `${ast extends bigint ? `${ast}n` : ast}`
+        : "..."
+
+
+type groupAst<ast> = ast extends List 
+    ? ast[1] extends "[]" 
+        ? astToStringRecurse<ast> 
+        : `(${astToStringRecurse<ast>})` 
+    : astToStringRecurse<ast>
