e946414956270059b3317f3aa49040ad91e9f919
David Blass <david@arktype.io>
2023-12-01 21:30:05 -0500

    remove tsup config

--- ark/repo/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup"
-
-export default defineConfig({
-	tsconfig: "tsconfig.build.json",
-	entry: ["main.ts"],
-	format: ["cjs", "esm"],
-	experimentalDts: true,
-	clean: true
-})
