8a8c8da299e9eff5ffbb2c57dd5862fbcc1ef660
David Blass <david@arktype.io>
2023-12-07 14:19:44 -0500

    fix arkdark regex perf

--- ark/schema/refinements/props/sequence.ts
+++ ark/schema/refinements/props/sequence.ts
@@ -2,7 +2,6 @@ import type { TypeNode, TypeSchema } from "../../base.js"
 import type { CompilationContext, Problems } from "../../shared/compilation.js"
 import type { declareNode, withAttributes } from "../../shared/declare.js"
 import type {
-	KeyDefinitions,
 	NodeKeyImplementation,
 	NodeParserImplementation
 } from "../../shared/define.js"
@@ -66,7 +65,7 @@ export class SequenceNode extends RefinementNode<SequenceDeclaration> {
 	traverseApply = (data: readonly unknown[], problems: Problems) => {}
 
 	getCheckedDefinitions() {
-		return ["object"] as const
+		return [Array] as const
 	}
 
 	writeDefaultDescription() {
@@ -77,3 +76,145 @@ export class SequenceNode extends RefinementNode<SequenceDeclaration> {
 		return ""
 	}
 }
+
+// const arrayIndexSourceSuffix = `(?:0|(?:[1-9]\\d*))$`
+
+// const arrayIndexLiteralSuffix = `${arrayIndexSourceSuffix}/` as const
+
+// export type ArrayIndexMatcherSource =
+// 	`${string}${typeof arrayIndexSourceSuffix}`
+
+// const excludedIndexMatcherStart = "^(?!("
+// const excludedIndexMatcherEnd = ")$)"
+
+// // Build a pattern to exclude all indices from firstVariadic - 1 down to 0
+// const excludedIndicesSource = (firstVariadic: number) => {
+// 	if (firstVariadic < 1) {
+// 		return throwInternalError(
+// 			`Unexpectedly tried to create a variadic index < 1 (was ${firstVariadic})`
+// 		)
+// 	}
+// 	let excludedIndices = `${firstVariadic - 1}`
+// 	for (let i = firstVariadic - 2; i >= 0; i--) {
+// 		excludedIndices += `|${i}`
+// 	}
+// 	return `${excludedIndexMatcherStart}${excludedIndices}${excludedIndexMatcherEnd}${arrayIndexSourceSuffix}` as const
+// }
+
+// export type VariadicIndexMatcherSource = ReturnType<
+// 	typeof excludedIndicesSource
+// >
+
+// export type VariadicIndexMatcherLiteral = `/${VariadicIndexMatcherSource}/`
+
+// const nonVariadicIndexMatcherSource = `^${arrayIndexSourceSuffix}` as const
+
+// export type NonVariadicIndexMatcherSource = typeof nonVariadicIndexMatcherSource
+
+// export type NonVariadicIndexMatcherLiteral =
+// 	`/${NonVariadicIndexMatcherSource}/`
+
+// export const arrayIndexMatcherSource = <index extends number>(
+// 	firstVariadic: index
+// ) =>
+// 	(firstVariadic === 0
+// 		? // If the variadic pattern starts at index 0, return the base array index matcher
+// 		  nonVariadicIndexMatcherSource
+// 		: excludedIndicesSource(firstVariadic)) as index extends 0
+// 		? NonVariadicIndexMatcherSource
+// 		: VariadicIndexMatcherSource
+
+// export const extractArrayIndexRegex = (keyNode: TypeNode) => {
+// 	if (keyNode.branches.length !== 1) {
+// 		return
+// 	}
+// 	const regexRefinements = keyNode.branches[0].regex
+// 	if (!regexRefinements || regexRefinements.length !== 1) {
+// 		return
+// 	}
+// 	const regexLiteral = regexRefinements[0].rule
+// 	if (!regexLiteral.endsWith(arrayIndexLiteralSuffix)) {
+// 		return
+// 	}
+// 	return sourceFromRegexLiteral(regexLiteral) as ArrayIndexMatcherSource
+// }
+
+// export const extractFirstVariadicIndex = (source: ArrayIndexMatcherSource) => {
+// 	if (!source.startsWith(excludedIndexMatcherStart)) {
+// 		return 0
+// 	}
+// 	const excludedIndices = source.slice(
+// 		excludedIndexMatcherStart.length,
+// 		source.indexOf(excludedIndexMatcherEnd)
+// 	)
+// 	const firstExcludedIndex = excludedIndices.split("|")[0]
+// 	return (
+// 		tryParseWellFormedInteger(
+// 			firstExcludedIndex,
+// 			`Unexpectedly failed to parse a variadic index from ${source}`
+// 		) + 1
+// 	)
+// }
+
+// export const arrayIndexInput = <index extends number = 0>(
+// 	firstVariadicIndex: index = 0 as index
+// ) =>
+// 	({
+// 		basis: "string",
+// 		regex: `/${arrayIndexMatcherSource(firstVariadicIndex)}/`
+// 		// TODO: reenable
+// 	}) as const // satisfies PredicateInput<"string">
+
+// export const arrayIndexTypeNode = (firstVariadicIndex = 0): TypeNode<string> =>
+// 	firstVariadicIndex === 0
+// 		? builtins.nonVariadicArrayIndex()
+// 		: node(arrayIndexInput(firstVariadicIndex))
+
+// export const compileArray = (
+// 	indexMatcher: ArrayIndexMatcherSource,
+// 	elementNode: TypeNode,
+// 	namedProps: readonly NamedPropRule[],
+// 	ctx: CompilationContext
+// ) => {
+// 	const firstVariadicIndex = extractFirstVariadicIndex(indexMatcher)
+// 	const namedCheck = namedProps
+// 		.map((named) => compileNamedProp(named, ctx))
+// 		.join("\n")
+// 	ctx.path.push(["i"])
+// 	const elementCondition = `${elementNode.alias}(${In}[i])`
+// 	ctx.path.pop()
+// 	return `${namedCheck}
+// for(let i = ${firstVariadicIndex}; i < ${In}.length; i++) {
+//     ${elementCondition}
+// }`
+// }
+
+// export const compileIndexed = (
+// 	namedProps: readonly NamedPropRule[],
+// 	indexedProps: readonly IndexedPropRule[],
+// 	ctx: CompilationContext
+// ) => {
+// 	const k = ctx.path.push(["k"])
+// 	const indexedChecks = indexedProps
+// 		.map((prop) =>
+// 			prop.key === builtins.string()
+// 				? // if the index signature is just for "string", we don't need to check it explicitly
+// 				  prop.value.compile(ctx)
+// 				: // Ensure condition is checked on the key variable as opposed to the input
+// 				  // TODO: fix ${prop.key.condition.replaceAll(InputParameterName, k)}
+// 				  `if(false){
+//     ${prop.value.compile(ctx)}
+// }`
+// 		)
+// 		.join("\n")
+// 	ctx.path.pop()
+// 	if (ctx) {
+// 		return throwInternalError(`Unimplemented?`)
+// 	}
+// 	// TODO: don't recheck named
+// 	return `${compileNamedProps(namedProps, ctx)}
+//     for(const ${k} in ${In}) {
+//         ${indexedChecks}
+//     }
+// `
+// }
--- test.ts
+++ test.ts
@@ -3,59 +3,60 @@ import { schema } from "./ark/schema/main.js"
 
 schema("bigint")
 
-// class Callable {
-// 	constructor(fn: Function) {
-// 		return () => fn.call(this)
-// 	}
-// }
+class Callable {
+	constructor(fn: Function) {
+		return () => fn.call(this)
+	}
+}
 
-// class MeCallable extends Callable {
-// 	constructor() {
-// 		super(() => Sub.prototype.foo.call(this))
-// 		Object.setPrototypeOf(this, Sub.prototype)
-// 	}
-// 	salutation = "Mr. "
-// 	doSomething() {
-// 		return this.salutation + "doSomething"
-// 	}
-// }
+class MeCallable extends Callable {
+	constructor() {
+		super(() => Sub.prototype.foo.call(this))
+		Object.setPrototypeOf(this, Sub.prototype)
+	}
 
-// class Sub extends MeCallable {
-// 	// constructor() {
-// 	// 	super(() => {
-// 	// 		return this.foo
-// 	// 	})
-// 	// }
+	salutation = "Mr. "
+	doSomething() {
+		return this.salutation + "doSomething"
+	}
+}
 
-// 	foo() {
-// 		return this.doSomething()
-// 	}
-// }
+class Sub extends MeCallable {
+	// constructor() {
+	// 	super(() => {
+	// 		return this.foo
+	// 	})
+	// }
 
-// const abc = new Sub()
+	foo() {
+		return this.doSomething()
+	}
+}
+
+const abc = new Sub()
 
 // abc.doSomething() //?
 // abc() //?
 
-// class Callable2 {
-// 	constructor(defaultFn: Function) {
-// 		return (...args: any[]) => {
-// 			return defaultFn.call(this, ...args)
-// 		}
-// 	}
-// }
+class Callable2 {
+	constructor(defaultFn: Function) {
+		return (...args: any[]) => {
+			return defaultFn.call(this, ...args)
+		}
+	}
+}
 
-// class MeCallable2 extends Callable2 {
-// 	constructor() {
-// 		super((name: string) => "Hello: " + this.salutation + name)
-// 		Object.setPrototypeOf(this, MeCallable.prototype)
-// 	}
-// 	salutation = "Mr. "
-// 	doSomething() {
-// 		console.log(this.salutation, "doSomething")
-// 	}
-// }
+class MeCallable2 extends Callable2 {
+	constructor() {
+		super((name: string) => "Hello: " + this.salutation + name)
+		Object.setPrototypeOf(this, MeCallable.prototype)
+	}
+	salutation = "Mr. "
+	doSomething() {
+		console.log(this.salutation, "doSomething")
+	}
+}
 
-// const abc2 = new MeCallable2()
+const abc2 = new MeCallable2()
 
 // abc2("ff") //?
