96f9a9f46c7246b52cde3d199b2e1ff86cea983b
David Blass <david@arktype.io>
2023-12-11 15:01:19 -0500

    cointii

--- ark/util/trait2.ts
+++ ark/util/trait2.ts
@@ -212,3 +212,56 @@ export type Disambiguation = Record<PropertyKey, Constructor>
 // 		? never
 // 		: k
 // }>
+
+// type validateTraits<
+// 	traits extends readonly Trait[],
+// 	base extends Trait = EmptyTrait,
+// 	result extends Trait[] = []
+// > = traits extends readonly [
+// 	infer head extends Trait,
+// 	...infer tail extends Trait[]
+// ]
+// 	? validateTraits<
+// 			tail,
+// 			intersectTraits<base, head>,
+// 			[...result, validateExtension<base, head>]
+// 	  >
+// 	: result
+
+// type validateExtension<l extends Trait, r extends Trait> = [l, r] extends [
+// 	Trait<any, infer lImplementation, infer lBase>,
+// 	Trait<infer rInput, infer rImplementation, infer rBase>
+// ]
+// 	? Trait<
+// 			rInput,
+// 			{
+// 				[k in keyof rImplementation]: k extends keyof lImplementation
+// 					? error<`Key '${k & string} appears in multiple implementations'`>
+// 					: k extends keyof lBase
+// 					? rImplementation[k] extends lBase[k]
+// 						? rImplementation[k]
+// 						: error<`'${k &
+// 								string}' incorrectly implements a previous base constraint'`>
+// 					: rImplementation[k]
+// 			},
+// 			{
+// 				[k in keyof rBase]: k extends keyof lImplementation
+// 					? lImplementation[k] extends rBase[k]
+// 						? rBase[k]
+// 						: error<`'${k &
+// 								string}' conflicts with a previously implemented constraint'`>
+// 					: rBase[k]
+// 			}
+// 	  >
+// 	: never
+
+// type intersectTraits<l extends Trait, r extends Trait> = [l, r] extends [
+// 	Trait<infer lArgs, infer lImplementation, infer lBase>,
+// 	Trait<infer rArgs, infer rImplementation, infer rBase>
+// ]
+// 	? Trait<
+// 			intersectParameters<lArgs, rArgs>,
+// 			evaluate<lImplementation & rImplementation>,
+// 			evaluate<Omit<lBase & rBase, keyof (lImplementation & rImplementation)>>
+// 	  >
+// 	: never
