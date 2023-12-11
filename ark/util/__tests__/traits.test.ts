0b083964e20ac7066569516fa4cd99a750b45cc4
David Blass <david@arktype.io>
2023-12-10 22:48:18 -0500

    preserve static properties, traitsOf

--- ark/util/__tests__/traits.test.ts
+++ ark/util/__tests__/traits.test.ts
@@ -69,4 +69,70 @@ describe("traits", () => {
 			withDefault instanceof Describable
 		]).equals([true, true, true])
 	})
+	it("works with subclasses", () => {
+		abstract class Foo extends Boundable<number> {
+			getFoo() {
+				return "foo"
+			}
+		}
+		class Bar extends compose(Foo) {
+			sizeOf(data: number) {
+				return data
+			}
+		}
+		const b = new Bar({ limit: 2 })
+		attest(b.check(1)).equals(true)
+		attest(b.check(3)).equals(false)
+		attest(b.getFoo()).equals("foo")
+	})
+	it("preserves static", () => {
+		abstract class A extends Trait {
+			static readonly a = "a"
+
+			readonly a = "a"
+		}
+		abstract class B extends Trait {
+			static readonly b = "b"
+
+			readonly b = "b"
+		}
+		class C extends compose(A, B) {
+			static readonly c = "c"
+
+			readonly c = "c"
+		}
+
+		attest<"a">(C.a).equals("a")
+		attest<"b">(C.b).equals("b")
+		attest<"c">(C.c).equals("c")
+		const c = new C()
+		attest<"a">(c.a).equals("a")
+		attest<"b">(c.b).equals("b")
+		attest<"c">(c.c).equals("c")
+	})
+
+	it("trait from trait", () => {
+		abstract class A extends Trait {
+			readonly a = "a"
+		}
+		abstract class B extends Trait {
+			readonly b = "b"
+		}
+		class C extends compose(A, B) {
+			readonly c = "c"
+		}
+		class D extends Trait {
+			readonly d = "d"
+		}
+		class E extends compose(C, D) {
+			readonly e = "e"
+		}
+		const e = new E()
+		attest<"a">(e.a).equals("a")
+		attest<"b">(e.b).equals("b")
+		attest<"c">(e.c).equals("c")
+		attest<"d">(e.d).equals("d")
+		attest<"e">(e.e).equals("e")
+		attest(e.traitsOf()).equals([A, B, C, D])
+	})
 })
--- ark/util/traits.ts
+++ ark/util/traits.ts
@@ -5,11 +5,22 @@ export type TraitComposition = <traits extends readonly Constructor[]>(
 	...traits: traits
 ) => compose<traits>
 
-export const traitsOf = Symbol("hasTraits")
+// even though the value we attach will be identical, we use this so classes
+// won't be treated as instanceof a Trait
+const implementedTraits = Symbol("implementedTraits")
 
 export abstract class Trait {
-	static [Symbol.hasInstance](o: any) {
-		return Array.isArray(o[traitsOf]) && o[traitsOf].includes(this)
+	static [Symbol.hasInstance](o: object) {
+		return (
+			implementedTraits in o.constructor &&
+			(o.constructor[implementedTraits] as Function[]).includes(this)
+		)
+	}
+
+	traitsOf(): readonly Function[] {
+		return implementedTraits in this.constructor
+			? (this.constructor[implementedTraits] as Function[])
+			: []
 	}
 }
 
@@ -26,28 +37,54 @@ const collectPrototypeDescriptors = (trait: Function) => {
 }
 
 export const compose = ((...traits: Function[]) => {
+	if (traits.length === 0) {
+		return Object
+	}
+	if (traits.length === 1) {
+		return traits[0]
+	}
 	const base: any = function (this: any, ...args: any[]) {
 		for (const trait of traits) {
-			Object.assign(this, Reflect.construct(trait, args, this.constructor))
+			const instance = Reflect.construct(trait, args, this.constructor)
+			Object.assign(this, instance)
 		}
-		Object.defineProperty(this, traitsOf, { value: traits, enumerable: false })
 	}
-
+	const flatImplementedTraits: Function[] = []
 	for (const trait of traits) {
+		// copy static properties
+		Object.assign(base, trait)
+		// flatten and copy prototype
 		Object.defineProperties(base.prototype, collectPrototypeDescriptors(trait))
+		if (implementedTraits in trait) {
+			// add any ancestor traits from which the current trait was composed
+			for (const innerTrait of trait[implementedTraits] as Function[]) {
+				if (!flatImplementedTraits.includes(innerTrait)) {
+					flatImplementedTraits.push(innerTrait)
+				}
+			}
+		}
+		if (!flatImplementedTraits.includes(trait)) {
+			flatImplementedTraits.push(trait)
+		}
 	}
+	Object.defineProperty(base, implementedTraits, {
+		value: flatImplementedTraits,
+		enumerable: false
+	})
 	return base
 }) as TraitComposition
 
 export type compose<traits extends readonly Constructor[]> = composeRecurse<
 	traits,
 	[],
+	{},
 	{}
 >
 
 export type composeRecurse<
 	traits extends readonly unknown[],
 	parameters extends readonly unknown[],
+	statics extends {},
 	instance extends {}
 > = traits extends readonly [
 	abstract new (...args: infer nextArgs) => infer nextInstance,
@@ -56,6 +93,7 @@ export type composeRecurse<
 	? composeRecurse<
 			tail,
 			intersectParameters<parameters, nextArgs>,
+			statics & { [k in keyof traits[0]]: traits[0][k] },
 			instance & nextInstance
 	  >
-	: abstract new (...args: parameters) => instance
+	: statics & (abstract new (...args: parameters) => instance)
