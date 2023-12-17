4d7722498fb4553727a35cd33da4e4a767f82e5e
David Blass <david@arktype.io>
2023-12-16 22:11:22 -0500

    update somesthing

--- ark/schema/keywords/tsKeywords.ts
+++ ark/schema/keywords/tsKeywords.ts
@@ -28,14 +28,14 @@ export const TsKeywords: ScopeNode<TsKeywords.resolutions> = ScopeNode.from({
 	bigint: "bigint",
 	// since we know this won't be reduced, it can be safely cast to a union
 	boolean: [{ unit: false }, { unit: true }] as schema.cast<boolean, "union">,
-	false: { unit: false as const },
+	false: { unit: false },
 	never: [],
 	null: { unit: null },
 	number: "number",
 	object: "object",
 	string: "string",
 	symbol: "symbol",
-	true: { unit: true as const },
+	true: { unit: true },
 	unknown: {},
 	void: { unit: undefined } as schema.cast<void, "unit">,
 	undefined: { unit: undefined }
