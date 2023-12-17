88e17dc53f66471f7ed5b544789c6bb951ea317c
David Blass <david@arktype.io>
2023-12-16 23:34:13 -0500

    fix build scripts

--- ark/repo/build.ts
+++ ark/repo/build.ts
@@ -1,16 +1,21 @@
+import { symlinkSync, unlinkSync } from "fs"
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
-import { fromCwd, fromHere, shell, writeJson } from "../fs/main.js"
+import { fromCwd, shell, writeJson } from "../fs/main.js"
 
 const isCjs = process.argv.includes("--cjs") || process.env.ARKTYPE_CJS
-const cjsTsconfig = fromHere("tsconfig.cjs.json")
-const esmTsconfig = fromHere("tsconfig.esm.json")
 
 try {
-	shell(`ln -sf ${isCjs ? cjsTsconfig : esmTsconfig} tsconfig.build.json`)
+	if (isCjs) {
+		unlinkSync("tsconfig.build.json")
+		symlinkSync(`../repo/tsconfig.cjs.json`, "tsconfig.build.json")
+	}
 	shell("pnpm tsc --project tsconfig.build.json")
 	if (isCjs) {
 		writeJson(fromCwd("out", "package.json"), { type: "commonjs" })
 	}
 } finally {
-	shell(`ln -sf ${esmTsconfig} tsconfig.build.json`)
+	if (isCjs) {
+		unlinkSync("tsconfig.build.json")
+		symlinkSync("../repo/tsconfig.esm.json", "tsconfig.build.json")
+	}
 }
