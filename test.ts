a85e1257219433e9e8ccac090cf3a30f96321e46
David Blass <david@arktype.io>
2024-01-10 16:01:05 -0500

    update examples

--- test.ts
+++ test.ts
@@ -1,69 +1,8 @@
 /* eslint-disable @typescript-eslint/no-restricted-imports */
 import { type } from "arktype"
-import { schema } from "./ark/schema/main.js"
 
 const User = type({
 	id: "string"
 })
 
 type User = typeof User.infer
-
-schema("bigint")
-
-class Callable {
-	constructor(fn: Function) {
-		return () => fn.call(this)
-	}
-}
-
-class MeCallable extends Callable {
-	constructor() {
-		super(() => Sub.prototype.foo.call(this))
-		Object.setPrototypeOf(this, Sub.prototype)
-	}
-
-	salutation = "Mr. "
-	doSomething() {
-		return this.salutation + "doSomething"
-	}
-}
-
-class Sub extends MeCallable {
-	// constructor() {
-	// 	super(() => {
-	// 		return this.foo
-	// 	})
-	// }
-
-	foo() {
-		return this.doSomething()
-	}
-}
-
-const abc = new Sub()
-
-// abc.doSomething() //?
-// abc() //?
-
-class Callable2 {
-	constructor(defaultFn: Function) {
-		return (...args: any[]) => {
-			return defaultFn.call(this, ...args)
-		}
-	}
-}
-
-class MeCallable2 extends Callable2 {
-	constructor() {
-		super((name: string) => "Hello: " + this.salutation + name)
-		Object.setPrototypeOf(this, MeCallable.prototype)
-	}
-	salutation = "Mr. "
-	doSomething() {
-		console.log(this.salutation, "doSomething")
-	}
-}
-
-const abc2 = new MeCallable2()
-
-// abc2("ff") //?
