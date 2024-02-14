a894549c177c8fa7d44943b7cf7b77ae9cef3543
David Blass <david@arktype.io>
2024-02-14 15:02:13 -0500

    improve analyze trace resolution

--- ark/attest/cli/trace.ts
+++ ark/attest/cli/trace.ts
@@ -2,19 +2,8 @@ import { execSync } from "child_process"
 import { existsSync } from "fs"
 import { join } from "path"
 
-export const trace = (args: string[]) => {
+export const trace = async (args: string[]) => {
 	const packageDir = args[0]
-
-	// for some reason pnpm installing @typescript/analyze-trace is resulting in a .bin file referencing a dist folder
-	// at the wrong relative path, so resolving this directly for now (should be changed if this is resolved)
-	const analyzeTraceBin = join(
-		process.cwd(),
-		"node_modules",
-		"@typescript",
-		"analyze-trace",
-		"bin",
-		"analyze-trace"
-	)
 
 	if (!existsSync(packageDir)) {
 		throw new Error(
@@ -26,7 +15,6 @@ export const trace = (args: string[]) => {
 		console.log(`⏳ Gathering type trace data for ${packageDir}...`)
 		// These cache files have to be removed before any analysis is done otherwise
 		// the results will be meaningless.
-
 		// the .tstrace directory will contain a trace.json file and a types.json file.
 		// the trace.json file can be viewed via a tool like https://ui.perfetto.dev/
 		// the types.json file can be used to associate IDs from the trace file with type aliases
@@ -41,10 +29,14 @@ export const trace = (args: string[]) => {
 		console.error(String(e))
 	} finally {
 		console.log(`⏳ Analyzing type trace data for ${packageDir}...`)
+		// allow analyze-trace to process the args
+		process.argv = [
+			"node",
+			"node_modules/@typescript/analyze-trace/dist/analyze-trace-dir.js",
+			join(packageDir, ".tstrace")
+		]
 		// TypeScript's analyze-trace tool can be used to automatically detect hot-spots in your code.
 		// It's not a perfect match for what can be optimized, but it can be a helpful place to start
-		execSync(`node ${analyzeTraceBin} ${join(packageDir, ".tstrace")}`, {
-			stdio: "inherit"
-		})
+		await import("@typescript/analyze-trace/dist/analyze-trace-dir.js")
 	}
 }
