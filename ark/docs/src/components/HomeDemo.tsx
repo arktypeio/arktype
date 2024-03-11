4b673b7aa622271f6549c55f8add5a7f402ae5c3
David Blass <david@arktype.io>
2024-03-11 01:44:37 -0400

    work on monaco integration

--- ark/docs/src/components/HomeDemo.tsx
+++ ark/docs/src/components/HomeDemo.tsx
@@ -1,79 +1,106 @@
-import Editor from "@monaco-editor/react"
-import React from "react"
+import Editor, { useMonaco } from "@monaco-editor/react"
+import type * as Monaco from "monaco-editor"
+import { wireTmGrammars } from "monaco-editor-textmate"
+import { Registry } from "monaco-textmate"
+import { loadWASM } from "onigasm"
+import onigasm from "onigasm/lib/onigasm.wasm?url"
+import React, { useState } from "react"
+import "../styles.css"
+import arkdarkColors from "./arkdark.json"
+import syntax from "./syntax.ts?raw"
+import arktypeTextmate from "./tsWithArkType.tmLanguage.json"
+
+interface IVSCodeTheme {
+	colors: {
+		[name: string]: string
+	}
+	tokenColors: ITokenColor[]
+}
+interface ITokenColor {
+	scope: string | string[]
+	settings: {
+		foreground?: string
+		background?: string
+		fontStyle?: string
+	}
+}
+
+const translateVSCodeTheme = (
+	theme: IVSCodeTheme
+): Monaco.editor.IStandaloneThemeData => {
+	theme.colors["editor.background"] = theme.colors["editor.background"] + "66"
+	return {
+		base: "vs-dark",
+		inherit: false,
+		colors: theme.colors,
+		rules: theme.tokenColors.flatMap((c) => {
+			if (Array.isArray(c.scope)) {
+				return c.scope.map((sub) => {
+					return {
+						token: sub,
+						background: c.settings.background,
+						foreground: c.settings.foreground,
+						fontStyle: c.settings.fontStyle
+					} as Monaco.editor.ITokenThemeRule
+				})
+			}
+			return {
+				token: c.scope,
+				background: c.settings.background,
+				foreground: c.settings.foreground,
+				fontStyle: c.settings.fontStyle
+			} as Monaco.editor.ITokenThemeRule
+		})
+	}
+}
+
+const theme = translateVSCodeTheme(arkdarkColors)
+
+const setupMonaco = async (monaco: typeof Monaco) => {
+	await loadWASM(onigasm)
+	monaco.editor.defineTheme("arkdark", theme)
+	await wireTmGrammars(
+		monaco,
+		new Registry({
+			getGrammarDefinition: async () => ({
+				format: "json",
+				content: arktypeTextmate
+			})
+		}),
+		new Map().set("typescript", "source.ts")
+	)
+}
 
 export const HomeDemo = () => {
-	return (
+	const [loaded, setLoaded] = useState(false)
+	const monaco = useMonaco()
+	if (monaco && !loaded) setupMonaco(monaco).then(() => setLoaded(true))
+	return loaded ? (
 		<Editor
 			height="30vh"
 			defaultLanguage="typescript"
-			defaultValue="// artkype code"
-			theme="vs-dark"
+			defaultValue={syntax}
+			theme="arkdark"
+			options={{
+				minimap: { enabled: false },
+				scrollBeyondLastLine: false
+			}}
+			onMount={(editor, monaco) => {
+				const editorElement = editor.getDomNode()
+				if (editorElement) {
+					editorElement.style.borderRadius = "16px"
+					editorElement.style.boxShadow =
+						"0 10px 15px 0 rgba(0, 0, 0, 0.3), 0 15px 30px 0 rgba(0, 0, 0, 0.22)"
+					editorElement.style.transition = "all 0.3s cubic-bezier(.25,.8,.25,1)"
+					editorElement.style.backdropFilter = "blur(16px)"
+					const guard = editorElement?.querySelector(
+						".overflow-guard"
+					) as HTMLElement | null
+					guard!.style.borderRadius = "16px"
+				}
+			}}
 		/>
+	) : (
+		"Loading..."
 	)
 }
-// const { pathname } = useLocation()
-// const palette = useTheme().palette
-// const isDarkMode = useColorMode().colorMode === "dark"
-// const [isActive, setIsActive] = useState<boolean>(pathname.includes("try"))
-// const backgroundColor = isDarkMode ? "#ffffff00" : "#000000aa"
-// return (
-// 	<Stack alignItems="start" width="100%">
-// 		<Button
-// 			variant="contained"
-// 			sx={{
-// 				backgroundColor,
-// 				backdropFilter: "blur(4px)",
-// 				borderRadius: "2rem",
-// 				fontSize: "1.5rem",
-// 				fontFamily: cascadiaCodeFamily,
-// 				textTransform: "none",
-// 				color: palette.primary.main,
-// 				"&:hover": {
-// 					backgroundColor,
-// 					color: palette.secondary.main,
-// 					backdropFilter: "blur(6px)"
-// 				},
-// 				zIndex: 1
-// 			}}
-// 			onClick={() => setIsActive(!isActive)}
-// 			endIcon={
-// 				<div style={{ display: "flex" }}>
-// 					{isActive ? <Collapse /> : <Expand />}
-// 				</div>
-// 			}
-// 		>
-// 			{isActive ? "$ wq!" : "$ code demo.ts"}
-// 			<motion.div
-// 				animate={{ opacity: 0 }}
-// 				transition={{
-// 					duration: 0.5,
-// 					repeatType: "mirror",
-// 					repeat: Infinity
-// 				}}
-// 			>
-// 				_
-// 			</motion.div>
-// 		</Button>
-// 		<Stack width="100%">
-// 			{isActive ? (
-// 				<StackBlitzDemo embedId="demo" />
-// 			) : (
-// 				<AutoplayDemo
-// 					src="/img/arktype.mp4"
-// 					style={{
-// 						width: "100%",
-// 						marginTop: "-2.8rem"
-// 					}}
-// 				/>
-// 			)}
-// 		</Stack>
-// 		<sub>
-// 			<code>typescript@4.9.5</code> in VS Code— no extensions or plugins
-// 			required (
-// 			<a href="https://github.com/arktypeio/arktype#how" target="_blank">
-// 				how?
-// 			</a>
-// 			)
-// 		</sub>
-// 	</Stack>
-// )
