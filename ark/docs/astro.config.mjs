dd8b4edff204a5261570c88c755dd705638dcece
David Blass <david@arktype.io>
2024-04-28 01:13:03 -0400

    ci: iterate on docs publishing (#937)

--- ark/docs/astro.config.mjs
+++ ark/docs/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://arktype.io",
 	outDir: "out",
 	integrations: [
 		starlight({
@@ -35,6 +36,5 @@ export default defineConfig({
 			]
 		}),
 		react()
-	],
-	site: "https://arktype.io"
+	]
 })
