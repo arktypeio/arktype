38f6b12b0b7214cd27a7dd16e23b3356410ecbc0
David Blass <david@arktype.io>
2024-06-11 18:44:23 -0400

    continuuu

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,13 +1,37 @@
-import type { CastableBase } from "@arktype/util"
-import { type } from "arktype"
-import { scope } from "../type/scope.js"
-import type { Type } from "../type/type.js"
+import { type, type Type } from "arktype"
 
-const _Contact = type({
+const _contact = type({
 	email: "email",
 	score: "integer < 100"
 })
 
-interface Contact extends CastableBase<typeof _Contact.t> {}
+type _Contact = typeof _contact.t
 
-const Contact: Type<Contact> = _Contact
+interface Contact extends _Contact {}
+
+export const contact: Type<Contact> = _contact
+
+const contacts = contact.array().atLeastLength(1)
+
+const extractDuplicates = (arr: typeof contacts.infer) =>
+	Object.values(Object.groupBy(arr, contact => contact.email)).flatMap(
+		contactsWithEmail =>
+			contactsWithEmail && contactsWithEmail.length > 1 ?
+				contactsWithEmail[0].email
+			:	[]
+	)
+// ---cut---
+
+const uniqueContacts = contacts.narrow((arr, ctx) => {
+	// get a list of the duplicate emails
+	const duplicates = extractDuplicates(arr)
+
+	// return true if there are no duplicates
+	if (duplicates.length === 0) return true
+
+	// or false with a custom error message listing the duplicates
+	return ctx.invalid({
+		expected: "an array of unique contacts",
+		actual: `duplicated by ${duplicates}`
+	})
+})
