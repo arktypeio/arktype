ea933710a85f5faad932322128a3f569d5d45249
David Blass <david@arktype.io>
2024-07-19 17:31:28 -0400

    specific errors on multiple initialization

--- ark/schema/shared/registry.ts
+++ ark/schema/shared/registry.ts
@@ -1,4 +1,9 @@
-import { $ark, register } from "@ark/util"
+import {
+	$ark,
+	groupBy,
+	register,
+	type InitialRegistryContents
+} from "@ark/util"
 import type { NonNegativeIntegerString } from "../keywords/internal.js"
 
 let _registryName = "$ark"
@@ -8,6 +13,39 @@ while (_registryName in globalThis) _registryName = `$ark${suffix++}`
 
 export const registryName = _registryName
 ;(globalThis as any)[registryName] = $ark
+
+if (suffix !== 2) {
+	const g: any = globalThis
+	const registries: InitialRegistryContents[] = [g.$ark]
+	for (let i = 2; i < suffix; i++)
+		if (g[`$ark${i}`]) registries.push(g[`$ark${i}`])
+
+	console.warn(
+		`Multiple @ark registries detected. This can lead to unexpected behavior.`
+	)
+	const byPath = groupBy(registries, "filename")
+
+	const paths = Object.keys(byPath)
+
+	for (const path of paths) {
+		if (byPath[path]!.length > 1) {
+			console.warn(
+				`File ${path} was initialized multiple times, likely due to being imported from both CJS and ESM contexts.`
+			)
+		}
+	}
+
+	if (paths.length > 1) {
+		console.warn(
+			`Registries were initialized at the following paths:` +
+				paths
+					.map(
+						path => `	${path} (@ark/util version ${byPath[path]![0].version})`
+					)
+					.join("\n")
+		)
+	}
+}
 
 export const reference = (name: string): RegisteredReference =>
 	`${registryName}.${name}` as never
