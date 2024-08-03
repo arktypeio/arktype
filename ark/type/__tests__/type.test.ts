c75c7b0805851e682db0580e5de93ff798a5da9c
David Blass <david@arktype.io>
2024-08-03 17:32:53 -0400

    base bases base bas basdfds

--- ark/type/__tests__/type.test.ts
+++ ark/type/__tests__/type.test.ts
@@ -1,5 +1,5 @@
 import { attest, contextualize } from "@ark/attest"
-import { type } from "arktype"
+import { scope, type, type Type } from "arktype"
 import { AssertionError } from "node:assert"
 
 contextualize(() => {
@@ -39,6 +39,34 @@ contextualize(() => {
 		attest(t.assert({ a: "1" })).equals({ a: "1" })
 		attest(() => t.assert({ a: 1 })).throws.snap(
 			"AggregateError: a must be a string (was number)"
+		)
+	})
+
+	it("is treated as contravariant", () => {
+		const ok: Type<number> = type("1")
+
+		// currently treated as bivariant here, should be error
+		const shouldBeError: Type<string> = type("1")
+
+		// errors correctly if t is declared as its own type param
+		const accept = <t extends string>(t: Type<t>) => t
+
+		// @ts-expect-error
+		attest(() => accept(type("1"))).type.errors(
+			"Argument of type 'Type<1, {}>' is not assignable to parameter of type 'Type<string, {}>'"
+		)
+	})
+
+	it("Type.Any allows arbitrary scope", () => {
+		const foo = scope({
+			foo: "string"
+		}).resolve("foo")
+
+		const t: Type.Any<string> = foo
+
+		// @ts-expect-error (fails with default ambient type)
+		attest((): Type<string> => foo).type.errors(
+			"Type<string, { foo: string; }>' is not assignable to type 'Type<string, {}>'"
 		)
 	})
 })
