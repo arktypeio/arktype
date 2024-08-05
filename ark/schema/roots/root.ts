b3c77176c2cd7beec53e855a40e9fae7e1294241
David Blass <david@arktype.io>
2024-08-04 20:54:12 -0400

    fix again???

--- ark/schema/roots/root.ts
+++ ark/schema/roots/root.ts
@@ -335,6 +335,13 @@ export abstract class BaseRoot<
 		kind: kind,
 		schema: NodeSchema<kind>
 	): BaseRoot {
+		return this._constrain("root", kind, schema)
+	}
+
+	constrainIn<kind extends Constraint.PrimitiveKind>(
+		kind: kind,
+		schema: NodeSchema<kind>
+	): BaseRoot {
 		return this._constrain("in", kind, schema)
 	}
 
@@ -352,7 +359,10 @@ export abstract class BaseRoot<
 	): BaseRoot {
 		const constraint = this.$.node(kind, schema)
 		const operand = io === "root" ? this : this[io]
-		if (constraint.impliedBasis && !operand.extends(constraint.impliedBasis)) {
+		if (
+			operand.hasKind("morph") ||
+			(constraint.impliedBasis && !operand.extends(constraint.impliedBasis))
+		) {
 			return throwInvalidOperandError(
 				kind,
 				constraint.impliedBasis as never,
