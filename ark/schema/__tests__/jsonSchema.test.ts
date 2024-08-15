70ace6f4e813cc35f7a93489f2f9952662208507
David Blass <david@arktype.io>
2024-08-15 14:03:29 -0400

    cont in u

--- ark/schema/__tests__/jsonSchema.test.ts
+++ ark/schema/__tests__/jsonSchema.test.ts
@@ -1,5 +1,11 @@
 import { attest, contextualize } from "@ark/attest"
-import { intrinsic } from "@ark/schema"
+import {
+	$ark,
+	intrinsic,
+	rootNode,
+	writeCyclicJsonSchemaMessage,
+	writeJsonSchemaMorphMessage
+} from "@ark/schema"
 
 contextualize(() => {
 	it("base primitives", () => {
@@ -11,5 +17,24 @@ contextualize(() => {
 				{ const: null }
 			]
 		})
+	})
+
+	it("errors on morph", () => {
+		const morph = rootNode({
+			in: "string",
+			morphs: [(s: string) => Number.parseInt(s)]
+		})
+
+		attest(() => morph.toJsonSchema()).throws(
+			writeJsonSchemaMorphMessage(morph.expression)
+		)
+	})
+
+	it("errors on cyclic", () => {
+		attest(() => $ark.intrinsic.json.toJsonSchema()).throws.snap()
+
+		attest(() => $ark.intrinsic.json.toJsonSchema()).throws(
+			writeCyclicJsonSchemaMessage($ark.intrinsic.json.expression)
+		)
 	})
 })
