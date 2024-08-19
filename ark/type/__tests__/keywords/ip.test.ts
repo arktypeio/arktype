965aafb43d8e51249f97a54124317fc1ee77027b
David Blass <david@arktype.io>
2024-08-19 16:52:55 -0400

    arkdark5.7.4

--- ark/type/__tests__/keywords/ip.test.ts
+++ ark/type/__tests__/keywords/ip.test.ts
@@ -6,7 +6,7 @@ const validIPv6 = "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
 
 contextualize(() => {
 	it("root", () => {
-		const ip = type("ip")
+		const ip = type("string.ip")
 
 		attest(ip(validIPv4)).equals(validIPv4)
 
