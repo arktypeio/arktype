760a8330753b79c66aea261a7ded23b69c74ff71
David Blass <david@arktype.io>
2024-08-28 17:30:55 -0400

    revert

--- ark/util/__tests__/clone.test.ts
+++ ark/util/__tests__/clone.test.ts
@@ -1,5 +1,5 @@
 import { attest, contextualize } from "@ark/attest"
-import { deepClone } from "@ark/util"
+import { deepClone, shallowClone } from "@ark/util"
 
 contextualize(() => {
 	it("should return a deep clone of a simple object", () => {
@@ -26,21 +26,6 @@ contextualize(() => {
 		attest(cloned.self === cloned).equals(true)
 	})
 
-	it("should preserve the prototype of the object", () => {
-		class MyClass {
-			a: number
-
-			constructor(a: number) {
-				this.a = a
-			}
-		}
-		const obj = new MyClass(1)
-		const cloned = deepClone(obj)
-		attest<typeof obj>(cloned).equals(obj)
-		attest(cloned !== obj).equals(true)
-		attest(cloned).instanceOf(MyClass)
-	})
-
 	it("should handle deep cycles correctly", () => {
 		const obj: any = { a: 1, b: { c: 2 } }
 		obj.b.self = obj
@@ -48,22 +33,6 @@ contextualize(() => {
 		attest<typeof obj>(cloned).equals(obj)
 		attest(cloned !== obj).equals(true)
 		attest(cloned.b.self === cloned).equals(true)
-	})
-
-	it("should preserve the prototype of nested objects", () => {
-		class MyClass {
-			a: number
-
-			constructor(a: number) {
-				this.a = a
-			}
-		}
-		const obj = { a: 1, b: new MyClass(2) }
-		const cloned = deepClone(obj)
-		attest<typeof obj>(cloned).equals(obj)
-		attest(cloned !== obj).equals(true)
-		attest(cloned.b !== obj.b).equals(true)
-		attest(cloned.b instanceof MyClass).equals(true)
 	})
 
 	it("should handle null values correctly", () => {
@@ -88,53 +57,6 @@ contextualize(() => {
 		attest(cloned.b !== obj.b).equals(true)
 	})
 
-	it("should handle array subclasses with extra properties correctly", () => {
-		class MyArray extends Array {
-			extraProp = "extra"
-		}
-
-		const arr = new MyArray()
-		arr.push(1, 2, 3)
-		const cloned = deepClone(arr)
-
-		attest<MyArray>(cloned).equals(arr)
-		attest(cloned !== arr).equals(true)
-		attest(cloned.extraProp).equals(arr.extraProp)
-	})
-
-	it("rebinds methods", () => {
-		class MyClass {
-			a: number
-
-			constructor(a: number) {
-				this.a = a
-			}
-
-			getA() {
-				return this.a
-			}
-
-			setA(a: number) {
-				this.a = a
-			}
-		}
-
-		const original = new MyClass(5)
-		const cloned = deepClone(original)
-
-		attest(cloned.getA()).equals(5)
-
-		cloned.a = 6
-
-		attest(cloned.getA()).equals(6)
-
-		cloned.setA(7)
-
-		attest(cloned.getA()).equals(7)
-
-		attest(original.a).equals(5)
-	})
-
 	it("Date", () => {
 		const original = new Date(2000, 1)
 		const cloned = deepClone(original)
@@ -146,31 +68,5 @@ contextualize(() => {
 		cloned.setDate(3)
 		attest(cloned.getDate()).equals(3)
 		attest(original.getDate()).equals(2)
-	})
-
-	it("Set", () => {
-		const original = new Set([1, 2, 3])
-		const cloned = deepClone(original)
-		attest(cloned).instanceOf(Set)
-		attest(cloned.has(1)).equals(true)
-		attest(cloned.has(4)).equals(false)
-
-		cloned.add(4)
-		attest(cloned.has(4)).equals(true)
-		attest(original.has(4)).equals(false)
-	})
-
-	it("Map", () => {
-		const original = new Map([
-			[1, 2],
-			[3, 4]
-		])
-		const cloned = deepClone(original)
-		attest(cloned).instanceOf(Map)
-		attest(cloned.get(1)).equals(2)
-		attest(cloned.get(4)).equals(undefined)
-		cloned.set(5, 6)
-		attest(cloned.get(5)).equals(6)
-		attest(original.get(5)).equals(undefined)
 	})
 })
--- ark/util/clone.ts
+++ ark/util/clone.ts
@@ -1,37 +1,19 @@
-import {
-	builtinConstructors,
-	getBuiltinNameOfConstructor,
-	objectKindOf,
-	type BuiltinObjectKind
-} from "./objectKinds.ts"
-import type { keySet } from "./records.ts"
+import { getBuiltinNameOfConstructor } from "./objectKinds.ts"
 
-/** Shallowly copy the properties and prototype of the object.
- *
- * NOTE: this still cannot guarantee arrow functions attached to the object
- * are rebound in case they reference `this`.
- *
- * See: https://x.com/colinhacks/status/1818422039210049985
- */
+/** Shallowly copy the properties of the object. */
 export const shallowClone: <input extends object>(
 	input: input
 ) => input = input => _clone(input, null)
 
-/** Deeply copy the properties and prototype of the object.
+/** Deeply copy the properties of the a non-subclassed Object, Array or Date.
  *
- * NOTE: this still cannot guarantee arrow functions attached to the object
- * are rebound in case they reference `this`.
+ * @param input The object to clone
  *
- * See: https://x.com/colinhacks/status/1818422039210049985
+ * @returns A new deeply cloned version of the object, or the original object
+ * if it has a prototype other than Object, Array Date, or null.
  */
 export const deepClone = <input extends object>(input: input): input =>
 	_clone(input, new Map())
-
-const deepClonableObjectKind: keySet<BuiltinObjectKind> = {
-	Array: 1,
-	Set: 1,
-	Map: 1
-}
 
 const _clone = (input: unknown, seen: Map<unknown, unknown> | null): any => {
 	if (typeof input !== "object" || input === null) return input
@@ -39,26 +21,16 @@ const _clone = (input: unknown, seen: Map<unknown, unknown> | null): any => {
 
 	const builtinConstructorName = getBuiltinNameOfConstructor(input.constructor)
 
-	if (builtinConstructorName) {
-		if (!deepClonableObjectKind[builtinConstructorName]) {
-			if (builtinConstructorName === "Date")
-				return new Date((input as Date).getTime())
-			else return input
-		}
-	} else {
-		const inheritableBuiltinConstructorName = objectKindOf(input)
-		if (
-			inheritableBuiltinConstructorName &&
-			inheritableBuiltinConstructorName !== "Array"
-		)
-			// we can clone array subclass using slice, but not other subclasses of other builtins like Set and Map
-			return input
-	}
+	if (builtinConstructorName === "Date")
+		return new Date((input as Date).getTime())
+
+	// we don't try and clone other prototypes here since this we can't guarantee arrow functions attached to the object
+	// are rebound in case they reference `this` (see https://x.com/colinhacks/status/1818422039210049985)
+	if (builtinConstructorName && builtinConstructorName !== "Array") return input
 
 	const cloned =
-		Array.isArray(input) ? input.slice()
-		: builtinConstructorName ?
-			new (builtinConstructors[builtinConstructorName] as any)()
+		Array.isArray(input) ?
+			input.slice()
 		:	Object.create(Object.getPrototypeOf(input))
 
 	const propertyDescriptors = Object.getOwnPropertyDescriptors(input)
@@ -70,13 +42,6 @@ const _clone = (input: unknown, seen: Map<unknown, unknown> | null): any => {
 	}
 
 	Object.defineProperties(cloned, propertyDescriptors)
-
-	if (builtinConstructorName === "Set")
-		for (const item of input as Set<unknown>) cloned.add(_clone(item, seen))
-	else if (builtinConstructorName === "Map") {
-		for (const [k, v] of input as Map<unknown, unknown>)
-			cloned.set(k, _clone(v, seen))
-	}
 
 	return cloned
 }
