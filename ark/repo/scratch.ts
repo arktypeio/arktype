ceac0d7bb27f65218b6685a5c481e0231774ddc4
David Blass <david@arktype.io>
2024-09-08 19:54:13 -0400

    ok

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,11 +1,5 @@
 import { type } from "arktype"
 
-const t = type({ foo: "1" })
-	.or({ bar: "1" })
-	.pipe(o => Object.values(o))
-
-console.log(t.internal.precompilation)
-
-type({
-	foo: "string.normalize.NFC.preformatted"
+export const myType = type({ foo: "0 < number < 10" }).or({
+	bar: "string.alphanumeric"
 })
