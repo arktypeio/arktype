72c8b4b61ca86f9996fb0242cc9d2aac70d163c8
David Blass <david@arktype.io>
2024-09-14 00:37:15 -0400

    continuu

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,25 +1,7 @@
 import { type } from "arktype"
 
-const oneTo9999 = type("1 <= number.integer <= 9999")
-
-const T = type({
-	PORT: ["string.integer.parse", "=>", oneTo9999]
-})
-const defaults: typeof T.infer = {
-	PORT: 123
-}
-
-process.env.PORT = "456"
-
-const out = T.assert(process.env)
-
-console.log(out.PORT)
-
-export const _assetOptionsSchema = type({
-	"assetType?": type("string")
-		.default("image")
-		.configure({
-			text: "Uses Generative Fill to extended padded image with AI",
-			url: "https://cloudinary.com/documentation/transformation_reference#g_gravity"
-		} as ArkEnv.meta)
+export const cloudinaryResource = type({
+	"[string]": "unknown",
+	"alt?": "string",
+	"caption?": "string"
 })
