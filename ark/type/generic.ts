013a3088faa2568a16aa9360e2c05685898d590a
David Blass <david@arktype.io>
2024-09-17 08:48:56 -0400

    no generic noinfer

--- ark/type/generic.ts
+++ ark/type/generic.ts
@@ -60,7 +60,7 @@ export type GenericInstantiator<
 			// some of type's methods
 			<const a, r = instantiateGeneric<def, params, [a], $, args$>>(
 				a: type.validate<a, args$> & validateGenericArg<a, params[0], args$>
-			): NoInfer<r>
+			): r
 		}
 	: params["length"] extends 2 ?
 		{
