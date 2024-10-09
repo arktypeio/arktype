1b7276fee03c7ed92a542f560a52bea3ab935f75
David Blass <david@arktype.io>
2024-10-09 16:28:02 -0400

    test branding

--- ark/type/keywords/string/string.ts
+++ ark/type/keywords/string/string.ts
@@ -10,6 +10,7 @@ import type {
 	Narrowed,
 	Nominal,
 	Optional,
+	brand,
 	constraint,
 	of
 } from "../inference.ts"
@@ -121,6 +122,42 @@ export declare namespace string {
 		capitalize: capitalize.submodule
 		lower: lower.submodule
 		upper: upper.submodule
+	}
+
+	export namespace branded {
+		export type atLeastLength<rule> = brand<string, AtLeastLength<rule>>
+
+		export type moreThanLength<rule> = brand<string, MoreThanLength<rule>>
+
+		export type atMostLength<rule> = brand<string, AtMostLength<rule>>
+
+		export type lessThanLength<rule> = brand<string, LessThanLength<rule>>
+
+		export type exactlyLength<rule> = brand<string, ExactlyLength<rule>>
+
+		export type matching<rule> = brand<string, Matching<rule>>
+
+		export type narrowed = brand<string, Narrowed>
+
+		export type optional = brand<string, Optional>
+
+		export type defaultsTo<rule> = brand<string, Default<rule>>
+
+		export type branded<rule> = brand<string, Nominal<rule>>
+
+		export type is<attributes> = brand<string, attributes>
+
+		export type applyBrand<attribute> =
+			attribute extends ExactlyLength<infer rule> ? exactlyLength<rule>
+			: attribute extends MoreThanLength<infer rule> ? moreThanLength<rule>
+			: attribute extends AtLeastLength<infer rule> ? atLeastLength<rule>
+			: attribute extends AtMostLength<infer rule> ? atMostLength<rule>
+			: attribute extends LessThanLength<infer rule> ? lessThanLength<rule>
+			: attribute extends Matching<infer rule> ? matching<rule>
+			: attribute extends Optional ? optional
+			: attribute extends Default<infer rule> ? defaultsTo<rule>
+			: attribute extends Nominal<infer rule> ? branded<rule>
+			: never
 	}
 }
 
