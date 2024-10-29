8dc58260bfa2fbe393be1d0b3738b8c229188495
TizzySaurus <tizzysaurus@gmail.com>
2024-10-29 21:16:11 +0000

    Fix types for innerParseJsonSchema

--- ark/jsonschema/json.ts
+++ ark/jsonschema/json.ts
@@ -1,5 +1,5 @@
 import { printable, throwParseError } from "@ark/util"
-import { type, type Out, type Type } from "arktype"
+import { type } from "arktype"
 import { parseJsonSchemaAnyKeywords } from "./any.ts"
 import { validateJsonSchemaArray } from "./array.ts"
 import { parseJsonSchemaCompositionKeywords } from "./composition.ts"
@@ -8,70 +8,69 @@ import { validateJsonSchemaObject } from "./object.ts"
 import { JsonSchema } from "./scope.ts"
 import { validateJsonSchemaString } from "./string.ts"
 
-export const innerParseJsonSchema: Type<
-	(In: JsonSchema.Schema) => Out<Type<unknown, any>>,
-	any
-> = JsonSchema.Schema.pipe(jsonSchema => {
-	if (typeof jsonSchema === "boolean") {
-		if (jsonSchema) return JsonSchema.Json
-		else return type("never") // No runtime value ever passes validation for JSON schema of 'false'
-	}
-
-	if (Array.isArray(jsonSchema)) {
-		return (
-			parseJsonSchemaCompositionKeywords({ anyOf: jsonSchema }) ??
-			throwParseError(
-				"Failed to convert root array of JSON Schemas to an anyOf schema"
-			)
-		)
-	}
-
-	const constAndOrEnumValidator = parseJsonSchemaAnyKeywords(jsonSchema)
-	const compositionValidator = parseJsonSchemaCompositionKeywords(jsonSchema)
-
-	const preTypeValidator: Type | undefined =
-		constAndOrEnumValidator ?
-			compositionValidator ? compositionValidator.and(constAndOrEnumValidator)
-			:	constAndOrEnumValidator
-		:	compositionValidator
-
-	if ("type" in jsonSchema) {
-		let typeValidator: Type<unknown, any>
-
-		switch (jsonSchema.type) {
-			case "array":
-				typeValidator = validateJsonSchemaArray.assert(jsonSchema) as never // A bug in ArkType makes this cast necessary
-				break
-			case "boolean":
-			case "null":
-				typeValidator = type(jsonSchema.type)
-				break
-			case "integer":
-			case "number":
-				typeValidator = validateJsonSchemaNumber.assert(jsonSchema) as never // A bug in ArkType makes this cast necessary
-				break
-			case "object":
-				typeValidator = validateJsonSchemaObject.assert(jsonSchema) as never // A bug in ArkType makes this cast necessary
-				break
-			case "string":
-				typeValidator = validateJsonSchemaString.assert(jsonSchema) as never // A bug in ArkType makes this cast necessary
-				break
-			default:
-				throwParseError(
-					// @ts-expect-error -- All valid 'type' values should be handled above
-					`Provided 'type' value must be a supported JSON Schema type (was '${jsonSchema.type}')`
-				)
+export const innerParseJsonSchema = JsonSchema.Schema.pipe(
+	(jsonSchema: JsonSchema.Schema): type.Any => {
+		if (typeof jsonSchema === "boolean") {
+			if (jsonSchema) return JsonSchema.Json
+			else return type("never") // No runtime value ever passes validation for JSON schema of 'false'
 		}
-		if (preTypeValidator === undefined) return typeValidator
-		return typeValidator.and(preTypeValidator)
-	}
-	if (preTypeValidator === undefined) {
-		throwParseError(
-			`Provided JSON Schema must have one of 'type', 'enum', 'const', 'allOf', 'anyOf' but was ${printable(jsonSchema)}.`
-		)
-	}
-	return preTypeValidator
-})
 
-export const parseJsonSchema = (jsonSchema: JsonSchema.Schema): Type =>
+		if (Array.isArray(jsonSchema)) {
+			return (
+				parseJsonSchemaCompositionKeywords({ anyOf: jsonSchema }) ??
+				throwParseError(
+					"Failed to convert root array of JSON Schemas to an anyOf schema"
+				)
+			)
+		}
+
+		const constAndOrEnumValidator = parseJsonSchemaAnyKeywords(jsonSchema)
+		const compositionValidator = parseJsonSchemaCompositionKeywords(jsonSchema)
+
+		const preTypeValidator: type.Any | undefined =
+			constAndOrEnumValidator ?
+				compositionValidator ? compositionValidator.and(constAndOrEnumValidator)
+				:	constAndOrEnumValidator
+			:	compositionValidator
+
+		if ("type" in jsonSchema) {
+			let typeValidator: type.Any
+
+			switch (jsonSchema.type) {
+				case "array":
+					typeValidator = validateJsonSchemaArray.assert(jsonSchema)
+					break
+				case "boolean":
+				case "null":
+					typeValidator = type(jsonSchema.type)
+					break
+				case "integer":
+				case "number":
+					typeValidator = validateJsonSchemaNumber.assert(jsonSchema)
+					break
+				case "object":
+					typeValidator = validateJsonSchemaObject.assert(jsonSchema)
+					break
+				case "string":
+					typeValidator = validateJsonSchemaString.assert(jsonSchema)
+					break
+				default:
+					throwParseError(
+						// @ts-expect-error -- All valid 'type' values should be handled above
+						`Provided 'type' value must be a supported JSON Schema type (was '${jsonSchema.type}')`
+					)
+			}
+			if (preTypeValidator === undefined) return typeValidator
+			return typeValidator.and(preTypeValidator)
+		}
+		if (preTypeValidator === undefined) {
+			throwParseError(
+				`Provided JSON Schema must have one of 'type', 'enum', 'const', 'allOf', 'anyOf' but was ${printable(jsonSchema)}.`
+			)
+		}
+		return preTypeValidator
+	}
+)
+
+export const parseJsonSchema = (jsonSchema: JsonSchema.Schema): type.Any =>
 	innerParseJsonSchema.assert(jsonSchema) as never
