6ee2db6dbd2588b7e0f8d78d03a9988e56423190
David Blass <david@arktype.io>
2024-11-19 15:10:41 -0500

    add more notes on optionality

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,16 +1,11 @@
 import { type } from "arktype"
 
 const _user = type({
-	name: "string"
+	name: "string | undefined"
 })
 
 interface User extends type.infer<typeof _user> {}
 
 const user: type<User> = _user
-const booleans = type({
-	a: type.boolean,
-	b: type.keywords.true,
-	c: type.keywords.true,
-	// equivalent to boolean
-	d: type.keywords.true.or(type.keywords.false)
-})
+
+user({}).toString()
