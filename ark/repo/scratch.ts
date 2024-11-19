accaaef667494590b39e2681b36e5a1630083bd8
David Blass <david@arktype.io>
2024-11-19 14:33:36 -0500

    add more docs

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -7,3 +7,10 @@ const _user = type({
 interface User extends type.infer<typeof _user> {}
 
 const user: type<User> = _user
+const booleans = type({
+	a: type.boolean,
+	b: type.keywords.true,
+	c: type.keywords.true,
+	// equivalent to boolean
+	d: type.keywords.true.or(type.keywords.false)
+})
