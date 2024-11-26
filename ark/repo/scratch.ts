44e551f96359ab9ba74e32386573d3bbb7456c94
David Blass <david@arktype.io>
2024-11-26 16:02:19 -0500

    add ranges

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -9,3 +9,25 @@ interface User extends type.infer<typeof _user> {}
 const user: type<User> = _user
 
 user({}).toString()
+
+Date.now()
+
+const tenYearsAgo = new Date()
+	.setFullYear(new Date().getFullYear() - 10)
+	.valueOf()
+
+const bounded = type({
+	dateInTheLast10Years: `${tenYearsAgo} <= Date < ${Date.now()}`
+})
+
+Date.now() //?
+
+//?
+
+type.Date.laterThan("")
+
+const out = bounded.assert({
+	dateInThePast: new Date(0),
+	dateAfter2000: new Date(),
+	dateAtOrAfter1970: new Date(0)
+})
