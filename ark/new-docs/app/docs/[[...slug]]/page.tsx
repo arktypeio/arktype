0c322b88d9ef54339195bc045f8784ac884c14b3
David Blass <david@arktype.io>
2024-12-01 21:57:59 -0500

    continuuu

--- ark/new-docs/app/docs/[[...slug]]/page.tsx
+++ ark/new-docs/app/docs/[[...slug]]/page.tsx
@@ -18,7 +18,7 @@ export default async (props: { params: Promise<{ slug?: string[] }> }) => {
 	const MDX = page.data.body
 
 	return (
-		<DocsPage toc={page.data.toc} full={page.data.full}>
+		<DocsPage toc={page.data.toc} full={page.data.full ?? false}>
 			<DocsTitle>{page.data.title}</DocsTitle>
 			<DocsDescription>{page.data.description}</DocsDescription>
 			<DocsBody>
