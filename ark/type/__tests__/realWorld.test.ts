3dd1d88f376495f54e3ea4007aead3f2062cf1be
David Blass <david@arktype.io>
2024-12-04 21:44:14 -0500

    fix type snapshot

--- ark/type/__tests__/realWorld.test.ts
+++ ark/type/__tests__/realWorld.test.ts
@@ -1056,8 +1056,8 @@ nospace must be matched by ^\\S*$ (was "One space")`)
 		attest(feedbackSchema.expression).snap(
 			"{ contact: string == 0 | string /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$/ }"
 		)
-		attest(feedbackSchema.t).type.toString.snap(`{
-    contact: string.email | string.is<ExactlyLength<0>>;
-}`)
+		attest(feedbackSchema.t).type.toString.snap(
+			`{ contact: email | is<ExactlyLength<0>> }`
+		)
 	})
 })
