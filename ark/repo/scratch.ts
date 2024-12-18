8f60c023688b554d064da00c4d185002bebfe008
David Blass <david@arktype.io>
2024-12-18 12:37:25 -0500

    update stats

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,24 +1,10 @@
-import { type, type Ark } from "arktype"
+import { type } from "arktype"
 
-// type stats 12/7/2024 on main
+// type stats on attribute removal merge 12/18/2024
 // {
-//     "checkTime": 21.41,
-//     "types": 826014,
-//     "instantiations": 7316952
-// }
-
-// type stats 12/8/2024 on rc27
-// {
-//     "checkTime": 12.94,
-//     "types": 402148,
-//     "instantiations": 4850453
-// }
-
-// 12/13/2024
-// {
-//     "checkTime": 15.84,
-//     "types": 412039,
-//     "instantiations": 5075017
+//     "checkTime": 10.98,
+//     "types": 409252,
+//     "instantiations": 5066185
 // }
 
 // false
