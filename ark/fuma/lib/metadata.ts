14ae0f6ca8f4e2ba96ef2e8a6b42932bdcce5842
David Blass <david@arktype.io>
2024-12-19 18:38:57 -0500

    ok

--- ark/fuma/lib/metadata.ts
+++ ark/fuma/lib/metadata.ts
@@ -1,5 +1,5 @@
 import { createMetadataImage } from "fumadocs-core/server"
-import { source } from "./source.ts"
+import { source } from "./source.js"
 
 export const metadataImage = createMetadataImage({
 	imageRoute: "/docs-og",
