f6df0ca92d1a78e0048dcdf9e4ea3daa96db5772
David Blass <david@arktype.io>
2024-12-23 16:36:15 -0500

    fix links

---
+++ ark/docs/app/discord/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useEffect } from "react"
+
+export default () => {
+	useEffect(() => {
+		window.location.href = "https://discord.com/invite/xEzdc3fJQC"
+	}, [])
+
+	return null
+}
