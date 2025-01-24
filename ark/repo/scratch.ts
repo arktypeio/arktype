5edb9afaec0722a97e74b153b89375355d88ce74
David Blass <david@arktype.io>
2025-01-23 20:49:37 -0500

    continuu

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -6,16 +6,3 @@ const user = type({
 })
 
 export type User = typeof user.infer
-
-const badScope = scope({
-	id: "string",
-	// the global 'type' only knows about builtin keywords!
-	badEntity: type({
-		// TypeScript: 'id' is unresolvable
-		id: "id"
-	}),
-	// reference scoped definitions directly instead of wrapping them
-	goodEntity: {
-		id: "id"
-	}
-})
