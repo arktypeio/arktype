f006c9f77184d5b1e84a80689184334d31e81f06
David Blass <david@arktype.io>
2025-01-26 15:35:48 -0500

    scratch that

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,15 +1,6 @@
 import { type } from "arktype"
-import dayjs from "dayjs"
 
-dayjs.isDayjs
-
-// Dayjs is undefined here in my env
-const { Dayjs } = dayjs
-
-console.log(Dayjs)
-
-const out = type({
-	dayjs: type.instanceOf(Dayjs)
-})({
-	dayjs: dayjs()
+const t = type({
+	key: "string",
+	value: "string|number|boolean"
 })
