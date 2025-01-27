41292da94891d280029bfdb5ed0bac02925ef055
David Blass <david@arktype.io>
2025-01-27 16:56:35 -0500

    continuuu

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,19 +1,8 @@
 import { declare, scope, type } from "arktype"
 
-const point2d = type({
-	x: "number",
-	y: "number",
-	"+": "delete"
-})
-
-const point3d = type({
-	x: "number",
-	y: "number",
-	z: "number",
-	"+": "delete"
-})
-
-const t = point2d.or(point3d)
-
 // number
 const n = type("1 | number").expression
+
+const t = type("string.trim > 2")
+
+console.log(t.expression)
