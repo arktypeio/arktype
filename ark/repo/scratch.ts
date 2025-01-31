20a9f7537b045819e848349a3cc7acea337d770d
David Blass <david@arktype.io>
2025-01-30 21:42:13 -0500

    continuuu

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,18 +1,21 @@
 import "./config.ts"
 
-import { match, type } from "arktype"
+import { match } from "arktype"
 
 const sizeOf = match({
-	"string|Array": v => v.length,
+	"string | Array": v => v.length,
 	number: v => v,
 	bigint: v => v,
 	default: "assert"
 })
-const size1 = sizeOf("foo")
-console.log(size1)
-const size = sizeOf({})
 
-// default: "never" - throw on other value, input is narrowed
-// default: "assert" - throw on other value, input is unknown
-// default: "reject" - return ArkError on other value, input is unknown
-// default: () => unknown - specify a default value
+const size = sizeOf("foo")
+//    ^?
+console.log(size) // 3
+
+// narrows output from case mapping
+const bigSize = sizeOf(999n)
+//     ^?
+
+// MatchError: must be a string or an object, a number or a bigint (was boolean)
+const bad = sizeOf(true)
