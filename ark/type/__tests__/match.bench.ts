0c22babc54f096b247309f4c7ac963adba95ad8a
David Blass <david@arktype.io>
2025-02-04 23:16:36 -0500

    benches

--- ark/type/__tests__/match.bench.ts
+++ ark/type/__tests__/match.bench.ts
@@ -1,48 +1,134 @@
-// import { bench } from "@ark/attest"
-// import { match } from "arktype"
+import { bench } from "@ark/attest"
+import { match, type } from "arktype"
 
-// bench.baseline(() => type("never"))
+bench.baseline(() => {
+	type("never")
+	match.case("never", () => {}).default("assert")
+	match({
+		never: () => {},
+		default: "assert"
+	})
+})
 
-// bench("when(3)", () => {
-// 	const matcher = match()
-// 		.when("0", n => `${n}` as const)
-// 		.when("1", n => `${n}` as const)
-// 		.when("2", n => `${n}` as const)
-// 		.orThrow()
+bench("case(3, define)", () =>
+	match
+		.case("-1", n => `${n}` as const)
+		.case("-2", n => `${n}` as const)
+		.case("-3", n => `${n}` as const)
+		.default("assert")
+).types([2784, "instantiations"])
 
-// 	const zero = matcher(0)
-// 	const one = matcher(1)
-// 	const two = matcher(2)
-// }).types([34953, "instantiations"])
+bench("case(10, define)", () =>
+	match
+		.case("0", n => `${n}` as const)
+		.case("1", n => `${n}` as const)
+		.case("2", n => `${n}` as const)
+		.case("3", n => `${n}` as const)
+		.case("4", n => `${n}` as const)
+		.case("5", n => `${n}` as const)
+		.case("6", n => `${n}` as const)
+		.case("7", n => `${n}` as const)
+		.case("8", n => `${n}` as const)
+		.case("9", n => `${n}` as const)
+		.default("assert")
+).types([9528, "instantiations"])
 
-// bench("when(10)", () => {
-// 	const matcher = match()
-// 		.when("0", n => `${n}` as const)
-// 		.when("1", n => `${n}` as const)
-// 		.when("2", n => `${n}` as const)
-// 		.when("3", n => `${n}` as const)
-// 		.when("4", n => `${n}` as const)
-// 		.when("5", n => `${n}` as const)
-// 		.when("6", n => `${n}` as const)
-// 		.when("7", n => `${n}` as const)
-// 		.when("8", n => `${n}` as const)
-// 		.when("9", n => `${n}` as const)
-// 		.orThrow()
+bench("match.in<t> cases define and invoke", () => {
+	const matcher = match
+		.in<string | number | boolean>()
+		.case("string", s => s)
+		.case("number", n => n)
+		.case("boolean", b => b)
+		.default("assert")
 
-// 	const zero = matcher(0)
-// 	const one = matcher(1)
-// 	const two = matcher(2)
-// }).types([98902, "instantiations"])
+	const zero = matcher("abc")
+	const one = matcher(4)
+	const two = matcher(true)
+	return [zero, one, two]
+}).types([3035, "instantiations"])
 
-// bench("match.only<T>", () => {
-// 	const matcher = match
-// 		.only<string | number | boolean>()
-// 		.when("string", s => s)
-// 		.when("number", n => n)
-// 		.when("boolean", b => b)
-// 		.finalize()
+bench("record(3, define)", () =>
+	match({
+		"20": n => `${n}` as const,
+		"21": n => `${n}` as const,
+		"22": n => `${n}` as const,
+		default: "assert"
+	})
+).types([2182, "instantiations"])
 
-// 	const a = matcher("abc")
-// 	const b = matcher(4)
-// 	const c = matcher(true)
-// }).types([37333, "instantiations"])
+bench("record(10, define)", () =>
+	match({
+		"-10n": n => `${n}` as const,
+		"-1n": n => `${n}` as const,
+		"-2n": n => `${n}` as const,
+		"-3n": n => `${n}` as const,
+		"-4n": n => `${n}` as const,
+		"-5n": n => `${n}` as const,
+		"-6n": n => `${n}` as const,
+		"-7n": n => `${n}` as const,
+		"-8n": n => `${n}` as const,
+		"-9n": n => `${n}` as const,
+		default: "assert"
+	})
+).types([7297, "instantiations"])
+
+bench("record.in<t> define and invoke", () => {
+	const matcher = match.in<string | number | boolean>().match({
+		symbol: s => s,
+		object: n => n,
+		boolean: b => b,
+		default: "assert"
+	})
+
+	const zero = matcher("abc")
+	const one = matcher(4)
+	const two = matcher(true)
+	return [zero, one, two]
+}).types([3386, "instantiations"])
+
+// For some reason, these calls don't register instantiations
+// will have to look into that later, although generally
+// I'd expect the definition to be the more expensive API anyways
+
+// const invokedCases3 = match
+// 	.case("31", n => `${n}` as const)
+// 	.case("32", n => `${n}` as const)
+// 	.case("33", n => `${n}` as const)
+// 	.default("assert")
+
+// bench("case(3, invoke)", () => {
+// 	const zero = invokedCases3(31)
+// 	const one = invokedCases3(32)
+// 	const two = invokedCases3(33)
+
+// 	return [zero, one, two]
+// }).types([0, "instantiations"])
+
+// const invokedCases10 = match
+// 	.case("0n", n => `${n}` as const)
+// 	.case("1n", n => `${n}` as const)
+// 	.case("2n", n => `${n}` as const)
+// 	.case("3n", n => `${n}` as const)
+// 	.case("4n", n => `${n}` as const)
+// 	.case("5n", n => `${n}` as const)
+// 	.case("6n", n => `${n}` as const)
+// 	.case("7n", n => `${n}` as const)
+// 	.case("8n", n => `${n}` as const)
+// 	.case("9n", n => `${n}` as const)
+// 	.default("assert")
+
+// bench("case(10, invoke first)", () => {
+// 	const zero = invokedCases10(0n)
+// 	const one = invokedCases10(1n)
+// 	const two = invokedCases10(2n)
+
+// 	return [zero, one, two]
+// }).types([0, "instantiations"])
+
+// bench("case(10, invoke last)", () => {
+// 	const zero = invokedCases10(8n)
+// 	const one = invokedCases10(9n)
+// 	const two = invokedCases10(10n)
+
+// 	return [zero, one, two]
+// }).types([0, "instantiations"])
