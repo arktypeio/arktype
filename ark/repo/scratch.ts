2ce1cef86cd10dbf07c94e84952f351f8aeff690
David Blass <david@arktype.io>
2025-02-06 08:51:48 -0500

    update docs

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,26 +1,43 @@
 import { bench } from "@ark/attest"
 import { match, type } from "arktype"
 
+const disappointingGift = type({
+	label: "string",
+	"box?": "this"
+})
+
+const out = disappointingGift({
+	label: "foo",
+	box: { label: "bar", box: {} }
+})
+
+if (out instanceof type.errors) {
+	// ArkErrors: box.box.label must be a string (was missing)
+	console.error(out.summary)
+} else {
+	console.log(out.box?.box?.label)
+}
+
 // bench("morph", () => {
 // 	type.keywords.string.numeric.parse("5")
 // }).mean()
 
-const t = type({
-	"+": "delete",
-	a: "string"
-})
+// const t = type({
+// 	"+": "delete",
+// 	a: "string"
+// })
 
-bench("good", () => {
-	t({ a: "foo" })
-}).mean([253.32, "ns"])
+// bench("good", () => {
+// 	t({ a: "foo" })
+// }).mean([253.32, "ns"])
 
-bench("delete one", () => {
-	t({ a: "foo", b: true })
-}).mean([2.59, "us"])
+// bench("delete one", () => {
+// 	t({ a: "foo", b: true })
+// }).mean([2.59, "us"])
 
-bench("delete five", () => {
-	t({ a: "foo", b: true, c: true, d: true, e: true, f: true })
-}).mean([6.1, "us"])
+// bench("delete five", () => {
+// 	t({ a: "foo", b: true, c: true, d: true, e: true, f: true })
+// }).mean([6.1, "us"])
 
 // const m = match
 // 	.case("31", n => `${n}` as const)
