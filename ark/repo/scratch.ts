81a7bba6cf0614da49b51ec0e7ae2ff2990b4cdd
David Blass <david@arktype.io>
2025-02-08 00:48:45 -0500

    continuu

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,40 +1,5 @@
 import { type } from "arktype"
 
-interface RuntimeErrors extends type.errors {
-	/**even must be even (was 7)
-odd must be odd (was 8)*/
-	summary: string
-}
-
-const narrowMessage = (e: type.errors): e is RuntimeErrors => true
-// ---cut---
-
-// there's no "not divisible" expression - need to narrow
-const odd = type("number").narrow((n, ctx) =>
-	// if even, add a customizable error and return false
-	n % 2 === 0 ? ctx.mustBe("odd") : true
-)
-
-const favoriteNumbers = type({
-	even: "number % 2",
-	odd
-})
-
-const out = favoriteNumbers({
-	even: 7,
-	odd: 8
-})
-
-if (out instanceof type.errors) {
-	// ---cut-start---
-	if (!narrowMessage(out)) throw new Error()
-	// ---cut-end---
-	// hover summary to see validation errors
-	console.error(out.summary)
-} else {
-	console.log(out.odd)
-}
-
 // bench("morph", () => {
 // 	type.keywords.string.numeric.parse("5")
 // }).mean()
