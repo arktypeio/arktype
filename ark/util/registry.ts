de532016f59b9422be4442da53d779b3b92b8c51
TizzySaurus <tizzysaurus@gmail.com>
2025-02-15 23:23:48 +0000

    Revert @ark/util version bump, as per PR comment

--- ark/util/registry.ts
+++ ark/util/registry.ts
@@ -8,7 +8,7 @@ import { FileConstructor, objectKindOf } from "./objectKinds.ts"
 // recent node versions (https://nodejs.org/api/esm.html#json-modules).
 
 // For now, we assert this matches the package.json version via a unit test.
-export const arkUtilVersion = "0.40.0"
+export const arkUtilVersion = "0.39.0"
 
 export const initialRegistryContents = {
 	version: arkUtilVersion,
