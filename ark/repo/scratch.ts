81393ada67e118a7137dcdb3e5fe8eb7708e5068
David Blass <david@arktype.io>
2025-03-08 12:27:13 -0500

    scratch

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -9,3 +9,12 @@ const user = type({
 const defaultableProps = user.props.filter(
 	p => p.kind === "optional" && "default" in p
 )
+
+const nanToNull = type("number.NaN").pipe(() => null, type.null)
+
+const nullNumber = type("number").or(nanToNull)
+
+const out = nullNumber(5) // 5
+const out2 = nullNumber(Number.NaN) // null
+
+console.log(nullNumber.out.distribute(branch => branch.expression)) // ["number", "null"]
