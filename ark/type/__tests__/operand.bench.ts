bebae3019083eb105d5715e70bb6c10789900243
David Blass <david@arktype.io>
2025-03-08 00:36:54 -0500

    fix to tuple expression test

--- ark/type/__tests__/operand.bench.ts
+++ ark/type/__tests__/operand.bench.ts
@@ -4,27 +4,27 @@ import { type } from "arktype"
 bench.baseline(() => type("never"))
 
 bench("single-quoted", () => type("'nineteen characters'")).types([
-	714,
+	716,
 	"instantiations"
 ])
 
 bench("double-quoted", () => type('"nineteen characters"')).types([
-	714,
+	716,
 	"instantiations"
 ])
 
 bench("regex literal", () => type("/nineteen characters/")).types([
-	704,
+	706,
 	"instantiations"
 ])
 
-bench("keyword", () => type("string")).types([555, "instantiations"])
+bench("keyword", () => type("string")).types([554, "instantiations"])
 
-bench("number", () => type("-98765.4321")).types([503, "instantiations"])
+bench("number", () => type("-98765.4321")).types([507, "instantiations"])
 
-bench("bigint", () => type("-987654321n")).types([575, "instantiations"])
+bench("bigint", () => type("-987654321n")).types([579, "instantiations"])
 
-bench("object", () => type({ foo: "string" })).types([1179, "instantiations"])
+bench("object", () => type({ foo: "string" })).types([1116, "instantiations"])
 
 bench("union", () =>
 	// Union is automatically discriminated using shallow or deep keys
@@ -39,4 +39,4 @@ bench("union", () =>
 		.or({
 			kind: "'pleb'"
 		})
-).types([4992, "instantiations"])
+).types([4987, "instantiations"])
--- ark/type/__tests__/operator.bench.ts
+++ ark/type/__tests__/operator.bench.ts
@@ -9,50 +9,50 @@ bench.baseline(() => {
 	type("symbol").narrow(() => true)
 })
 
-bench("array-string", () => type("number[]")).types([917, "instantiations"])
+bench("array-string", () => type("number[]")).types([921, "instantiations"])
 
 bench("array-tuple", () => type(["number", "[]"])).types([
-	896,
+	900,
 	"instantiations"
 ])
 
 bench("array-chain", () => type("number").array()).types([
-	505,
+	509,
 	"instantiations"
 ])
 
 bench("union-string", () => type("number|string")).types([
-	1153,
+	1172,
 	"instantiations"
 ])
 
 bench("union-tuple", () => type(["number", "|", "string"])).types([
-	1100,
+	1108,
 	"instantiations"
 ])
 
 bench("union-chain", () => type("number").or("string")).types([
-	1397,
+	1405,
 	"instantiations"
 ])
 
 bench("union-10-ary", () => type("0|1|2|3|4|5|6|7|8|9")).types([
-	4240,
+	4339,
 	"instantiations"
 ])
 
 bench("intersection-string", () => type("number&0")).types([
-	1311,
+	1322,
 	"instantiations"
 ])
 
 bench("intersection-tuple", () => type(["number", "&", "0"])).types([
-	1261,
+	1269,
 	"instantiations"
 ])
 
 bench("intersection-chain", () => type("number").and("0")).types([
-	1643,
+	1651,
 	"instantiations"
 ])
 
@@ -60,49 +60,49 @@ bench("intersection-10-ary", () =>
 	type(
 		"unknown&unknown&unknown&unknown&unknown&unknown&unknown&unknown&unknown&unknown"
 	)
-).types([5030, "instantiations"])
+).types([5097, "instantiations"])
 
 bench("group-shallow", () => type("string|(number[])")).types([
-	1421,
+	1445,
 	"instantiations"
 ])
 
 bench("group-nested", () => type("string|(number|(boolean))[][]")).types([
-	2151,
+	2190,
 	"instantiations"
 ])
 
 bench("group-deep", () => type("(0|(1|(2|(3|(4|5)[])[])[])[])[]")).types([
-	6980,
+	7064,
 	"instantiations"
 ])
 
-bench("bound-single", () => type("string>5")).types([1362, "instantiations"])
+bench("bound-single", () => type("string>5")).types([1372, "instantiations"])
 
 bench("bound-double", () => type("-7<=string.integer<99")).types([
-	2079,
+	2097,
 	"instantiations"
 ])
 
-bench("divisor", () => type("number%5")).types([956, "instantiations"])
+bench("divisor", () => type("number%5")).types([963, "instantiations"])
 
 bench("filter-tuple", () => type(["boolean", ":", b => b])).types([
-	1346,
+	1350,
 	"instantiations"
 ])
 
 bench("filter-chain", () => type("boolean").narrow(b => b)).types([
-	738,
+	742,
 	"instantiations"
 ])
 
 bench("morph-tuple", () => type(["boolean", "=>", b => b])).types([
-	1416,
+	1420,
 	"instantiations"
 ])
 
 bench("morph-chain", () => type("boolean").pipe(b => b)).types([
-	848,
+	852,
 	"instantiations"
 ])
 
@@ -148,7 +148,7 @@ bench("base object", () =>
 		filter: "'filter'",
 		narrow: "'narrow'"
 	})
-).types([10807, "instantiations"])
+).types([12890, "instantiations"])
 
 type Expected = {
 	readonly: "readonly"
@@ -200,4 +200,4 @@ bench("base object", () =>
 		filter: "'filter'",
 		narrow: "'narrow'"
 	})
-).types([13020, "instantiations"])
+).types([13108, "instantiations"])
