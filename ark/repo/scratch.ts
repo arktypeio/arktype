aae2cb2073d8c390d8c56f42e7af7a03184e72cc
David Blass <david@arktype.io>
2025-03-27 19:49:43 -0400

    undo

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,43 +1,21 @@
-import { bench } from "@ark/attest"
-import { type } from "arktype"
+type merge<base, props> = Omit<base, keyof props & keyof base> & props
 
-export type merge<base, props> = Omit<base, keyof props & keyof base> & props
+type Type<t> = {
+	merge: <r>(r: r) => Type<merge<t, r>>
+}
 
-declare const merge: <l, r>(l: l, r: r) => merge<l, r>
+declare const a: Type<{ a: 1 }>
 
-bench("with caching", () => {
-	const a = merge({ a: 1 }, { b: 2 })
-	const b = merge(a, { c: 3 })
-	const c = merge(b, { d: 4 })
-	const d = merge(c, { e: 5 })
-	const e = merge(d, { f: 6 })
-	const f = merge(e, { g: 7 })
-	const g = merge(f, { h: 8 })
-	const h = merge(g, { i: 9 })
-	const i = merge(h, { j: 10 })
-	const j = merge(i, { k: 11 })
-	const k = merge(j, { l: 12 })
-	const l = merge(k, { m: 13 })
-	const m = merge(l, { n: 14 })
-	const n = merge(m, { o: 15 })
-	const o = merge(n, { p: 16 })
-}).types([733, "instantiations"])
-
-bench("actual", () => {
-	const a = merge({ a: 1 }, { b: 2 })
-	const b = merge(a, { c: 3 })
-	const c = merge(b, { d: 4 })
-	const d = merge(c, { e: 5 })
-	const e = merge(d, { f: 6 })
-	const f = merge(e, { g: 7 })
-	const g = merge(f, { h: 8 })
-	const h = merge(g, { i: 9 })
-	const i = merge(h, { j: 10 })
-	const j = merge(i, { k: 11 })
-	const k = merge(j, { l: 12 })
-	const l = merge(k, { m: 13 })
-	const m = merge(l, { n: 14 })
-	const n = merge(m, { o: 15 })
-	// Error: Type instantiation is excessively deep and possibly infinite
-	const o = merge(n, { p: 16 })
-}).types([24349567, "instantiations"])
+const b = a.merge({ b: 2 })
+const c = b.merge({ c: 3 })
+const d = c.merge({ d: 4 })
+const e = d.merge({ e: 5 })
+const f = e.merge({ f: 6 })
+const g = f.merge({ g: 7 })
+const h = g.merge({ h: 8 })
+const i = h.merge({ i: 9 })
+const j = i.merge({ j: 10 })
+const k = j.merge({ k: 11 })
+const l = k.merge({ l: 12 })
+const m = l.merge({ m: 13 })
+const n = m.merge({ n: 14 })
