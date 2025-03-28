52ff3ce1563862e4728d42226d27e0ac71b4c5f0
David Blass <david@arktype.io>
2025-03-27 20:12:08 -0400

    remove death

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,21 +1,24 @@
-type merge<base, props> = Omit<base, keyof props & keyof base> & props
+import { type } from "arktype"
 
-type Type<t> = {
-	merge: <r>(r: r) => Type<merge<t, r>>
-}
+const User = type({
+	name: "string",
+	platform: "'android' | 'ios'",
+	"version?": "number | string"
+})
 
-declare const a: Type<{ a: 1 }>
+const selected = User.select({
+	kind: "domain",
+	where: d => d.domain === "string"
+})
 
-const b = a.merge({ b: 2 })
-const c = b.merge({ c: 3 })
-const d = c.merge({ d: 4 })
-const e = d.merge({ e: 5 })
-const f = e.merge({ f: 6 })
-const g = f.merge({ g: 7 })
-const h = g.merge({ h: 8 })
-const i = h.merge({ i: 9 })
-const j = i.merge({ j: 10 })
-const k = j.merge({ k: 11 })
-const l = k.merge({ l: 12 })
-const m = l.merge({ m: 13 })
-const n = m.merge({ n: 14 })
+const ConfiguredUser = User.configure(
+	{ description: "A STRING" },
+	{
+		kind: "domain",
+		where: d => d.domain === "string"
+	}
+)
+
+ConfiguredUser.get("name").description // A STRING
+ConfiguredUser.get("platform").description // "android" | "ios"
+ConfiguredUser.get("version").description // a number, A STRING or undefined
