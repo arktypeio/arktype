b6bdc4b75d58f2a609e7d188ec26f5b2bc237de8
David Blass <david@arktype.io>
2025-04-01 15:06:14 -0400

    ok

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,3 +1,5 @@
+import { type } from "arktype"
+
 const User = type({
 	name: "string",
 	platform: "'android' | 'ios'",
@@ -20,54 +22,3 @@ const ConfiguredUser = User.configure(
 ConfiguredUser.get("name").description // A STRING
 ConfiguredUser.get("platform").description // "android" | "ios"
 ConfiguredUser.get("version").description // a number, A STRING or undefined
-
-import type * as a from "arktype"
-import type {
-	inferDefinition,
-	validateDefinition
-} from "arktype/internal/parser/definition.ts"
-import type { bindThis } from "arktype/internal/scope.ts"
-
-declare global {
-	const type: typeof a.type
-	namespace type {
-		export type cast<t> = {
-			[a.inferred]?: t
-		}
-
-		export type errors = a.ArkErrors
-
-		export type validate<
-			def,
-			$ = {},
-			args = bindThis<def>
-		> = validateDefinition<def, $, args>
-
-		export type instantiate<def, $ = {}, args = bindThis<def>> = type<
-			inferDefinition<def, $, args>,
-			$
-		>
-
-		export type infer<def, $ = {}, args = bindThis<def>> = inferDefinition<
-			def,
-			$,
-			args
-		>
-
-		/** @ts-ignore cast variance */
-		export interface Any<out t = any, $ = any> extends a.BaseType<t, $> {}
-	}
-
-	type type<t = unknown, $ = {}> = a.Type<t, $>
-	const scope: typeof a.scope
-	const match: typeof a.match
-}
-
-function createBox2<const def>(
-	of: type.validate<def>
-): type.instantiate<{ of: def }>
-function createBox2(of: unknown) {
-	return type.raw({
-		box: of
-	})
-}
