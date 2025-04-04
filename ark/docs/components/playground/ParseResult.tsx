aac7311eb301d2b3991672d507c33a828501b35c
David Blass <david@arktype.io>
2025-04-04 13:31:22 -0400

    ok

--- ark/docs/components/playground/ParseResult.tsx
+++ ark/docs/components/playground/ParseResult.tsx
@@ -1,4 +1,6 @@
+import { tryCatch } from "@ark/util"
 import { Type, type type } from "arktype"
+import { Tab, Tabs } from "fumadocs-ui/components/tabs"
 import { RestoreDefault } from "./RestoreDefault.tsx"
 import {
 	backgroundsByResultKind,
@@ -6,6 +8,8 @@ import {
 	playgroundTypeVariableName,
 	type ResultKind
 } from "./utils.ts"
+
+import { DynamicCodeBlock } from "fumadocs-ui/components/dynamic-codeblock"
 
 export type ParseResult = type | Error
 
@@ -23,22 +27,8 @@ export const ParseResult = ({ parsed, restoreDefault }: ParseResult.Props) => {
 
 	const contents =
 		parsed instanceof Error ?
-			parsed instanceof ReferenceError ?
-				parsed.message.startsWith(playgroundTypeVariableName) ?
-					<>
-						Define a <code>Type</code> called{" "}
-						<code>"{playgroundTypeVariableName}"</code> to enable introspection.
-						<RestoreDefault onClick={restoreDefault} />
-					</>
-				: parsed.message.startsWith(playgroundOutVariableName) ?
-					<>
-						Assign the result of <code>{"Thing({})"}</code> to a variable called{" "}
-						<code>"{playgroundOutVariableName}"</code> to see it.
-						<RestoreDefault onClick={restoreDefault} />
-					</>
-				:	parsed.message
-			:	parsed.message
-		:	parsed.expression
+			createErrorResult(parsed, restoreDefault)
+		:	createTypeResult(parsed)
 
 	return (
 		<div className="flex-1 min-h-0">
@@ -51,12 +41,72 @@ export const ParseResult = ({ parsed, restoreDefault }: ParseResult.Props) => {
 				<h3 className="text-3xl text-fd-foreground font-semibold mb-2">
 					{title}
 				</h3>
-				{
-					<pre className="m-0 whitespace-pre-wrap">
-						<code>{contents}</code>
-					</pre>
-				}
+				{contents}
 			</div>
 		</div>
 	)
 }
+
+const createErrorResult = (e: Error, restoreDefault: () => void) =>
+	e instanceof ReferenceError ?
+		e.message.startsWith(playgroundTypeVariableName) ?
+			<>
+				Define a <code>Type</code> called{" "}
+				<code>"{playgroundTypeVariableName}"</code> to enable introspection.
+				<RestoreDefault onClick={restoreDefault} />
+			</>
+		: e.message.startsWith(playgroundOutVariableName) ?
+			<>
+				Assign the result of <code>{"Thing({})"}</code> to a variable called{" "}
+				<code>"{playgroundOutVariableName}"</code> to see it.
+				<RestoreDefault onClick={restoreDefault} />
+			</>
+		:	e.message
+	:	e.message
+
+const TabDescription = ({ children }: { children: React.ReactNode }) => (
+	<p className="mb-2 text-sm">{children}</p>
+)
+
+const createTypeResult = (t: Type) => (
+	<Tabs
+		items={[
+			".expression",
+			".description",
+			".toJsonSchema()",
+			".json",
+			".precompilation"
+		]}
+	>
+		<Tab value=".expression">
+			<TabDescription>
+				A syntax string similar to native TypeScript
+			</TabDescription>
+			<DynamicCodeBlock lang="ts" code={t.expression} />
+		</Tab>
+		<Tab value=".description">
+			<TabDescription>A human-readable English description</TabDescription>
+			<DynamicCodeBlock lang="ts" code={t.description} />
+		</Tab>
+		<Tab value=".toJsonSchema()">
+			<TabDescription>JSON Schema this generates</TabDescription>
+			<DynamicCodeBlock
+				lang="json"
+				code={tryCatch(
+					() => JSON.stringify(t.toJsonSchema(), null, 4),
+					e => String(e)
+				)}
+			/>
+		</Tab>
+		<Tab value=".json">
+			<TabDescription>
+				A serialized representation of the Type's internal structure
+			</TabDescription>
+			<DynamicCodeBlock lang="json" code={JSON.stringify(t.json, null, 4)} />
+		</Tab>
+		<Tab value=".precompilation">
+			<TabDescription>JIT-optimized validation code</TabDescription>
+			<DynamicCodeBlock lang="js" code={t.precompilation!} />
+		</Tab>
+	</Tabs>
+)
