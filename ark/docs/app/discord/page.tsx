b2cf106a9adaaa4a13d93c91ff5f267bdd0853d2
David Blass <david@arktype.io>
2025-04-09 11:46:28 -0400

    fix discord link

--- ark/docs/app/discord/page.tsx
+++ ark/docs/app/discord/page.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react"
+import { redirect } from "next/navigation"
 import { defineMetadata } from "../metadata.ts"
 
 export const metadata = defineMetadata({
@@ -6,10 +6,6 @@ export const metadata = defineMetadata({
 	ogImage: "ogDiscord.png"
 })
 
-export default () => {
-	useCallback(() => {
-		window.location.href = "https://discord.com/invite/xEzdc3fJQC"
-	}, [])
-
-	return null
+export default function DiscordPage() {
+	redirect("https://discord.com/invite/xEzdc3fJQC")
 }
