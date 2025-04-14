0dd41b75b9009c527cda4bb3a59686943cd5f175
David Blass <david@arktype.io>
2025-04-14 10:18:17 -0400

    ok

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,3 +1,9 @@
 import { type } from "arktype"
 
-export const Test = type.string.brand("test")
+const Even = type.number.divisibleBy(2)
+const By3 = type.number.divisibleBy(3)
+const By6 = Even.and(By3)
+
+console.log(By6.description)
+By6.extends(By3) //?
+By3.extends(By6) //?
