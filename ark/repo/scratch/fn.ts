77a2899d3017803c9ca39b05b3f2b62657035842
David Blass <david@arktype.io>
2025-04-18 18:56:24 -0400

    ok

--- ark/repo/scratch/fn.ts
+++ ark/repo/scratch/fn.ts
@@ -2,7 +2,7 @@ import type { type } from "arktype"
 import type { ArkAmbient } from "arktype/config"
 
 export type FunctionParser<$> = {
-	<ret = unknown>(
+	<const ret = unknown>(
 		_?: ":",
 		ret?: type.validate<ret, $>
 	): <
@@ -12,7 +12,7 @@ export type FunctionParser<$> = {
 		implementation: implementation
 	) => implementation
 
-	<const arg0, ret = unknown>(
+	<const arg0, const ret = unknown>(
 		arg0: type.validate<arg0, $>,
 		_?: ":",
 		ret?: type.validate<ret, $>
@@ -24,7 +24,7 @@ export type FunctionParser<$> = {
 		implementation: implementation
 	) => implementation
 
-	<const arg0, const arg1, ret = unknown>(
+	<const arg0, const arg1, const ret = unknown>(
 		arg0: type.validate<arg0, $>,
 		arg1: type.validate<arg1, $>,
 		_?: ":",
@@ -38,7 +38,7 @@ export type FunctionParser<$> = {
 		implementation: implementation
 	) => implementation
 
-	<const arg0, const arg1, const arg2, ret = unknown>(
+	<const arg0, const arg1, const arg2, const ret = unknown>(
 		arg0: type.validate<arg0, $>,
 		arg1: type.validate<arg1, $>,
 		arg2: type.validate<arg2, $>,
@@ -54,7 +54,7 @@ export type FunctionParser<$> = {
 		implementation: implementation
 	) => implementation
 
-	<arg0, arg1, arg2, arg3, ret = unknown>(
+	<const arg0, const arg1, const arg2, const arg3, const ret = unknown>(
 		arg0: type.validate<arg0, $>,
 		arg1: type.validate<arg1, $>,
 		arg2: type.validate<arg2, $>,
@@ -72,7 +72,14 @@ export type FunctionParser<$> = {
 		implementation: implementation
 	) => implementation
 
-	<arg0, arg1, arg2, arg3, arg4, ret = unknown>(
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const ret = unknown
+	>(
 		arg0: type.validate<arg0, $>,
 		arg1: type.validate<arg1, $>,
 		arg2: type.validate<arg2, $>,
@@ -87,6 +94,630 @@ export type FunctionParser<$> = {
 			arg2: type.infer<arg2, $>,
 			arg3: type.infer<arg3, $>,
 			arg4: type.infer<arg4, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const arg10,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		arg10: type.validate<arg10, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>,
+			arg10: type.infer<arg10, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const arg10,
+		const arg11,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		arg10: type.validate<arg10, $>,
+		arg11: type.validate<arg11, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>,
+			arg10: type.infer<arg10, $>,
+			arg11: type.infer<arg11, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const arg10,
+		const arg11,
+		const arg12,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		arg10: type.validate<arg10, $>,
+		arg11: type.validate<arg11, $>,
+		arg12: type.validate<arg12, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>,
+			arg10: type.infer<arg10, $>,
+			arg11: type.infer<arg11, $>,
+			arg12: type.infer<arg12, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const arg10,
+		const arg11,
+		const arg12,
+		const arg13,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		arg10: type.validate<arg10, $>,
+		arg11: type.validate<arg11, $>,
+		arg12: type.validate<arg12, $>,
+		arg13: type.validate<arg13, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>,
+			arg10: type.infer<arg10, $>,
+			arg11: type.infer<arg11, $>,
+			arg12: type.infer<arg12, $>,
+			arg13: type.infer<arg13, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const arg10,
+		const arg11,
+		const arg12,
+		const arg13,
+		const arg14,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		arg10: type.validate<arg10, $>,
+		arg11: type.validate<arg11, $>,
+		arg12: type.validate<arg12, $>,
+		arg13: type.validate<arg13, $>,
+		arg14: type.validate<arg14, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>,
+			arg10: type.infer<arg10, $>,
+			arg11: type.infer<arg11, $>,
+			arg12: type.infer<arg12, $>,
+			arg13: type.infer<arg13, $>,
+			arg14: type.infer<arg14, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const arg10,
+		const arg11,
+		const arg12,
+		const arg13,
+		const arg14,
+		const arg15,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		arg10: type.validate<arg10, $>,
+		arg11: type.validate<arg11, $>,
+		arg12: type.validate<arg12, $>,
+		arg13: type.validate<arg13, $>,
+		arg14: type.validate<arg14, $>,
+		arg15: type.validate<arg15, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>,
+			arg10: type.infer<arg10, $>,
+			arg11: type.infer<arg11, $>,
+			arg12: type.infer<arg12, $>,
+			arg13: type.infer<arg13, $>,
+			arg14: type.infer<arg14, $>,
+			arg15: type.infer<arg15, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const arg10,
+		const arg11,
+		const arg12,
+		const arg13,
+		const arg14,
+		const arg15,
+		const arg16,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		arg10: type.validate<arg10, $>,
+		arg11: type.validate<arg11, $>,
+		arg12: type.validate<arg12, $>,
+		arg13: type.validate<arg13, $>,
+		arg14: type.validate<arg14, $>,
+		arg15: type.validate<arg15, $>,
+		arg16: type.validate<arg16, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>,
+			arg10: type.infer<arg10, $>,
+			arg11: type.infer<arg11, $>,
+			arg12: type.infer<arg12, $>,
+			arg13: type.infer<arg13, $>,
+			arg14: type.infer<arg14, $>,
+			arg15: type.infer<arg15, $>,
+			arg16: type.infer<arg16, $>
+		) => unknown extends ret ? unknown : type.infer<ret, $>
+	>(
+		implementation: implementation
+	) => implementation
+
+	<
+		const arg0,
+		const arg1,
+		const arg2,
+		const arg3,
+		const arg4,
+		const arg5,
+		const arg6,
+		const arg7,
+		const arg8,
+		const arg9,
+		const arg10,
+		const arg11,
+		const arg12,
+		const arg13,
+		const arg14,
+		const arg15,
+		const arg16,
+		const arg17,
+		const ret = unknown
+	>(
+		arg0: type.validate<arg0, $>,
+		arg1: type.validate<arg1, $>,
+		arg2: type.validate<arg2, $>,
+		arg3: type.validate<arg3, $>,
+		arg4: type.validate<arg4, $>,
+		arg5: type.validate<arg5, $>,
+		arg6: type.validate<arg6, $>,
+		arg7: type.validate<arg7, $>,
+		arg8: type.validate<arg8, $>,
+		arg9: type.validate<arg9, $>,
+		arg10: type.validate<arg10, $>,
+		arg11: type.validate<arg11, $>,
+		arg12: type.validate<arg12, $>,
+		arg13: type.validate<arg13, $>,
+		arg14: type.validate<arg14, $>,
+		arg15: type.validate<arg15, $>,
+		arg16: type.validate<arg16, $>,
+		arg17: type.validate<arg17, $>,
+		_?: ":",
+		ret?: type.validate<ret, $>
+	): <
+		implementation extends (
+			arg0: type.infer<arg0, $>,
+			arg1: type.infer<arg1, $>,
+			arg2: type.infer<arg2, $>,
+			arg3: type.infer<arg3, $>,
+			arg4: type.infer<arg4, $>,
+			arg5: type.infer<arg5, $>,
+			arg6: type.infer<arg6, $>,
+			arg7: type.infer<arg7, $>,
+			arg8: type.infer<arg8, $>,
+			arg9: type.infer<arg9, $>,
+			arg10: type.infer<arg10, $>,
+			arg11: type.infer<arg11, $>,
+			arg12: type.infer<arg12, $>,
+			arg13: type.infer<arg13, $>,
+			arg14: type.infer<arg14, $>,
+			arg15: type.infer<arg15, $>,
+			arg16: type.infer<arg16, $>,
+			arg17: type.infer<arg17, $>
 		) => unknown extends ret ? unknown : type.infer<ret, $>
 	>(
 		implementation: implementation
