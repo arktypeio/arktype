f96e8a5706a385198a32dd5f032eeb0794c435d8
David Blass <david@arktype.io>
2025-04-23 03:35:00 -0400

    ok

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,66 +1,110 @@
 import { bench } from "@ark/attest"
-import type { Backslash, Scanner, unset, WhitespaceChar } from "@ark/util"
+import type {
+	Backslash,
+	ErrorMessage,
+	Scanner,
+	WhitespaceChar
+} from "@ark/util"
 
-type inferRegex<unscanned extends string> = parseSequence<
-	unscanned,
-	never,
-	"",
-	""
->
-
-export type Quantifier = "*" | "+" | "?" | "{" | "}"
+export type Quantifier = "*" | "+" | "?" | "{"
 export type Boundary = "^" | "$" | "(" | ")" | "[" | "]"
-export type Operator = "|" | "."
-export type Escape = "\\"
 
-export type Control = Quantifier | Boundary | Operator | Escape
+export type Control = Quantifier | Boundary | "|" | "." | "/"
 
-export type StringClassChar = "w" | "W" | "D" | "S"
-
-type result = inferRegex<"afgdsfa|fsdhaoi|bag?|fdsa">
-//    ^?
+type inferRegex<unscanned extends string> = parseSequence<unscanned, never>
 
 type parseSequence<
-	remaining extends string,
-	branches extends string,
-	last extends string,
-	sequence extends string
+	source extends string,
+	inferredBranches extends string
+> = continueSequence<source, inferredBranches, [], string, empty>
+
+// we're just using the symbol keyword itself to represent an unset value here
+// since it not stringifiable so it must be handled
+type empty = symbol
+
+type updateSequence<sequence extends string, last extends string | empty> =
+	last extends string ? `${sequence}${last}` : string
+
+type continueSequence<
+	source extends string,
+	inferredBranches extends string,
+	groups extends string[],
+	sequence extends string,
+	last extends string | empty
 > =
-	remaining extends Scanner.shift<infer lookahead, infer unscanned> ?
+	source extends Scanner.shift<infer lookahead, infer unscanned> ?
 		lookahead extends Backslash ?
-			unscanned extends (
-				Scanner.shift<infer nextLookahead, infer nextUnscanned>
+			parseEscapedChar<unscanned> extends (
+				ParsedEscapeSequence<infer result, infer nextUnscanned>
 			) ?
-				nextLookahead extends StringClassChar ?
-					parseSequence<nextUnscanned, branches, string, `${sequence}${last}`>
-				: nextLookahead extends "d" ?
-					parseSequence<
+				result extends ErrorMessage ?
+					result
+				:	continueSequence<
 						nextUnscanned,
-						branches,
-						`${bigint}`,
-						`${sequence}${last}`
+						inferredBranches,
+						groups,
+						updateSequence<sequence, last>,
+						result
 					>
-				: nextLookahead extends "s" ?
-					parseSequence<
-						nextUnscanned,
-						branches,
-						WhitespaceChar,
-						`${sequence}${last}`
-					>
-				: nextLookahead extends Control | "/" ?
-					parseSequence<
-						nextUnscanned,
-						branches,
-						nextLookahead,
-						`${sequence}${last}`
-					>
-				:	"unnecessary escape"
-			:	"unmatched backslash"
+			:	never
 		: lookahead extends "|" ?
-			parseSequence<unscanned, branches | sequence, "", "">
-		:	parseSequence<unscanned, branches, lookahead, `${sequence}${last}`>
-	:	branches | sequence
+			parseSequence<unscanned, inferredBranches | sequence>
+		: lookahead extends "^" ?
+			[allGroupsEmpty<groups>, string, last] extends [true, sequence, empty] ?
+				continueSequence<unscanned, inferredBranches, groups, "", empty>
+			:	ErrorMessage<`Anchor ^ may not appear mid-pattern`>
+		: lookahead extends "$" ?
+			ErrorMessage<`Anchor $ may not appear mid-pattern`>
+		: lookahead extends "(" ? "groups unsupported"
+		: continueSequence<
+				unscanned,
+				inferredBranches,
+				groups,
+				updateSequence<sequence, last>,
+				lookahead
+			>
+	:	inferredBranches | sequence
+
+type allGroupsEmpty<groups extends unknown[]> =
+	groups extends [infer head, ...infer tail] ?
+		string extends head ?
+			allGroupsEmpty<tail>
+		:	false
+	:	true
+
+type ParsedEscapeSequence<result extends string, unscanned extends string> = [
+	result: result,
+	unscanned: unscanned
+]
+
+// all widened to string since TypeScript doesn't have a better type to represent them
+type StringClassChar = "w" | "W" | "D" | "S"
+
+type parseEscapedChar<source extends string> =
+	source extends Scanner.shift<infer lookahead, infer unscanned> ?
+		ParsedEscapeSequence<
+			lookahead extends StringClassChar ? string
+			: lookahead extends "d" ? `${bigint}`
+			: lookahead extends "s" ? WhitespaceChar
+			: lookahead extends Control ? lookahead
+			: ErrorMessage<`Escape preceding ${lookahead} is unnecessary and should be removed.`>,
+			unscanned
+		>
+	:	ParsedEscapeSequence<ErrorMessage<`A regex cannot end with \\`>, "">
+
+type TestA = inferRegex<"^^">
+//   ^?
+
+type TestB = inferRegex<"a^">
+//    ^?
+
+type TestC = inferRegex<"^foo|^bar">
+//    ^?
+
+type TestD = inferRegex<"f(^)">
+//    ^?
 
 bench("string", () => {
-	type Z = inferRegex<"abc|br?|superfood|okok">
-}).types([465, "instantiations"])
+	type fdasZ = inferRegex<"abc|^br?|superfood|okok">
+	//     ^?
+}).types([317, "instantiations"])
