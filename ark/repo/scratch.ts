5bd1e5a08233ddd82f72841b34a8780ad2c16ba2
David Blass <david@arktype.io>
2025-04-28 23:14:01 -0400

    fix error scaling

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,5 +1,22 @@
+import { bench } from "@ark/attest"
 import { type } from "arktype"
 
-const Thing = type(/^user-/).as<"foo">()
+const data = [...new Array(20000)].map(() => ({
+	a: "1",
+	b: "1",
+	c: "", // Make sure we get validation error on this field
+	d: "1",
+	e: "1"
+}))
 
-const out = Thing("f")
+const ArkType = type({
+	a: "string > 0",
+	b: "string > 0",
+	c: "string > 0",
+	d: "string > 0",
+	e: "string > 0"
+}).array()
+
+const start = performance.now()
+ArkType(data)
+console.log(performance.now() - start)
