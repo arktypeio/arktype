0b6995461594a80b6515141e0609c3071042d452
David Blass <david@arktype.io>
2025-07-09 14:03:56 -0400

    update benches

--- ark/regex/__tests__/regex.bench.ts
+++ ark/regex/__tests__/regex.bench.ts
@@ -12,36 +12,36 @@ type Result = array.repeat<3, 50>
 
 bench("repeat(100)", () => {
 	type Result = array.repeat<2, 100>
-}).types([713, "instantiations"])
+}).types([678, "instantiations"])
 
 bench("repeat(500)", () => {
 	type Result = array.repeat<2, 500>
-}).types([3616, "instantiations"])
+}).types([3552, "instantiations"])
 
 bench("repeat(512)", () => {
 	type Result = array.repeat<2, 512>
-}).types([560, "instantiations"])
+}).types([557, "instantiations"])
 
 bench("repeat(513)", () => {
 	type Result = array.repeat<2, 513>
-}).types([5988, "instantiations"])
+}).types([5968, "instantiations"])
 
 bench("anchored union", () => {
 	const r = regex("typescript|^go$|brrr$")
-}).types([2438, "instantiations"])
+}).types([2545, "instantiations"])
 
 bench("?(1)", () => {
 	const r = regex("^a?$")
-}).types([473, "instantiations"])
+}).types([407, "instantiations"])
 
 bench("?(4)", () => {
 	const r = regex("^a?b?c?d?$")
-}).types([1613, "instantiations"])
+}).types([1423, "instantiations"])
 
 bench("?(7)", () => {
 	const r = regex("^a?b?c?d?e?f?g?$")
-}).types([4462, "instantiations"])
+}).types([4170, "instantiations"])
 
 bench("escape(6)", () => {
 	const r = regex("^\t\n\r\v\f\0$")
-}).types([960, "instantiations"])
+}).types([958, "instantiations"])
