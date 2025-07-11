a115aaa7bc82a5b3607ee7fc51234d6120c28911
David Blass <david@arktype.io>
2025-07-11 16:46:21 -0400

    update example code

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,9 +1,35 @@
 import { type } from "arktype"
 
-const Even = type.number.divisibleBy(2)
-const By3 = type.number.divisibleBy(3)
-const By6 = Even.and(By3)
+const original = type({
+	id: "string",
+	name: "string",
+	"favoriteColor?": "string"
+})
 
-console.log(By6.description)
-By6.extends(By3) //?
-By3.extends(By6) //?
+const updateSchema = original.map(prop =>
+	prop.key === "name" ?
+		{
+			...prop,
+			// update key name if needed
+			key: `prefix${prop.key}` as const,
+			// update optionality if needed
+			kind: "optional",
+			// update value if needed
+			value: prop.value.or(type.null)
+		}
+	:	prop
+)
+
+type Result = {
+	id: string
+	favoriteColor?: string
+	prefixname?: string | null
+}
+
+/* updateSchema:
+{
+id: "string",
+"name?": "string"
+"favoriteColor?": "string | null"
+}
+*/
