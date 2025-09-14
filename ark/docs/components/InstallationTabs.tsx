5cf129b399fe4c6214055702bfb11ef0cc48d18c
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2025-09-15 00:56:49 +0600

    refactor installation tabs to use commands object

--- ark/docs/components/InstallationTabs.tsx
+++ ark/docs/components/InstallationTabs.tsx
@@ -1,22 +1,25 @@
 import { Tab, Tabs } from "fumadocs-ui/components/tabs"
 import { CodeBlock } from "./CodeBlock.tsx"
 
-const installers = ["pnpm", "npm", "yarn", "bun"] as const satisfies string[]
-
-export type Installer = (typeof installers)[number]
+const installerCommands = {
+	pnpm: "install",
+	npm: "install",
+	yarn: "add",
+	bun: "add"
+} as const satisfies Record<string, string>
 
 type InstallationTabProps = {
-	name: Installer
+	name: keyof typeof installerCommands
 }
 
 const InstallerTab = ({ name }: InstallationTabProps) => (
 	<Tab value={name} className="installer-tab">
-		<CodeBlock lang="bash">{`${name} ${name in ["yarn", "bun"] ? "add" : "install"} arktype`}</CodeBlock>
+		<CodeBlock lang="bash">{`${name} ${installerCommands[name]} arktype`}</CodeBlock>
 	</Tab>
 )
 
 export const InstallationTabs = () => (
-	<Tabs items={installers}>
+	<Tabs items={Object.keys(installerCommands)}>
 		<InstallerTab name="pnpm" />
 		<InstallerTab name="npm" />
 		<InstallerTab name="yarn" />
