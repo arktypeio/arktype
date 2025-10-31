ca682b0df4a89c96957ac7adbc684900adcc1e63
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-10-31 11:53:26 +0100

    Fixed inference for generic `Type`s when argument has `Default` property

--- ark/type/variants/base.ts
+++ ark/type/variants/base.ts
@@ -637,7 +637,10 @@ interface Type<out t = unknown, $ = {}>
 	): reduceOut
 
 	/** The Type's [StandardSchema](https://github.com/standard-schema/standard-schema) properties */
-	"~standard": StandardSchemaV1.ArkTypeProps<this["inferIn"], this["inferOut"]>
+	"~standard": StandardSchemaV1.ArkTypeProps<
+		NoInfer<this["inferIn"]>,
+		NoInfer<this["inferOut"]>
+	>
 
 	// deprecate Function methods so they are deprioritized as suggestions
 
