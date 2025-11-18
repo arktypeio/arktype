37a0cb814153468b2e60c7077aeee9eda2754c69
David Blass <david@arktype.io>
2025-11-18 18:02:17 -0500

    add typeFromStandardSchema to scratch

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,15 +1,44 @@
-declare const snippet: string
+import type { StandardSchemaV1 } from "@standard-schema/spec"
+import { type, type Out } from "arktype"
+import z from "zod"
 
-import { type } from "arktype"
+type standardSchemaToArkType<
+	schema extends StandardSchemaV1,
+	i = StandardSchemaV1.InferInput<schema>,
+	o = StandardSchemaV1.InferOutput<schema>
+> = [i, o] extends [o, i] ? type<i> : type<(In: i) => Out<o>>
 
-type Z<T> = { [k in keyof T]?: T[k] }
+const typeFromStandardSchema = <schema extends StandardSchemaV1>(
+	schema: schema
+): standardSchemaToArkType<schema> =>
+	type.unknown.pipe((v, ctx) => {
+		const result = schema["~standard"].validate(
+			v
+		) as StandardSchemaV1.Result<unknown>
 
-type R = Z<{ a?: string }>
+		if (result.issues) {
+			for (const { message, path } of result.issues) {
+				if (path) {
+					ctx.error({
+						message: message,
+						path: path.map(k => (typeof k === "object" ? k.key : k))
+					})
+				} else {
+					ctx.error({
+						message
+					})
+				}
+			}
+		} else {
+			return result.value
+		}
+	}) as never
 
-const htmlTag = type({
-	html: "/^<(?<tag>[a-zA-Z]+)>.*?<\\/\\k<tag>>$/"
-})
+const TFromZod = typeFromStandardSchema(
+	z.object({
+		foo: z.string()
+	})
+)
 
-const t = /.*/.exec("")!
-
-t.groups
+const valid = TFromZod({ foo: "foo" })
+const invalid = TFromZod({ foo: 5 })
