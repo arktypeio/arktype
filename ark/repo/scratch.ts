75813a5ff0b44b0ef658cc64c8354ef1265fda38
David Blass <david@arktype.io>
2025-12-01 13:40:54 -0500

    ok

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,3 +1,16 @@
-import { fn, type } from "arktype"
+import { type } from "arktype"
 import * as v from "valibot"
 import z from "zod"
+
+const ZodAddress = z.object({
+	street: z.string(),
+	city: z.string()
+})
+
+const User = type({
+	name: "string",
+	age: v.number(),
+	address: ZodAddress
+})
+
+User
