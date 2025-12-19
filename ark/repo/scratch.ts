e19347a4483ead764cdfb4917597a3fe06ec8600
David Blass <david@arktype.io>
2025-12-19 18:42:15 -0500

    ok

--- ark/repo/scratch.ts
+++ ark/repo/scratch.ts
@@ -1,16 +1,12 @@
-import { scope, type } from "arktype"
+import { join } from "@ark/util"
+import { type } from "arktype"
 
-const $ = scope({
-	Foo: {
-		"oneOf?": "Bar[]" // NB: don't get the error if this is not an array
-	},
-	Bar: "Foo"
-}).export()
+const options = ["red", "blue"] as const
 
-const baz = $.Bar.pipe((_: object): type.Any | undefined => {
-	console.log("this never gets logged since pipe isn't entered")
-	return type("string")
-})
+const color = type.enumerated(...options)
 
-const r = baz({ oneOf: [{}] }) // throws "TypeError: this.Foo1Apply is not a function"
-console.log(r?.toString())
+const darkColorLiteral = type.enumerated(
+	...options.map(base => `${base}Dark` as const)
+)
+
+const darkColorRegex = type(`/^(${join(options, "|")})Dark$/`)
